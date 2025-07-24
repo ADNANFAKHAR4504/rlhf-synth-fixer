6947d52b91b87774c45248453cdbb7f13906e814
Mathew Kadambatt <mathew.k@turing.com>
2025-07-24 09:00:59 +0530

    fix: adding correct path fo rthe init test cases ref file

--- test/tap-stack.unit.test.ts
+++ test/tap-stack.unit.test.ts
@@ -10,7 +10,7 @@ describe('Elastic Beanstalk CloudFormation Template', () => {
 
   beforeAll(() => {
     // Load the JSON version of the CloudFormation template.
-    const templatePath = path.join(__dirname, '/lib/TapStack.json');
+    const templatePath = path.join(__dirname, '../lib/TapStack.json');
     const templateContent = fs.readFileSync(templatePath, 'utf8');
     template = JSON.parse(templateContent);
   });
