4a221ad5b5bb560e35a02589bf72022e32e783a6
Subhasis Das <subhasisdas125@gmail.com>
2025-07-25 14:36:23 +0530

    reset cdktf ts template

--- templates/cdktf-ts/jest.config.js
+++ templates/cdktf-ts/jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
     '!<rootDir>/**/*.test.ts',
     '!<rootDir>/node_modules/**',
   ],
-  setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
   coverageReporters: ['text', 'lcov', 'json-summary'],
   coverageThreshold: {
     global: {
