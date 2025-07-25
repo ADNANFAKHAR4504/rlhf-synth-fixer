21045c4660099e52b71660be86e52ca00d4b14bd
Subhasis Das <subhasisdas125@gmail.com>
2025-07-25 15:08:04 +0530

    added sample s3 bucket stack using cdktf for testing out workflow

---
+++ lib/s3-stack.ts
@@ -0,0 +1,23 @@
+import { S3Bucket } from '@cdktf/provider-aws/lib/s3-bucket';
+import { TerraformOutput, TerraformStack } from 'cdktf';
+import { Construct } from 'constructs';
+
+export class S3Stack extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    // create an S3 bucket
+    const bucket = new S3Bucket(this, 'S3Bucket', {
+      bucketPrefix: `CdkTfTsTest`,
+      versioning: {
+        enabled: true,
+      },
+    });
+
+    // Output the bucket name in the S3Stack
+    new TerraformOutput(this, "bucket_name", {
+      value: bucket.id,
+      description: "The name of the S3 bucket created by CDKTF",
+    });
+  }
+}
---
+++ test/tap-stack.int.test.ts
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+
+describe('Terraform Outputs', () => {
+  
+  let outputFilePath : string;
+  let outputData : string;
+  let outputs : any;
+
+  beforeEach(() => {
+    // Reset mocks before each test
+    jest.clearAllMocks();
+
+    outputFilePath = path.join(__dirname, 'cfn-outputs/flat-outputs.json');
+    outputData = fs.readFileSync(outputFilePath, 'utf-8');
+    outputs = JSON.parse(outputData);
+  });
+
+  describe("Bucket Name", () => {
+    test("Bucket Name should contain the correct substring", () => {
+      expect(outputs.bucket_name.split("/").pop()).toContain("CdkTfTsTest");
+    });
+  });
+});
