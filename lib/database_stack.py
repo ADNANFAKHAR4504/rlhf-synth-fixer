e287a8f99a462c4b83404aa1c213c19090c9da48
Rajendra <rajendra.s@turing.com>
2025-07-26 17:13:33 +0530

    fix db issues

--- lib/database_stack.py
+++ lib/database_stack.py
@@ -15,8 +15,9 @@ class DatabaseStack(Stack):
     super().__init__(scope, construct_id, **kwargs)
 
     # aurora_version = rds.AuroraMysqlEngineVersion.VER_2_08_1
-    aurora_version = rds.AuroraMysqlEngineVersion.of("8.0.mysql_aurora.3.04.1")
+    aurora_version = aurora_version = rds.AuroraMysqlEngineVersion.VER_3_04_1
 
+    # engine = rds.DatabaseClusterEngine.aurora_mysql(version=aurora_version)
     # Create a new parameter group for your Aurora MySQL version
     parameter_group = rds.ParameterGroup(
         self, "AuroraMySQL8ParameterGroup",
