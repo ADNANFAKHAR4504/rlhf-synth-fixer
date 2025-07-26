32486adf730068b97e252207673d1dfbd4104df5
rajendra-s-aws <rajendra.s@turing.com>
2025-07-26 11:45:22 +0530

    Update metadata_stack.py

--- lib/metadata_stack.py
+++ lib/metadata_stack.py
@@ -15,7 +15,7 @@ class SecureInfrastructureStack(Stack):
   def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
     super().__init__(scope, construct_id, **kwargs)
 
-    # Define the KMS Key for encrypting environment variables
+    # Define the KMS Key for encrypting environment variables 
     self.encryption_key = kms.Key(
       self,
       "LambdaEnvVarsEncryptionKey",
