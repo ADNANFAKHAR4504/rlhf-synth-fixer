346ca1d6d5cf9323843a26161cfdeb23544a7000
Rajendra <rajendra.s@turing.com>
2025-07-28 14:49:49 +0530

    added auto generated files

---
+++ tests/unit/test_tap_stack.py
@@ -0,0 +1,53 @@
+# import os
+# import sys
+import unittest
+
+import aws_cdk as cdk
+# import pytest
+# from aws_cdk.assertions import Match, Template
+from aws_cdk.assertions import Template
+from pytest import mark
+
+from lib.tap_stack import TapStack, TapStackProps
+
+
+@mark.describe("TapStack")
+class TestTapStack(unittest.TestCase):
+  """Test cases for the TapStack CDK stack"""
+
+  def setUp(self):
+    """Set up a fresh CDK app for each test"""
+    self.app = cdk.App()
+
+  @mark.it("creates an S3 bucket with the correct environment suffix")
+  def test_creates_s3_bucket_with_env_suffix(self):
+    # ARRANGE
+    env_suffix = "testenv"
+    stack = TapStack(self.app, "TapStackTest",
+                     TapStackProps(environment_suffix=env_suffix))
+    template = Template.from_stack(stack)
+
+    # ASSERT
+    template.resource_count_is("AWS::S3::Bucket", 1)
+    template.has_resource_properties("AWS::S3::Bucket", {
+        "BucketName": f"tap-bucket-{env_suffix}"
+    })
+
+  @mark.it("defaults environment suffix to 'dev' if not provided")
+  def test_defaults_env_suffix_to_dev(self):
+    # ARRANGE
+    stack = TapStack(self.app, "TapStackTestDefault")
+    template = Template.from_stack(stack)
+
+    # ASSERT
+    template.resource_count_is("AWS::S3::Bucket", 1)
+    template.has_resource_properties("AWS::S3::Bucket", {
+        "BucketName": "tap-bucket-dev"
+    })
+
+  @mark.it("Write Unit Tests")
+  def test_write_unit_tests(self):
+    # ARRANGE
+    self.fail(
+        "Unit test for TapStack should be implemented here."
+    )
