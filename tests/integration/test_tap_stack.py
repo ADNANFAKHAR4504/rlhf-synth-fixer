6fac819055a58e7342394893e17c3da115e0f049
Ali iqbal <iqbala@turing.com>
2025-07-30 23:14:09 +0500

    Integration tests

--- tests/integration/test_tap_stack.py
+++ tests/integration/test_tap_stack.py
@@ -1,78 +1,75 @@
+"""Integration tests for nested SecureS3Stack in TapStack."""
+
 import pytest
 from aws_cdk import App
 from aws_cdk.assertions import Template
 
-from lib.tap_stack import TapStack, TapStackProps
+try:
+  from tests.tap.tap_stack import TapStack
+  from tests.tap.constructs.secure_s3_stack import SecureS3Stack
+except ImportError:
+  from tap.tap_stack import TapStack
+  from tap.constructs.secure_s3_stack import SecureS3Stack
 
 
 @pytest.fixture(scope="module")
-def synthesized_template():
+def template_fixture():
+  """Fixture to synthesize the nested SecureS3Stack."""
   app = App()
-  stack = TapStack(
-    app,
-    "IntegrationTestTapStack",
-    props=TapStackProps(
-      environment_suffix="int",
-      principal_arns=["arn:aws:iam::123456789012:user/test-user"]
-    )
+  tap_stack = TapStack(app, "TapStackTest", environment="dev")
+  nested_stack = next(
+      child for child in tap_stack.node.children if isinstance(child, SecureS3Stack)
   )
-  return Template.from_stack(stack)
+  return Template.from_stack(nested_stack)
 
 
-def test_kms_key_created(synthesized_template):
-  template = synthesized_template
-  template.resource_count_is("AWS::KMS::Key", 1)
+def test_kms_key_created(template_fixture):
+  template_fixture.resource_count_is("AWS::KMS::Key", 1)
 
 
-def test_s3_bucket_created(synthesized_template):
-  template = synthesized_template
-  template.resource_count_is("AWS::S3::Bucket", 1)
+def test_s3_bucket_created(template_fixture):
+  template_fixture.resource_count_is("AWS::S3::Bucket", 1)
 
 
-def test_bucket_encryption_is_kms(synthesized_template):
-  template = synthesized_template
-  template.has_resource_properties("AWS::S3::Bucket", {
-    "BucketEncryption": {
-      "ServerSideEncryptionConfiguration": [
-        {
-          "ServerSideEncryptionByDefault": {
-            "SSEAlgorithm": "aws:kms"
-          }
-        }
-      ]
-    }
+def test_bucket_encryption_is_kms(template_fixture):
+  template_fixture.has_resource_properties("AWS::S3::Bucket", {
+      "BucketEncryption": {
+          "ServerSideEncryptionConfiguration": [
+              {
+                  "ServerSideEncryptionByDefault": {
+                      "SSEAlgorithm": "aws:kms"
+                  }
+              }
+          ]
+      }
   })
 
 
-def test_bucket_policy_exists(synthesized_template):
-  template = synthesized_template
-  template.resource_count_is("AWS::S3::BucketPolicy", 1)
+def test_bucket_policy_exists(template_fixture):
+  template_fixture.resource_count_is("AWS::S3::BucketPolicy", 1)
 
 
-def test_bucket_policy_denies_http_access(synthesized_template):
-  template = synthesized_template
-  template.has_resource_properties("AWS::S3::BucketPolicy", {
-    "PolicyDocument": {
-      "Statement": [
-        {
-          "Action": "*",
-          "Effect": "Deny",
-          "Principal": "*",
-          "Resource": "*",
-          "Condition": {
-            "Bool": {
-              "aws:SecureTransport": "false"
-            }
-          }
-        }
-      ]
-    }
+def test_bucket_policy_denies_http_access(template_fixture):
+  template_fixture.has_resource_properties("AWS::S3::BucketPolicy", {
+      "PolicyDocument": {
+          "Statement": [
+              {
+                  "Action": "*",
+                  "Effect": "Deny",
+                  "Principal": "*",
+                  "Resource": "*",
+                  "Condition": {
+                      "Bool": {
+                          "aws:SecureTransport": "false"
+                      }
+                  }
+              }
+          ]
+      }
   })
 
 
-def test_stack_outputs_exist(synthesized_template):
-  template = synthesized_template
-  output_keys = template.to_json().get("Outputs", {}).keys()
+def test_stack_outputs_exist(template_fixture):
+  output_keys = template_fixture.to_json().get("Outputs", {}).keys()
   expected = {"BucketName", "BucketArn", "KmsKeyArn"}
   assert expected.issubset(output_keys)
-
