dced7a5223c16fbe620f4351f73c081d4bdd762d
sreeni-ops <sreenivas.k@turing.com>
2025-07-31 10:41:14 +0530

    MultiEnvironmentConsistency_CDK_Python_b84k2njq9r13 v2

--- tests/unit/test_tap_stack.py
+++ tests/unit/test_tap_stack.py
@@ -1,41 +1,50 @@
+import os
+import unittest
 from aws_cdk import App
 from aws_cdk.assertions import Template, Match
-from lib.tap_stack import VPCStack, IAMStack, TapStackProps
+
+from lib.tap_stack import TapStack, TapStackProps
 
 
-def test_vpc_is_created_with_public_and_private_subnets():
-    app = App()
-    stack = VPCStack(app, "TestVpcStack", environment_suffix="test")
-    template = Template.from_stack(stack)
+class TestTapStack(unittest.TestCase):
 
-    template.resource_count_is("AWS::EC2::VPC", 1)
+    def setUp(self):
+        # Read from env vars, fallback to defaults
+        self.env_suffix = os.getenv("TEST_ENV", "dev")
+        self.region = os.getenv("TEST_REGION", "us-east-1")
+        self.account = os.getenv("TEST_ACCOUNT", "111111111111")
+        self.app = App(context={"environmentSuffix": self.env_suffix})
 
+    def test_vpc_is_created_with_public_and_private_subnets(self):
+        stack = TapStack(self.app, "TestVpcStack", TapStackProps(environment_suffix=self.env_suffix))
+        template = Template.from_stack(stack)
+        template.resource_count_is("AWS::EC2::VPC", 1)
 
-def test_iam_role_is_created_with_ec2_read_only_policy():
-    app = App()
-    stack = IAMStack(app, "TestIamStack", environment_suffix="test")
-    template = Template.from_stack(stack)
-
-    template.resource_count_is("AWS::IAM::Role", 1)
-
-    template.has_resource_properties("AWS::IAM::Role", {
-        "AssumeRolePolicyDocument": Match.object_like({
-            "Statement": Match.array_with([
-                Match.object_like({
-                    "Principal": Match.object_like({
-                        "Service": "ec2.amazonaws.com"
+    def test_iam_role_is_created_with_ec2_read_only_policy(self):
+        stack = TapStack(self.app, "TestIamStack", TapStackProps(environment_suffix=self.env_suffix))
+        template = Template.from_stack(stack)
+        template.resource_count_is("AWS::IAM::Role", 1)
+        template.has_resource_properties("AWS::IAM::Role", {
+            "AssumeRolePolicyDocument": Match.object_like({
+                "Statement": Match.array_with([
+                    Match.object_like({
+                        "Principal": {
+                            "Service": "ec2.amazonaws.com"
+                        }
                     })
+                ])
+            }),
+            "Policies": Match.array_with([
+                Match.object_like({
+                    "PolicyName": "CustomEC2ReadOnlyPolicy",
                 })
             ])
         })
-    })
 
-
-def test_environment_suffix_default_to_dev():
-    app = App()
-    stack = IAMStack(app, "TestDefaultEnvStack", environment_suffix="dev")
-    template = Template.from_stack(stack)
-
-    template.has_resource_properties("AWS::IAM::Role", {
-        "RoleName": Match.string_like_regexp(".*dev.*")
-    })
+    def test_environment_suffix_default_to_dev(self):
+        app = App()  # no context, should fall back to "dev"
+        stack = TapStack(app, "TestDefaultEnvStack")
+        template = Template.from_stack(stack)
+        template.has_resource_properties("AWS::IAM::Role", {
+            "RoleName": Match.string_like_regexp(".*dev.*")
+        })
