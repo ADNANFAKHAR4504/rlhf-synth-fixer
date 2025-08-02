ba0f9aa394f2997638132dc4dba7c44daa5e491e
David Michael <davidlearn27@gmail.com>
2025-08-02 16:29:59 +0100

    Update TapStack AWS region and state bucket region to 'eu-central-1' for improved deployment alignment

--- lib/tap_stack.py
+++ lib/tap_stack.py
@@ -31,8 +31,10 @@ class TapStack(TerraformStack):
 
     # Extract configuration from kwargs
     environment_suffix = kwargs.get('environment_suffix', 'dev')
-    aws_region = kwargs.get('aws_region', 'us-east-1')
-    state_bucket_region = kwargs.get('state_bucket_region', 'us-east-1')
+    # aws_region = kwargs.get('aws_region', 'us-east-1')
+    # state_bucket_region = kwargs.get('state_bucket_region', 'us-east-1')
+    aws_region = "eu-central-1"
+    state_bucket_region = "eu-central-1"
     state_bucket = kwargs.get('state_bucket', 'iac-rlhf-tf-states')
     default_tags = kwargs.get('default_tags', {})
     bucket_prefix = kwargs.get('bucket_prefix', 'secure-data')
