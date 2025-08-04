60b3c620239927133aa998319236477472f62814
nikhilturing <nikhil.s2@turing.com>
2025-08-04 16:12:05 +0530

    Fix deployment issue

--- lib/tap-stack.ts
+++ lib/tap-stack.ts
@@ -32,8 +32,8 @@ export class TapStack extends TerraformStack {
     super(scope, id);
 
     const environmentSuffix = props?.environmentSuffix || 'dev';
-    const awsRegion =  props?.awsRegion || 'us-east-1';
-    const stateBucketRegion = props?.stateBucketRegion || 'us-east-1';
+    const awsRegion = props?.awsRegion || 'us-west-2';
+    const stateBucketRegion = props?.stateBucketRegion || 'us-west-2';
     const stateBucket = props?.stateBucket || 'iac-rlhf-tf-states';
     const defaultTags = props?.defaultTags ? [props.defaultTags] : [];
 
