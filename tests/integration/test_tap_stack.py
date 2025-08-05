2400a13bf1f93b298ffff88e3f885c23f5d4ec30
Tahir <tahirawan7892@gmail.com>
2025-08-05 17:58:03 +0500

    integration code

--- tests/integration/test_tap_stack.py
+++ tests/integration/test_tap_stack.py
@@ -7,13 +7,11 @@ from pulumi import automation as auto
 from lib import tap_stack
 
 
-
 class TestTapStackLiveIntegration(unittest.TestCase):
-  """Integration tests against live deployed Pulumi stack."""
+  """Basic integration tests for 6 core secure infrastructure components."""
 
   @classmethod
   def setUpClass(cls):
-    """Set up Pulumi stack and AWS clients."""
     cls.stack_name = os.getenv("PULUMI_STACK", "dev")
     cls.project_name = os.getenv("PULUMI_PROJECT", "pulumi-infra")
     cls.region = os.getenv("AWS_REGION", "us-east-1")
@@ -26,64 +24,42 @@ class TestTapStackLiveIntegration(unittest.TestCase):
       return tap_stack.TapStack(
         "TapStack",
         tap_stack.TapStackArgs(environment_suffix=cls.stack_name)
-        )
+      )
 
-    try:
-      cls.stack = auto.select_stack(
-        stack_name=cls.stack_name,
-        project_name=cls.project_name,
-        program=pulumi_program,
-        )
-    except auto.StackNotFoundError:
-      cls.stack = auto.create_stack(
-        stack_name=cls.stack_name,
-        project_name=cls.project_name,
-        program=pulumi_program,
-        )
-      cls.stack.set_config("aws:region", auto.ConfigValue(value=cls.region))
-      cls.stack.set_config("TapStack:db_password",
-                            auto.ConfigValue(value="dummy-password", secret=True))
+    cls.stack = auto.select_stack(
+      stack_name=cls.stack_name,
+      project_name=cls.project_name,
+      program=pulumi_program
+    )
 
-    # Ensure workspace env vars
-    cls.stack.workspace.env_vars["AWS_REGION"] = cls.region
-    cls.stack.workspace.env_vars["PULUMI_BACKEND_URL"] = cls.s3_backend
-
-    # Refresh to avoid duplicate errors if resources already exist
     cls.stack.refresh(on_output=print)
 
-    # Deploy stack (no-op if already up-to-date)
-    cls.stack.up(on_output=print)
-
-    # Safe output extraction
     outputs = cls.stack.outputs()
-    cls.outputs = outputs
-    cls.vpc_id = outputs.get("vpc_id", auto.Output.from_input(None)).value
-    cls.sg_id = outputs.get("security_group_id", auto.Output.from_input(None)).value
-    cls.user_arn = outputs.get("iam_user_arn", auto.Output.from_input(None)).value
-    cls.access_key_id = outputs.get("access_key_id", auto.Output.from_input(None)).value
-    cls.kms_key_id = outputs.get("kms_key_id", auto.Output.from_input(None)).value
-    cls.kms_alias = outputs.get("kms_alias", auto.Output.from_input("")).value
-    cls.encrypted_blob = outputs.get("encrypted_db_password_blob", auto.Output.from_input("")).value
+    cls.vpc_id = outputs["vpc_id"].value
+    cls.sg_id = outputs["security_group_id"].value
+    cls.user_arn = outputs["iam_user_arn"].value
+    cls.access_key_id = outputs["access_key_id"].value
+    cls.kms_key_id = outputs["kms_key_id"].value
+    cls.encrypted_blob = outputs["encrypted_db_password_blob"].value
 
-    # AWS clients
     cls.ec2 = boto3.client("ec2", region_name=cls.region)
     cls.iam = boto3.client("iam", region_name=cls.region)
     cls.kms = boto3.client("kms", region_name=cls.region)
 
   def test_vpc_exists(self):
-    response = self.ec2.describe_vpcs(VpcIds=[self.vpc_id])
-    self.assertEqual(response["Vpcs"][0]["State"], "available")
+    vpcs = self.ec2.describe_vpcs(VpcIds=[self.vpc_id])["Vpcs"]
+    self.assertEqual(vpcs[0]["State"], "available")
 
   def test_security_group_exists(self):
-    response = self.ec2.describe_security_groups(GroupIds=[self.sg_id])
-    self.assertEqual(len(response["SecurityGroups"]), 1)
+    sgs = self.ec2.describe_security_groups(GroupIds=[self.sg_id])["SecurityGroups"]
+    self.assertEqual(len(sgs), 1)
 
   def test_iam_user_exists(self):
     username = self.user_arn.split("/")[-1]
-    response = self.iam.get_user(UserName=username)
-    self.assertEqual(response["User"]["Arn"], self.user_arn)
+    user = self.iam.get_user(UserName=username)["User"]
+    self.assertEqual(user["Arn"], self.user_arn)
 
-  def test_access_key_active(self):
+  def test_access_key_exists(self):
     username = self.user_arn.split("/")[-1]
     keys = self.iam.list_access_keys(UserName=username)["AccessKeyMetadata"]
     key_ids = [k["AccessKeyId"] for k in keys]
@@ -92,7 +68,6 @@ class TestTapStackLiveIntegration(unittest.TestCase):
   def test_kms_key_is_enabled(self):
     key = self.kms.describe_key(KeyId=self.kms_key_id)["KeyMetadata"]
     self.assertTrue(key["Enabled"])
-    self.assertIn("secure-web-key", self.kms_alias)
 
   def test_encrypted_blob_is_non_empty(self):
     self.assertIsInstance(self.encrypted_blob, str)
