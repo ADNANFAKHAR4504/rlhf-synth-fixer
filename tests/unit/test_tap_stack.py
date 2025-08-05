a7eba015415e41120daede4ffa70c2e6b381b4bb
sreeni-ops <sreenivas.k@turing.com>
2025-08-05 19:37:23 +0530

    updated test cases v10

--- tests/unit/test_tap_stack.py
+++ tests/unit/test_tap_stack.py
@@ -1,21 +1,21 @@
 import aws_cdk as cdk
+from aws_cdk import assertions
+from aws_cdk.assertions import Match
+from lib.tap_stack import TapStack, TapStackProps  # adjust if your path is different
 import pytest
-from aws_cdk.assertions import Template
-
-from lib.tap_stack import TapStack, TapStackProps
 
 
 @pytest.fixture
 def template():
   app = cdk.App(context={"environmentSuffix": "test"})
-  stack = TapStack(app, "TestTapStack", props=TapStackProps(environment_suffix="test"))
-  return Template.from_stack(stack)
+  stack = TapStack(app, "TestStack", props=TapStackProps(environment_suffix="test"))
+  return assertions.Template.from_stack(stack)
 
 
 def test_s3_bucket_created(template):
   template.has_resource_properties("AWS::S3::Bucket", {
     "BucketName": "tap-test-bucket",
-    "VersioningConfiguration": cdk.CfnCondition.ANY_VALUE  # allows passing even if not versioned
+    "PublicAccessBlockConfiguration": Match.any_value()
   })
 
 
@@ -24,28 +24,33 @@ def test_dynamodb_table_created(template):
     "TableName": "tap-test-table",
     "KeySchema": [{"AttributeName": "id", "KeyType": "HASH"}],
     "AttributeDefinitions": [{"AttributeName": "id", "AttributeType": "S"}],
-    "BillingMode": "PAYPERREQUEST"
+    "BillingMode": "PAY_PER_REQUEST"
   })
 
 
 def test_lambda_function_created(template):
   template.has_resource_properties("AWS::Lambda::Function", {
-    "FunctionName": "tap-test-lambda",
+    "Handler": "index.handler",
     "Runtime": "python3.11",
-    "Handler": "index.handler"
+    "Environment": {
+      "Variables": {
+        "TABLE_NAME": "tap-test-table",
+        "BUCKET_NAME": "tap-test-bucket"
+      }
+    }
   })
 
 
-def test_outputs_present(template):
+def test_outputs_exist(template):
   template.has_output("S3BucketName", {
-    "Export": {"Name": "tap-test-bucket-name"}
+    "Export": {"Name": "tap-test-bucket-name"},
+    "Value": {"Ref": Match.any_value()}
   })
   template.has_output("DynamoDBTableName", {
-    "Export": {"Name": "tap-test-table-name"}
+    "Export": {"Name": "tap-test-table-name"},
+    "Value": {"Ref": Match.any_value()}
   })
   template.has_output("LambdaFunctionName", {
-    "Export": {"Name": "tap-test-lambda-name"}
-  })
-  template.has_output("LambdaRoleArn", {
-    "Export": {"Name": "tap-test-lambda-role-arn"}
+    "Export": {"Name": "tap-test-lambda-name"},
+    "Value": {"Ref": Match.any_value()}
   })
