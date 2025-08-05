329d45e9d0eef040081a7e11807a8681b0d3737f
sudhakar01 <sudhakarsingh23@gmail.com>
2025-08-04 23:54:00 -0600

    Security_Configuration_as_Code_CloudFormation_YAML_jd93vkmsl28s

--- test/tap-stack.unit.test.ts
+++ test/tap-stack.unit.test.ts
@@ -218,7 +218,7 @@ describe('Secure Infrastructure CloudFormation Template', () => {
       expect(output.Description).toBe('ID of the created VPC');
       expect(output.Value).toEqual({ Ref: 'SecureVPC' });
       expect(output.Export.Name).toEqual({
-        'Fn::Sub': `${'${AWS::StackName}'}-${outputKey.replace(/Id$/, 'ID')}`,
+        'Fn::Sub': '${AWS::StackName}-VPC-ID',
       });
     });
 
