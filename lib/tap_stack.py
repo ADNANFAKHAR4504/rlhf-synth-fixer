bf1ed79268cfe443050b533dc451429a2dcdf757
Tahir <tahirawan7892@gmail.com>
2025-08-05 18:36:02 +0500

    update code

--- lib/tap_stack.py
+++ lib/tap_stack.py
@@ -90,16 +90,18 @@ class TapStack(pulumi.ComponentResource):
     # --------------------------------------------------------
     user_name = f"secure-web-app-user-{env}"
 
+
     try:
       existing_user = aws.iam.get_user_output(user_name=user_name)
       user = aws.iam.User.get(f"existing-user-{env}", id=user_name)
-    except Exception:
+    except:
       user = aws.iam.User(
         f"web-app-user-{env}",
         name=user_name,
         path="/applications/",
         tags={**tags, "Application": "secure-web-app"}
       )
+
 
     def rotation_policy(days: int) -> str:
       return json.dumps({
