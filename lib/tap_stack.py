5abb38b7850e65bbdf94582b7007ff2049835300
Pradeep Chauhan <pradeep.chauhan67@gmail.com>
2025-08-06 20:38:02 +0530

    fixed integration test live

--- lib/tap_stack.py
+++ lib/tap_stack.py
@@ -390,7 +390,7 @@ def create_load_balancer(
     health_check=aws.lb.TargetGroupHealthCheckArgs(
       enabled=True,
       healthy_threshold=2,
-      unhealthy_threshold=3,
+      unhealthy_threshold=5,
       timeout=10,
       interval=30,
       path="/",
