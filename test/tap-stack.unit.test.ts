d6788084000ad3bb986bb3f25ef5cc91691cab02
Barun Mishra <barun@getcarnera.com>
2025-08-07 14:56:26 +0530

    fix: updated test case

--- test/tap-stack.unit.test.ts
+++ test/tap-stack.unit.test.ts
@@ -7,7 +7,7 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
   let template: any;
 
   beforeAll(() => {
-    // Load the JSON version of the template
+    // Load the JSON version of the template for testing
     const templatePath = path.join(__dirname, '../lib/TapStack.json');
     const templateContent = fs.readFileSync(templatePath, 'utf8');
     template = JSON.parse(templateContent);
@@ -33,7 +33,9 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
       const envParam = template.Parameters.Environment;
       expect(envParam.Type).toBe('String');
       expect(envParam.Default).toBe('prod');
-      expect(envParam.Description).toBe('Environment name for resource tagging and naming');
+      expect(envParam.Description).toBe(
+        'Environment name for resource tagging and naming'
+      );
       expect(envParam.AllowedValues).toEqual(['dev', 'staging', 'prod']);
     });
   });
@@ -54,10 +56,10 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
     test('should have public and private subnets', () => {
       expect(template.Resources.PublicSubnet).toBeDefined();
       expect(template.Resources.PrivateSubnet).toBeDefined();
-      
+
       const publicSubnet = template.Resources.PublicSubnet.Properties;
       const privateSubnet = template.Resources.PrivateSubnet.Properties;
-      
+
       expect(publicSubnet.CidrBlock).toBe('10.0.1.0/24');
       expect(privateSubnet.CidrBlock).toBe('10.0.2.0/24');
       expect(publicSubnet.MapPublicIpOnLaunch).toBe(true);
@@ -86,20 +88,22 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
     test('MyCrudTable should have correct properties', () => {
       const table = template.Resources.MyCrudTable.Properties;
       expect(table.TableName).toEqual({
-        'Fn::Sub': 'MyCrudTable${Environment}'
+        'Fn::Sub': 'MyCrudTable${Environment}',
       });
       expect(table.BillingMode).toBe('ON_DEMAND');
       expect(table.DeletionProtectionEnabled).toBe(false);
-      expect(table.PointInTimeRecoverySpecification.PointInTimeRecoveryEnabled).toBe(true);
+      expect(
+        table.PointInTimeRecoverySpecification.PointInTimeRecoveryEnabled
+      ).toBe(true);
     });
 
     test('MyCrudTable should have correct schema', () => {
       const table = template.Resources.MyCrudTable.Properties;
-      
+
       expect(table.AttributeDefinitions).toHaveLength(1);
       expect(table.AttributeDefinitions[0].AttributeName).toBe('id');
       expect(table.AttributeDefinitions[0].AttributeType).toBe('S');
-      
+
       expect(table.KeySchema).toHaveLength(1);
       expect(table.KeySchema[0].AttributeName).toBe('id');
       expect(table.KeySchema[0].KeyType).toBe('HASH');
@@ -109,15 +113,17 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
   describe('Lambda Functions', () => {
     const lambdaFunctions = [
       'CreateItemFunction',
-      'GetItemFunction', 
+      'GetItemFunction',
       'UpdateItemFunction',
-      'DeleteItemFunction'
+      'DeleteItemFunction',
     ];
 
     lambdaFunctions.forEach(functionName => {
       test(`should have ${functionName}`, () => {
         expect(template.Resources[functionName]).toBeDefined();
-        expect(template.Resources[functionName].Type).toBe('AWS::Lambda::Function');
+        expect(template.Resources[functionName].Type).toBe(
+          'AWS::Lambda::Function'
+        );
       });
 
       test(`${functionName} should have correct runtime and configuration`, () => {
@@ -131,21 +137,25 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
       test(`${functionName} should be in VPC private subnet`, () => {
         const lambdaFunc = template.Resources[functionName].Properties;
         expect(lambdaFunc.VpcConfig).toBeDefined();
-        expect(lambdaFunc.VpcConfig.SecurityGroupIds).toEqual([{
-          Ref: 'LambdaSecurityGroup'
-        }]);
-        expect(lambdaFunc.VpcConfig.SubnetIds).toEqual([{
-          Ref: 'PrivateSubnet'
-        }]);
+        expect(lambdaFunc.VpcConfig.SecurityGroupIds).toEqual([
+          {
+            Ref: 'LambdaSecurityGroup',
+          },
+        ]);
+        expect(lambdaFunc.VpcConfig.SubnetIds).toEqual([
+          {
+            Ref: 'PrivateSubnet',
+          },
+        ]);
       });
 
       test(`${functionName} should have environment variables`, () => {
         const lambdaFunc = template.Resources[functionName].Properties;
         expect(lambdaFunc.Environment.Variables.TABLE_NAME).toEqual({
-          Ref: 'MyCrudTable'
+          Ref: 'MyCrudTable',
         });
         expect(lambdaFunc.Environment.Variables.ENVIRONMENT).toEqual({
-          Ref: 'Environment'
+          Ref: 'Environment',
         });
       });
     });
@@ -155,8 +165,8 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
     const iamRoles = [
       'CreateItemRole',
       'GetItemRole',
-      'UpdateItemRole', 
-      'DeleteItemRole'
+      'UpdateItemRole',
+      'DeleteItemRole',
     ];
 
     iamRoles.forEach(roleName => {
@@ -175,20 +185,20 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
       test(`${roleName} should have specific DynamoDB permissions`, () => {
         const role = template.Resources[roleName].Properties;
         expect(role.Policies).toHaveLength(1);
-        
+
         const policy = role.Policies[0];
         expect(policy.PolicyDocument.Statement[0].Resource).toEqual({
-          'Fn::GetAtt': ['MyCrudTable', 'Arn']
+          'Fn::GetAtt': ['MyCrudTable', 'Arn'],
         });
-        
+
         // Check specific permissions based on role
         const expectedActions: { [key: string]: string[] } = {
-          'CreateItemRole': ['dynamodb:PutItem'],
-          'GetItemRole': ['dynamodb:GetItem'],
-          'UpdateItemRole': ['dynamodb:UpdateItem'],
-          'DeleteItemRole': ['dynamodb:DeleteItem']
+          CreateItemRole: ['dynamodb:PutItem'],
+          GetItemRole: ['dynamodb:GetItem'],
+          UpdateItemRole: ['dynamodb:UpdateItem'],
+          DeleteItemRole: ['dynamodb:DeleteItem'],
         };
-        
+
         expect(policy.PolicyDocument.Statement[0].Action).toEqual(
           expectedActions[roleName]
         );
@@ -199,14 +209,18 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
   describe('API Gateway', () => {
     test('should have REST API', () => {
       expect(template.Resources.MyRestApi).toBeDefined();
-      expect(template.Resources.MyRestApi.Type).toBe('AWS::ApiGateway::RestApi');
+      expect(template.Resources.MyRestApi.Type).toBe(
+        'AWS::ApiGateway::RestApi'
+      );
     });
 
     test('should have items resource and item resource with path parameter', () => {
       expect(template.Resources.ItemsResource).toBeDefined();
       expect(template.Resources.ItemResource).toBeDefined();
-      
-      expect(template.Resources.ItemsResource.Properties.PathPart).toBe('items');
+
+      expect(template.Resources.ItemsResource.Properties.PathPart).toBe(
+        'items'
+      );
       expect(template.Resources.ItemResource.Properties.PathPart).toBe('{id}');
     });
 
@@ -214,25 +228,32 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
       const methods = [
         'CreateItemMethod',
         'GetItemMethod',
-        'UpdateItemMethod', 
-        'DeleteItemMethod'
+        'UpdateItemMethod',
+        'DeleteItemMethod',
       ];
-      
+
       const expectedHttpMethods = ['POST', 'GET', 'PUT', 'DELETE'];
-      
+
       methods.forEach((method, index) => {
         expect(template.Resources[method]).toBeDefined();
         expect(template.Resources[method].Type).toBe('AWS::ApiGateway::Method');
-        expect(template.Resources[method].Properties.HttpMethod).toBe(expectedHttpMethods[index]);
-        expect(template.Resources[method].Properties.Integration.Type).toBe('AWS_PROXY');
+        expect(template.Resources[method].Properties.HttpMethod).toBe(
+          expectedHttpMethods[index]
+        );
+        expect(template.Resources[method].Properties.Integration.Type).toBe(
+          'AWS_PROXY'
+        );
       });
     });
 
     test('should have OPTIONS methods for CORS', () => {
       expect(template.Resources.ItemsOptionsMethod).toBeDefined();
       expect(template.Resources.ItemOptionsMethod).toBeDefined();
-      
-      [template.Resources.ItemsOptionsMethod, template.Resources.ItemOptionsMethod].forEach(optionsMethod => {
+
+      [
+        template.Resources.ItemsOptionsMethod,
+        template.Resources.ItemOptionsMethod,
+      ].forEach(optionsMethod => {
         expect(optionsMethod.Properties.HttpMethod).toBe('OPTIONS');
         expect(optionsMethod.Properties.Integration.Type).toBe('MOCK');
       });
@@ -241,8 +262,10 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
     test('should have deployment and stage', () => {
       expect(template.Resources.ApiDeployment).toBeDefined();
       expect(template.Resources.ApiStage).toBeDefined();
-      
-      expect(template.Resources.ApiDeployment.Type).toBe('AWS::ApiGateway::Deployment');
+
+      expect(template.Resources.ApiDeployment.Type).toBe(
+        'AWS::ApiGateway::Deployment'
+      );
       expect(template.Resources.ApiStage.Type).toBe('AWS::ApiGateway::Stage');
     });
   });
@@ -252,14 +275,16 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
       'CreateItemPermission',
       'GetItemPermission',
       'UpdateItemPermission',
-      'DeleteItemPermission'
+      'DeleteItemPermission',
     ];
 
     permissions.forEach(permission => {
       test(`should have ${permission} for API Gateway`, () => {
         expect(template.Resources[permission]).toBeDefined();
-        expect(template.Resources[permission].Type).toBe('AWS::Lambda::Permission');
-        
+        expect(template.Resources[permission].Type).toBe(
+          'AWS::Lambda::Permission'
+        );
+
         const perm = template.Resources[permission].Properties;
         expect(perm.Action).toBe('lambda:InvokeFunction');
         expect(perm.Principal).toBe('apigateway.amazonaws.com');
@@ -270,15 +295,21 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
   describe('Security Group', () => {
     test('should have Lambda security group with least privilege', () => {
       expect(template.Resources.LambdaSecurityGroup).toBeDefined();
-      expect(template.Resources.LambdaSecurityGroup.Type).toBe('AWS::EC2::SecurityGroup');
-      
+      expect(template.Resources.LambdaSecurityGroup.Type).toBe(
+        'AWS::EC2::SecurityGroup'
+      );
+
       const sg = template.Resources.LambdaSecurityGroup.Properties;
       expect(sg.SecurityGroupEgress).toHaveLength(2);
-      
+
       // Check HTTPS and HTTP outbound rules
-      const httpsRule = sg.SecurityGroupEgress.find((rule: any) => rule.FromPort === 443);
-      const httpRule = sg.SecurityGroupEgress.find((rule: any) => rule.FromPort === 80);
-      
+      const httpsRule = sg.SecurityGroupEgress.find(
+        (rule: any) => rule.FromPort === 443
+      );
+      const httpRule = sg.SecurityGroupEgress.find(
+        (rule: any) => rule.FromPort === 80
+      );
+
       expect(httpsRule).toBeDefined();
       expect(httpRule).toBeDefined();
       expect(httpsRule.ToPort).toBe(443);
@@ -292,7 +323,8 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
       const output = template.Outputs.ApiGatewayInvokeURL;
       expect(output.Description).toContain('REST API');
       expect(output.Value).toEqual({
-        'Fn::Sub': 'https://${MyRestApi}.execute-api.${AWS::Region}.amazonaws.com/${Environment}'
+        'Fn::Sub':
+          'https://${MyRestApi}.execute-api.${AWS::Region}.amazonaws.com/${Environment}',
       });
     });
 
@@ -306,11 +338,11 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
     test('should have VPC infrastructure outputs', () => {
       const infrastructureOutputs = [
         'VPCId',
-        'PrivateSubnetId', 
+        'PrivateSubnetId',
         'PublicSubnetId',
-        'LambdaSecurityGroupId'
+        'LambdaSecurityGroupId',
       ];
-      
+
       infrastructureOutputs.forEach(outputName => {
         expect(template.Outputs[outputName]).toBeDefined();
         expect(template.Outputs[outputName].Description).toBeDefined();
@@ -339,14 +371,21 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
     });
 
     test('Lambda functions should have proper Python code embedded', () => {
-      const lambdaFunctions = ['CreateItemFunction', 'GetItemFunction', 'UpdateItemFunction', 'DeleteItemFunction'];
-      
+      const lambdaFunctions = [
+        'CreateItemFunction',
+        'GetItemFunction',
+        'UpdateItemFunction',
+        'DeleteItemFunction',
+      ];
+
       lambdaFunctions.forEach(functionName => {
         const lambdaFunc = template.Resources[functionName].Properties;
         expect(lambdaFunc.Code.ZipFile).toContain('import json');
         expect(lambdaFunc.Code.ZipFile).toContain('import boto3');
         expect(lambdaFunc.Code.ZipFile).toContain('lambda_handler');
-        expect(lambdaFunc.Code.ZipFile).toContain('Access-Control-Allow-Origin');
+        expect(lambdaFunc.Code.ZipFile).toContain(
+          'Access-Control-Allow-Origin'
+        );
       });
     });
   });
@@ -356,7 +395,7 @@ describe('TapStack CloudFormation Template - Serverless CRUD API', () => {
       const func = template.Resources.CreateItemFunction.Properties;
       expect(func.Code.ZipFile).toContain('created_at');
       expect(func.Code.ZipFile).toContain('table.put_item');
-      expect(func.Code.ZipFile).toContain('statusCode\': 201');
+      expect(func.Code.ZipFile).toContain("statusCode': 201");
     });
 
     test('GetItemFunction should handle GET operations', () => {
