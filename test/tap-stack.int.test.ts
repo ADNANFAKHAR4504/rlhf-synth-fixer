1de88f1f84d521672bdad51d64255214e75d234f
root <root@DESKTOP-3KTOO66.localdomain>
2025-08-08 22:39:44 +0530

    fixing try catch in intg test

--- test/tap-stack.int.test.ts
+++ test/tap-stack.int.test.ts
@@ -1,5 +1,6 @@
 // Configuration - These are coming from cfn-outputs after cdk deploy
 import fs from 'fs';
+
 // Get environment suffix from environment variable (set by CI/CD pipeline)
 const environmentSuffix = process.env.ENVIRONMENT_SUFFIX || 'dev';
 
@@ -7,7 +8,11 @@ let outputs: any = {};
 let hasDeployedResources = false;
 
 // Try to load outputs if they exist (after deployment)
-@@ -19,16 +19,62 @@ try {
+try {
+  const data = fs.readFileSync(`./cfn-outputs-${environmentSuffix}.json`, 'utf-8');
+  outputs = JSON.parse(data);
+  hasDeployedResources = true;
+} catch (err) {
   console.warn('No deployment outputs found, skipping integration tests');
 }
 
