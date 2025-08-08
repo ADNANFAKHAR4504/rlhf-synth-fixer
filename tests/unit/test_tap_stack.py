a7c02a9907ad8dd7f83df2911b40840439b0e356
sreeni-ops <sreenivas.k@turing.com>
2025-08-08 19:33:58 +0530

    Serverless_Infrastructure_CDK_Python_f1h8z3n0h2j4 v6

--- tests/unit/test_tap_stack.py
+++ tests/unit/test_tap_stack.py
@@ -1,18 +1,39 @@
+import sys
+import os
 import unittest
 import aws_cdk as cdk
 from aws_cdk.assertions import Template, Match
 
+# Add the root project directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
 # Import the actual stack class
-from lib.tap_stack import ServerlessStack
+from lib.serverless_stack import ServerlessStack
 
 
 class TestServerlessStack(unittest.TestCase):
   """Test cases for the ServerlessStack CDK stack"""
 
   def setUp(self):
+    """Initializes a new CDK App and Stack for each test."""
     self.app = cdk.App()
     self.stack = ServerlessStack(self.app, "TestServerlessStack")
     self.template = Template.from_stack(self.stack)
+
+  def test_dynamodb_table_created(self):
+    """Ensure DynamoDB table is created with correct schema and billing mode."""
+    self.template.resource_count_is("AWS::DynamoDB::Table", 1)
+    self.template.has_resource_properties("AWS::DynamoDB::Table", {
+      "KeySchema": [{
+        "AttributeName": "ItemId",
+        "KeyType": "HASH"
+      }],
+      "AttributeDefinitions": [{
+        "AttributeName": "ItemId",
+        "AttributeType": "S"
+      }],
+      "BillingMode": "PROVISIONED"
+    })
 
   def test_lambda_function_created(self):
     """Ensure Lambda function is defined with environment and runtime settings."""
@@ -28,15 +49,74 @@ class TestServerlessStack(unittest.TestCase):
       }
     })
 
+  def test_lambda_execution_role_created(self):
+    """Ensure IAM role for Lambda has correct trust policy and managed policy."""
+    self.template.resource_count_is("AWS::IAM::Role", 1)
+    self.template.has_resource_properties("AWS::IAM::Role", {
+      "AssumeRolePolicyDocument": {
+        "Statement": [{
+          "Action": "sts:AssumeRole",
+          "Effect": "Allow",
+          "Principal": {
+            "Service": "lambda.amazonaws.com"
+          }
+        }]
+      },
+      "ManagedPolicyArns": [{
+        "Fn::Join": [
+          "",
+          [
+            "arn:",
+            {"Ref": "AWS::Partition"},
+            ":iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+          ]
+        ]
+      }]
+    })
+
+  def test_lambda_grants_dynamodb_access(self):
+    """Test that the IAM role has permissions to access the DynamoDB table."""
+    self.template.has_resource_properties("AWS::IAM::Policy", {
+      "PolicyDocument": {
+        "Statement": [
+          {
+            "Action": [
+              "dynamodb:BatchGetItem",
+              "dynamodb:GetItem",
+              "dynamodb:Scan",
+              "dynamodb:Query",
+              "dynamodb:GetRecords",
+              "dynamodb:BatchWriteItem",
+              "dynamodb:PutItem",
+              "dynamodb:UpdateItem",
+              "dynamodb:DeleteItem"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+              {"Fn::GetAtt": [Match.any_value(), "Arn"]},
+              {
+                "Fn::Join": [
+                  "",
+                  [{"Fn::GetAtt": [Match.any_value(), "Arn"]}, "/*"]
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    })
+
   def test_autoscaling_targets_created(self):
     """Check that DynamoDB read/write autoscaling is configured."""
     self.template.resource_count_is("AWS::ApplicationAutoScaling::ScalableTarget", 2)
+
     self.template.has_resource_properties("AWS::ApplicationAutoScaling::ScalableTarget", {
       "MinCapacity": 1,
       "MaxCapacity": 1000,
       "ScalableDimension": "dynamodb:table:ReadCapacityUnits",
       "ServiceNamespace": "dynamodb"
     })
+
     self.template.has_resource_properties("AWS::ApplicationAutoScaling::ScalableTarget", {
       "MinCapacity": 1,
       "MaxCapacity": 1000,
@@ -45,23 +125,29 @@ class TestServerlessStack(unittest.TestCase):
     })
 
   def test_cloudwatch_dashboard_created(self):
-    """Ensure a CloudWatch dashboard is defined."""
+    """Ensure a CloudWatch dashboard is defined with the correct name."""
     self.template.resource_count_is("AWS::CloudWatch::Dashboard", 1)
     self.template.has_resource_properties("AWS::CloudWatch::Dashboard", {
-      "DashboardName": "ServerlessMonitoringDashboard"
+      "DashboardName": "TestServerlessStack-ServerlessMonitoringDashboardV3"
     })
 
   def test_cfn_outputs_exist(self):
-    """Ensure CFN Outputs are created for Lambda, DynamoDB, and Dashboard."""
+    """Ensure CFN Outputs are created for Lambda, DynamoDB, and Dashboard with updated export names."""
     self.template.has_output("DynamoDBTableName", {
       "Value": {"Ref": Match.any_value()},
-      "Export": {"Name": "ServerlessStackDynamoDBTableName"}
+      "Export": {"Name": "ServerlessStackV3DynamoDBTableName"}
     })
+
     self.template.has_output("LambdaFunctionName", {
       "Value": {"Ref": Match.any_value()},
-      "Export": {"Name": "ServerlessStackLambdaFunctionName"}
+      "Export": {"Name": "ServerlessStackV3LambdaFunctionName"}
     })
+
     self.template.has_output("CloudWatchDashboardName", {
       "Value": {"Ref": Match.any_value()},
-      "Export": {"Name": "ServerlessStackCloudWatchDashboardName"}
+      "Export": {"Name": "ServerlessStackV3CloudWatchDashboardName"}
     })
+
+
+if __name__ == "__main__":
+  unittest.main()
