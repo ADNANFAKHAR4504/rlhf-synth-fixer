94f5f69517ab01d47e65de76e6323721232adb71
hernanmog-turing <hernan.m@turing.com>
2025-08-08 14:45:44 -0500

    Delete test-debug2.js

--- test-debug2.js
+++
@@ -1,9 +0,0 @@
-const cdk = require('aws-cdk-lib');
-
-const app = new cdk.App();
-const stack = new cdk.Stack(app, 'TestStack', {
-  env: { region: 'us-east-1' }
-});
-
-console.log('Available AZs:', cdk.Stack.of(stack).availabilityZones);
-console.log('AZ count:', cdk.Stack.of(stack).availabilityZones.length);
