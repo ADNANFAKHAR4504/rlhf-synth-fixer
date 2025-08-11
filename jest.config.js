d080e2af3d3802fdd04ac65f757a226685558ca9
Mikias <mikiaszerihundemissie@gmail.com>
2025-08-11 13:49:52 +0300

    Discarding unwanted changes

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,28 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
-  testMatch: ['**/test/**/*.test.ts'],
+  testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.tsx?$': 'ts-jest',
   },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverageFrom: [
-    'lib/**/*.{ts,js}',
-    '!lib/**/*.d.ts',
+    '<rootDir>/lib/**/*.ts',
+    '!<rootDir>/bin/**/*.ts',
+    '!<rootDir>/**/*.d.ts',
+    '!<rootDir>/**/*.js',
+    '!<rootDir>/**/*.test.ts',
+    '!<rootDir>/node_modules/**',
   ],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html'],
+  coverageReporters: ['text', 'lcov', 'json-summary'],
   coverageThreshold: {
     global: {
       branches: 70,
       functions: 70,
       lines: 70,
-      statements: 70
-    }
-  }
+      statements: 70,
+    },
+  },
+  silent: false,
+  verbose: true,
 };
