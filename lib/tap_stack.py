632f49e4c9baf2df9f109408aedd86c06222264b
Pradeep Chauhan <pradeep.chauhan67@gmail.com>
2025-08-12 00:31:38 +0530

    fixed stack

--- lib/tap_stack.py
+++ lib/tap_stack.py
@@ -59,9 +59,6 @@ def create_infrastructure():
     "public-subnet-1",
     vpc_id=vpc.id,
     cidr_block="10.0.1.0/24",
-    ipv6_cidr_block=vpc.ipv6_cidr_block.apply(
-      lambda cidr: cidr[:-5] + "1::/64" if cidr else None
-    ),
     assign_ipv6_address_on_creation=True,
     availability_zone=f"{region}a",
     tags={"Name": "public-subnet-1"},
@@ -72,9 +69,6 @@ def create_infrastructure():
     "public-subnet-2",
     vpc_id=vpc.id,
     cidr_block="10.0.2.0/24",
-    ipv6_cidr_block=vpc.ipv6_cidr_block.apply(
-      lambda cidr: cidr[:-5] + "2::/64" if cidr else None
-    ),
     assign_ipv6_address_on_creation=True,
     availability_zone=f"{region}b",
     tags={"Name": "public-subnet-2"},
