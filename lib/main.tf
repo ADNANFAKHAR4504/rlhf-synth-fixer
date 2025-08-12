8b770183099b053a2ed006aa17ae6755fe3c6f73
root <root@DESKTOP-69FG5E3.localdomain>
2025-08-12 12:43:56 +0500

    Terraform code for IAC-291208

--- lib/main.tf
+++ lib/main.tf
@@ -2,7 +2,7 @@
 # Variables
 #########################################################
 
-variable "region" {
+variable "aws_region" {
   description = "AWS region"
   type        = string
   default     = "us-west-2"
