f86dca778aae099df4f903bb8ee4ce74cdad7029
Mathew Kadambatt <mathew.k@turing.com>
2025-08-12 21:31:09 +0530

    fix: stac content additon

---
+++ bin/tap.ts
@@ -0,0 +1,17 @@
+// bin/app.ts
+
+#!/usr/bin/env node
+
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { TapStack } from '../lib/tapstack';
+
+const app = new cdk.App();
+
+new TapStack(app, 'TapStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: 'us-west-2'
+  },
+  description: 'Scalable serverless architecture for nova-team development environment'
+});
