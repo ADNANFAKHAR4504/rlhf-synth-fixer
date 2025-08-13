3948ecbafe908644434f6d31aca66742ab1bcd60
Pradeep Chauhan <pradeep.chauhan67@gmail.com>
2025-08-13 19:27:33 +0530

    fixed stack

--- lib/tap_stack.py
+++ lib/tap_stack.py
@@ -12,7 +12,7 @@ config = pulumi.Config()
 ENVIRONMENT = config.get("environment") or "dev"
 AWS_REGION = "us-west-2"
 INSTANCE_TYPE = "t3.micro"
-PROJECT_NAME = "dswa-v5"
+PROJECT_NAME = "dswa-v6"
 
 DEPLOYMENT_ID = str(int(time.time()))[-4:]
 
