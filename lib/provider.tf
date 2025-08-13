f51940f45a16e9bc0d61c9b4ad86e91e51f1dab7
root <root@DESKTOP-69FG5E3.localdomain>
2025-08-13 18:06:03 +0500

    Terraform code Third

--- lib/provider.tf
+++ lib/provider.tf
@@ -16,7 +16,7 @@ terraform {
   backend "s3" {
     bucket         = "iac-rlhf-tf-states"      # Central S3 bucket for all TF states
     key            = "global/app-http-https-sg/terraform.tfstate"  # Path in bucket
-    region         = "us-east-1"              # Region of S3 bucket
+    region         = "us-west-2"              # Region of S3 bucket
     encrypt        = true                      # Enable server-side encryption
    # use_lockfile   = false                     # Disable state locking (set to true and add dynamodb_table for production)
     # Note: State locking is disabled to avoid DynamoDB dependency. For production, create a DynamoDB table (e.g., 'iac-rlhf-tf-locks') with a partition key 'LockID' (string) and set use_lockfile = true, dynamodb_table = "iac-rlhf-tf-locks".
