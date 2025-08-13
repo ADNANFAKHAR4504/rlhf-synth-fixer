b168c8fb8dbc57c15abc26de9529a473260fc89f
AnilG <anil.g@turing.com>
2025-08-13 18:23:51 +0530

    trigger deployment

--- lib/tap-stack.ts
+++ lib/tap-stack.ts
@@ -11,7 +11,7 @@ export class TapStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: TapStackProps) {
     super(scope, id, props);
 
-    // Get environment suffix from props, context, or use "dev" as default
+    // Get environment suffix from props, context, or use 'dev' as default
     const environmentSuffix =
       props?.environmentSuffix ||
       this.node.tryGetContext('environmentSuffix') ||
