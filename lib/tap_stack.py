c75139a5de4605cb855a1fa1c0790839ff2fe253
sreeni-ops <sreenivas.k@turing.com>
2025-08-13 20:22:59 +0530

    updated unit test v6

--- lib/tap_stack.py
+++ lib/tap_stack.py
@@ -104,7 +104,7 @@ class WebApplicationStack(Stack):
       self, "ASG",
       vpc=vpc,
       vpc_subnets=ec2.SubnetSelection(
-        subnet_type=ec2.SubnetType.PRIVATE
+        subnet_type=ec2.SubnetType.PUBLIC
       ),
       instance_type=ec2.InstanceType.of(
         ec2.InstanceClass.BURSTABLE2,
