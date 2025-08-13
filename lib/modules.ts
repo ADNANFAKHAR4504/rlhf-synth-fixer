c2778225d7489dbc00aff42a32fec863c4a898e6
Sanket Shirode <sanketshirode1994@gmail.com>
2025-08-13 14:27:18 +0530

    changed modules.ts

--- lib/modules.ts
+++ lib/modules.ts
@@ -301,8 +301,8 @@ export class ComputeModule extends Construct {
       'yum install -y httpd',
       'systemctl start httpd',
       'systemctl enable httpd',
-      'echo "<h1>Deployed via CDKTF</h1>" > /var/www/html/index.html',
-      'echo "Instance: $(curl -s http://169.254.169.254/latest/meta-data/instance-id)" >> /var/www/html/index.html',
+      'echo \"<h1>Deployed via CDKTF</h1>\" > /var/www/html/index.html',
+      'echo \"Instance: $(curl -s http://169.254.169.254/latest/meta-data/instance-id)\" >> /var/www/html/index.html',
     ].join('\n');
 
     const launchTemplate = new aws.launchTemplate.LaunchTemplate(
