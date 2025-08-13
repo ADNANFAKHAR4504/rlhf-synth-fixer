b14dcd0a4ba69416db952e387e302bf5a121081c
root <root@DESKTOP-3KTOO66.localdomain>
2025-08-13 23:06:20 +0530

    adding depends on

--- lib/main.tf
+++ lib/main.tf
@@ -203,6 +203,7 @@ resource "aws_iam_role" "ec2_role" {
 resource "aws_iam_role_policy_attachment" "ec2_s3_access" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  depends_on = [aws_iam_role.ec2_role]
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
