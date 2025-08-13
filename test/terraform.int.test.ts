068abff54754b411346f370deb3abe97525c76e5
root <root@DESKTOP-3KTOO66.localdomain>
2025-08-13 21:09:55 +0530

    adding integration test

--- test/terraform.int.test.ts
+++ test/terraform.int.test.ts
@@ -1,7 +1,30 @@
-describe('Turn Around Prompt API Integration Tests', () => {
-  describe('Write Integration TESTS', () => {
-    test('Dont forget!', async () => {
-      expect(false).toBe(true);
+// test/terraform.int.test.ts
+import { execSync } from 'child_process';
+
+describe('Terraform Infrastructure Integration Tests', () => {
+  let tfOutputs: Record<string, any> = {};
+
+  beforeAll(() => {
+    // Get Terraform outputs as JSON
+    const output = execSync('cd lib && terraform output -json', {
+      encoding: 'utf-8',
     });
+    tfOutputs = JSON.parse(output);
+  });
+
+  test('ALB DNS name should exist and be non-empty', () => {
+    expect(tfOutputs).toHaveProperty('alb_dns_name');
+    expect(tfOutputs.alb_dns_name.value).toMatch(/^.+$/); // Not empty
+  });
+
+  test('RDS endpoint should exist and look like a hostname', () => {
+    expect(tfOutputs).toHaveProperty('rds_endpoint');
+    expect(tfOutputs.rds_endpoint.value).toMatch(/^[a-z0-9.-]+$/);
+  });
+
+  test('S3 buckets should have been created', () => {
+    expect(tfOutputs).toHaveProperty('app_data_bucket');
+    expect(tfOutputs.app_data_bucket.value).toMatch(/^[a-z0-9-]+$/);
   });
 });
+
