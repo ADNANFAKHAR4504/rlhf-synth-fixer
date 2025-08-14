32790db892109fb834c9b7c6b68cd57e16739e7a
olatunjym <adeteyelinux@gmail.com>
2025-08-14 15:55:47 +0100

    updated cloudtrail and fix

--- lib/logging.ts
+++ lib/logging.ts
@@ -1,5 +1,7 @@
 import { Cloudtrail } from '@cdktf/provider-aws/lib/cloudtrail';
 import { S3Bucket } from '@cdktf/provider-aws/lib/s3-bucket';
+import { S3BucketPolicy } from '@cdktf/provider-aws/lib/s3-bucket-policy';
+
 import { Construct } from 'constructs';
 
 interface LoggingProps {
@@ -14,13 +16,41 @@ export class Logging extends Construct {
   constructor(scope: Construct, id: string, props: LoggingProps) {
     super(scope, id);
 
-    // Create the S3 bucket for CloudTrail logs
+    const bucketName = `${props.environment}-${props.region}-trail-bucket`;
+
     const trailBucket = new S3Bucket(this, 'CloudTrailBucket', {
-      bucket: `${props.environment}-${props.region}-trail-bucket`,
+      bucket: bucketName,
       tags: {
         ...props.tags,
-        Name: `${props.environment}-${props.region}-trail-bucket`,
+        Name: bucketName,
       },
+    });
+
+    // Add bucket policy for CloudTrail
+    new S3BucketPolicy(this, 'CloudTrailBucketPolicy', {
+      bucket: trailBucket.bucket,
+      policy: JSON.stringify({
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Sid: 'AWSCloudTrailAclCheck',
+            Effect: 'Allow',
+            Principal: { Service: 'cloudtrail.amazonaws.com' },
+            Action: 's3:GetBucketAcl',
+            Resource: `arn:aws:s3:::${bucketName}`,
+          },
+          {
+            Sid: 'AWSCloudTrailWrite',
+            Effect: 'Allow',
+            Principal: { Service: 'cloudtrail.amazonaws.com' },
+            Action: 's3:PutObject',
+            Resource: `arn:aws:s3:::${bucketName}/AWSLogs/*`,
+            Condition: {
+              StringEquals: { 's3:x-amz-acl': 'bucket-owner-full-control' },
+            },
+          },
+        ],
+      }),
     });
 
     // Create the CloudTrail resource
