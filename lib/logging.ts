2723c4cdaadf45ee12610ee045c4d9d5130c328c
olatunjym <adeteyelinux@gmail.com>
2025-08-14 15:36:52 +0100

    updated region due to constraints

--- lib/logging.ts
+++ lib/logging.ts
@@ -1,4 +1,5 @@
 import { Cloudtrail } from '@cdktf/provider-aws/lib/cloudtrail';
+import { S3Bucket } from '@cdktf/provider-aws/lib/s3-bucket';
 import { Construct } from 'constructs';
 
 interface LoggingProps {
@@ -13,9 +14,19 @@ export class Logging extends Construct {
   constructor(scope: Construct, id: string, props: LoggingProps) {
     super(scope, id);
 
+    // Create the S3 bucket for CloudTrail logs
+    const trailBucket = new S3Bucket(this, 'CloudTrailBucket', {
+      bucket: `${props.environment}-${props.region}-trail-bucket`,
+      tags: {
+        ...props.tags,
+        Name: `${props.environment}-${props.region}-trail-bucket`,
+      },
+    });
+
+    // Create the CloudTrail resource
     this.trail = new Cloudtrail(this, 'CloudTrail', {
       name: `${props.environment}-${props.region}-trail`,
-      s3BucketName: `${props.environment}-${props.region}-trail-bucket`,
+      s3BucketName: trailBucket.bucket,
       isMultiRegionTrail: true,
       tags: {
         ...props.tags,
