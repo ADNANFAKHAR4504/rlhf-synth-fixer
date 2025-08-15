5f18d7d43d67e883e4167dba1012a01664cb8873
soumya <misrasoumya05@gmail.com>
2025-08-16 01:34:45 +0530

    codechange

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_version = ">= 1.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = ">= 2.4.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.5.1"
-    }
-  }
-}
-
 variable "environment_suffix" {
   type        = string
   default     = "dev"
