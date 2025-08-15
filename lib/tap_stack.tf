91def43195dfa8fe6eef2a854d1265538a72cc07
Emmanuel Nyachoke <nyachoke.e@turing.com>
2025-08-15 11:04:49 +0300

    aws_region

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -1,3 +1,11 @@
+########################
+# Variables
+########################
+variable "aws_region" {
+  description = "AWS provider region"
+  type        = string
+  default     = "us-east-1"
+}
 # lib/tap_stack.tf
 
 # Data sources for availability zones
