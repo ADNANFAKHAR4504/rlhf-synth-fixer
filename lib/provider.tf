f3a7474ddd4949b4214055f64b069f687742b9a2
alexstan89 <stan.alexandru.dan@gmail.com>
2025-08-15 20:59:07 +0200

    [IAC-291844] Fix

--- lib/provider.tf
+++ lib/provider.tf
@@ -8,6 +8,9 @@
 # ------------------------------------------------------------------------------
 
 terraform {
+
+  backend "s3" {}
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
