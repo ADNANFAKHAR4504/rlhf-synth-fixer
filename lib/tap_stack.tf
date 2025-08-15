408b8aba3e4b0ef767dba3a604de86feadad576b
Pradeep Chauhan <pradeep.chauhan67@gmail.com>
2025-08-15 14:42:42 +0530

    fixed tap stack

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -98,7 +98,7 @@ resource "aws_kms_key" "main" {
   enable_key_rotation     = true
 
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
         Sid    = "Enable IAM User Permissions"
@@ -108,6 +108,21 @@ resource "aws_kms_key" "main" {
         }
         Action   = "kms:*"
         Resource = "*"
+      },
+      {
+        Sid: "Allow CloudWatch Logs to use the key",
+        Effect: "Allow",
+        Principal: {
+          Service: "logs.${var.aws_region}.amazonaws.com"
+        },
+        Action: [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ],
+        Resource: "*"
       }
     ]
   })
