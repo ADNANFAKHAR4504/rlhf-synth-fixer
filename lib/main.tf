e0836a531e135af06e9215ee1daa1c78c89b0395
alexstan89 <stan.alexandru.dan@gmail.com>
2025-08-15 13:58:45 +0200

    [IAC-291302] Fixed account id issue

--- lib/main.tf
+++ lib/main.tf
@@ -37,6 +37,7 @@ data "aws_caller_identity" "current" {}
 # -----------------------------------------------------------------------------
 locals {
   name_prefix = "${var.project_name}-${var.environment}"
+  account_id  = data.aws_caller_identity.current.account_id
 
   common_tags = {
     Project     = var.project_name
