ba927e47f0f1cda385666d44966c034d74bb4ac7
alexstan89 <stan.alexandru.dan@gmail.com>
2025-08-16 11:18:09 +0200

    [IAC-291844] Updated region aliases

--- lib/provider.tf
+++ lib/provider.tf
@@ -1,14 +1,10 @@
 # /-----------------------------------------------------------------------------
 # | Terraform & Provider Configuration
 # |-----------------------------------------------------------------------------
-# |
-# | Defines required providers and configures them for multi-region deployments.
-# | Aliases are used to target specific AWS regions from a single configuration.
-# |
-# ------------------------------------------------------------------------------
 
 terraform {
-
+  # This backend block is required by your CI/CD pipeline.
+  # The configuration is passed in dynamically during initialization.
   backend "s3" {}
 
   required_providers {
@@ -26,20 +22,19 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-# Default provider configuration.
-# This is used for non-regional resources like IAM.
+# Default provider configuration for non-regional resources like IAM.
 provider "aws" {
   region = var.aws_region
 }
 
 # Provider alias for the US East (N. Virginia) region.
 provider "aws" {
-  alias  = "us-east-1"
+  alias  = "us_east_1"
   region = "us-east-1"
 }
 
 # Provider alias for the US West (Oregon) region.
 provider "aws" {
-  alias  = "us-west-2"
+  alias  = "us_west_2"
   region = "us-west-2"
 }
