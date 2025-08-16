ba81f58cd4abc1546777ce75299ab1b3cd164476
Amit kumar <amit.k6@turing.com>
2025-08-16 14:42:02 +0000

    IAC-291749 Security_Configuration_as_Code_Terraform_HCL

--- lib/main.tf
+++ lib/main.tf
@@ -1,9 +1,3 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-west-2"
-}
-
 variable "environment" {
   description = "Environment name"
   type        = string
