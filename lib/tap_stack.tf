dc88bed266ed5de5f4bef4e1824088fbac780745
ngwakoleslieelijah <raditupa@gmail.com>
2025-08-16 20:14:19 +0200

    more specific S3 bucket policy

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -583,27 +583,13 @@ resource "aws_s3_bucket_policy" "alb_logs" {
     Version = "2012-10-17",
     Statement = [
       {
-        Sid    = "AllowLogDelivery",
+        Sid    = "AllowPutObject",
         Effect = "Allow",
         Principal = {
-          Service = "logdelivery.elasticloadbalancing.amazonaws.com"
+          AWS = data.aws_elb_service_account.main.arn
         },
         Action   = "s3:PutObject",
-        Resource = "${aws_s3_bucket.alb_logs.arn}/*",
-        Condition = {
-          StringEquals = {
-            "s3:x-amz-acl" = "bucket-owner-full-control"
-          }
-        }
-      },
-      {
-        Sid    = "AllowLogDeliveryAclCheck",
-        Effect = "Allow",
-        Principal = {
-          Service = "logdelivery.elasticloadbalancing.amazonaws.com"
-        },
-        Action   = "s3:GetBucketAcl",
-        Resource = aws_s3_bucket.alb_logs.arn
+        Resource = "${aws_s3_bucket.alb_logs.arn}/alb-logs/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
       }
     ]
   })
