d9d3f0815a663cb5a6b8442e7594e1b3aa87bf98
Mathew Kadambatt <mathew.k@turing.com>
2025-08-18 03:40:44 +0530

    add changes

--- test/tap-stack.int.test.ts
+++ test/tap-stack.int.test.ts
@@ -14,3 +14,41 @@ describe('Turn Around Prompt API Integration Tests', () => {
     });
   });
 });
+
+describe('TapStack CloudFormation Outputs Integration', () => {
+  test('should have VPCId output', () => {
+    expect(outputs.VPCId).toBeDefined();
+    expect(typeof outputs.VPCId).toBe('string');
+    expect(outputs.VPCId).toMatch(/^vpc-/);
+  });
+
+  test('should have WebServerInstanceId output', () => {
+    expect(outputs.WebServerInstanceId).toBeDefined();
+    expect(typeof outputs.WebServerInstanceId).toBe('string');
+    expect(outputs.WebServerInstanceId).toMatch(/^i-/);
+  });
+
+  test('should have WebServerPublicIP output', () => {
+    expect(outputs.WebServerPublicIP).toBeDefined();
+    expect(typeof outputs.WebServerPublicIP).toBe('string');
+    expect(outputs.WebServerPublicIP).toMatch(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/);
+  });
+
+  test('should have S3BucketName output', () => {
+    expect(outputs.S3BucketName).toBeDefined();
+    expect(typeof outputs.S3BucketName).toBe('string');
+    expect(outputs.S3BucketName).toMatch(/^app-bucket-/);
+  });
+
+  test('should have DatabaseEndpoint output', () => {
+    expect(outputs.DatabaseEndpoint).toBeDefined();
+    expect(typeof outputs.DatabaseEndpoint).toBe('string');
+    expect(outputs.DatabaseEndpoint).toMatch(/\.rds\.amazonaws\.com$/);
+  });
+
+  test('should have DatabasePort output', () => {
+    expect(outputs.DatabasePort).toBeDefined();
+    expect(typeof outputs.DatabasePort).toBe('string');
+    expect(Number(outputs.DatabasePort)).toBeGreaterThan(0);
+  });
+});
