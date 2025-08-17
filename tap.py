3584522e648f3ddddb48e5c733d7896253e42c15
aafaq-turing <aafaq.r@turing.com>
2025-08-17 21:58:42 +0500

    Update AWS_REGION to us-east-1 in configuration files

--- tap.py
+++ tap.py
@@ -36,7 +36,7 @@ props = TapStackProps(
     environment_suffix=environment_suffix,
     env=cdk.Environment(
         account=os.getenv('CDK_DEFAULT_ACCOUNT'),
-        region=os.getenv('CDK_DEFAULT_REGION', 'ap-northeast-1')
+        region=os.getenv('CDK_DEFAULT_REGION', 'us-east-1')
     )
 )
 
