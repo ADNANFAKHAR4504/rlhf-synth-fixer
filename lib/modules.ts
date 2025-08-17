ee579271ad8f1bf4d55208b48e0995e3d3c201c9
tushar-turing8 <dighe.t@turing.com>
2025-08-17 13:49:41 +0530

    fixing arn

--- lib/modules.ts
+++ lib/modules.ts
@@ -533,7 +533,7 @@ export class S3Module extends Construct {
               Service: 'cloudtrail.amazonaws.com',
             },
             Action: 's3:PutObject',
-            Resource: `arn:aws:s3:::${this.cloudtrailBucket.bucket}/AWSLogs/718240086340/*`,
+            Resource: `arn:aws:s3:::${this.cloudtrailBucket.bucket}/AWSLogs/718240086340/AWSCloudTrail/us-east-1/*`,
             Condition: {
               StringEquals: {
                 's3:x-amz-acl': 'bucket-owner-full-control',
