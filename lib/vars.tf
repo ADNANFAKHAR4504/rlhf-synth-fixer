b7155e579bebc44bd0fca3c3820592f5989a022f
jannatul-turing <jannatul.h@turing.com>
2025-08-17 20:10:57 +0530

    IAC-291320-tf

--- lib/vars.tf
+++ lib/vars.tf
@@ -13,19 +13,19 @@ variable "aws_region" {
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "172.31.0.0/16"
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
   description = "CIDR block for public subnet"
   type        = string
-  default     = "172.31.0.0/20"
+  default     = "10.0.1.0/24"
 }
 
 variable "private_subnet_cidr" {
   description = "CIDR block for private subnet"
   type        = string
-  default     = "172.31.16.0/20"
+  default     = "10.0.2.0/24"
 }
 
 variable "ec2_instance_type" {
