5cb0b80121c7328f3cd906aa9c355026356e7d72
ngwakoleslieelijah <raditupa@gmail.com>
2025-08-18 21:35:12 +0200

    updated fixes

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -38,3 +38,18 @@ output "s3_data_bucket_name" {
   description = "Name of the S3 data bucket"
   value       = module.infra.s3_data_bucket_name
 }
+
+output "nat_gateway_id" {
+  description = "ID of the NAT Gateway"
+  value       = module.infra.nat_gateway_id
+}
+
+output "kms_key_arn" {
+  description = "ARN of the KMS key"
+  value       = module.infra.kms_key_arn
+}
+
+output "instance_profile_name" {
+  description = "Name of the EC2 instance profile"
+  value       = module.infra.instance_profile_name
+}
