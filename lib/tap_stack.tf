fc2d64de41d4f2797aaa5b0a1e1907b1e173b156
olatunjym <adeteyelinux@gmail.com>
2025-08-18 19:28:00 +0100

    fix: update ideal response

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -87,40 +87,6 @@ output "dynamodb_table_name" {
 # S3 Bucket
 ########################
 
-/* resource "aws_s3_bucket" "this" {
-  bucket = var.bucket_name
-  tags   = var.bucket_tags
-}
-
-resource "aws_s3_bucket_public_access_block" "this" {
-  bucket                  = aws_s3_bucket.this.id
-  block_public_acls       = true
-  ignore_public_acls      = true
-  block_public_policy     = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_versioning" "this" {
-  bucket = aws_s3_bucket.this.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-########################
-# Outputs
-########################
-
-output "bucket_name" {
-  value = aws_s3_bucket.this.bucket
-}
-
-output "bucket_tags" {
-  value = aws_s3_bucket.this.tags
-}
-*/
-
 resource "random_id" "suffix" {
   byte_length = 4
 }
