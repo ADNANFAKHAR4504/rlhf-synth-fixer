22dca34aac551e35f3ddfd5ae8766cf663667671
jannatul-turing <jannatul.h@turing.com>
2025-08-18 23:30:17 +0530

    IAC-291320-tf

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -19,7 +19,7 @@ module "vpc" {
 }
 
 module "sg" {
-  source      = "./modules/security_groups"
+  source      = "./modules/security_group"
   project     = var.project
   vpc_id      = module.vpc.vpc_id
   common_tags = local.common_tags
