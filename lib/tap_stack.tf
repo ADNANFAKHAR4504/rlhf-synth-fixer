8e4b178abcb46a953a8ed30246dd2936b5da4de0
jannatul-turing <jannatul.h@turing.com>
2025-08-18 23:51:06 +0530

    IAC-291320-tf: Updated unit test code

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -18,7 +18,7 @@ module "vpc" {
   common_tags        = local.common_tags
 }
 
-module "sg" {
+module "security_group" {
   source      = "./modules/security_group"
   project     = var.project
   vpc_id      = module.vpc.vpc_id
