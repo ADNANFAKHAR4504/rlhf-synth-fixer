d8c36460bcf1ba8d98c1259c5f208ace37c3118f
olatunjym <adeteyelinux@gmail.com>
2025-08-19 12:18:23 +0100

    fix: fix as per claude review

--- lib/ec2.tf
+++ lib/ec2.tf
@@ -1,30 +1,30 @@
-########################
-# EC2 Bastion Hosts (Primary and Secondary Regions)
-########################
+# Use data source to lookup latest Amazon Linux 2 AMI in each region
 
-# Dummy/default AMI for CI (Amazon Linux 2, us-east-1 public AMI)
-variable "bastion_ami_primary" {
-  description = "AMI ID for bastion host in primary region"
-  type        = string
-  default     = "ami-0c02fb55956c7d316"
+data "aws_ami" "bastion_primary" {
+  provider = aws.primary
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
 }
 
-# Dummy/default AMI for CI (Amazon Linux 2, us-west-2 public AMI)
-variable "bastion_ami_secondary" {
-  description = "AMI ID for bastion host in secondary region"
-  type        = string
-  default     = "ami-08962a4068733a2b6"
-}
+data "aws_ami" "bastion_secondary" {
+  provider = aws.secondary
+  most_recent = true
+  owners      = ["amazon"]
 
-variable "bastion_instance_type" {
-  description = "Instance type for bastion hosts"
-  type        = string
-  default     = "t3.micro"
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
 }
 
 resource "aws_instance" "bastion_primary" {
   provider = aws.primary
-  ami           = var.bastion_ami_primary
+  ami           = data.aws_ami.bastion_primary.id
   instance_type = var.bastion_instance_type
   subnet_id     = aws_subnet.public_primary_1.id
   vpc_security_group_ids = [aws_security_group.bastion_primary.id]
@@ -40,7 +40,7 @@ resource "aws_instance" "bastion_primary" {
 
 resource "aws_instance" "bastion_secondary" {
   provider = aws.secondary
-  ami           = var.bastion_ami_secondary
+  ami           = data.aws_ami.bastion_secondary.id
   instance_type = var.bastion_instance_type
   subnet_id     = aws_subnet.public_secondary_1.id
   vpc_security_group_ids = [aws_security_group.bastion_secondary.id]
@@ -52,4 +52,10 @@ resource "aws_instance" "bastion_secondary" {
     ManagedBy   = "terraform"
     Project     = "secure-env"
   }
+}
+
+variable "bastion_instance_type" {
+  description = "Instance type for bastion hosts"
+  type        = string
+  default     = "t3.micro"
 }
