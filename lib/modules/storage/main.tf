45b58cad41a999c9bdeecab695ea38f0fadfc5ca
ngwakoleslieelijah <raditupa@gmail.com>
2025-08-20 00:40:20 +0200

    Fix

--- lib/modules/storage/main.tf
+++ lib/modules/storage/main.tf
@@ -5,42 +5,6 @@ resource "aws_kms_key" "main" {
   enable_key_rotation     = true
 }
 
-resource "aws_s3_bucket" "data" {
-  bucket_prefix = "${var.project_name}-data-bucket"
-
-  tags = {
-    Name        = "${var.project_name}-data-bucket"
-    Project     = var.project_name
-    Environment = var.environment
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "data" {
-  bucket = aws_s3_bucket.data.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
-      kms_master_key_id = aws_kms_key.main.arn
-    }
-  }
-}
-
-resource "aws_s3_bucket_versioning" "data" {
-  bucket = aws_s3_bucket.data.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "data" {
-  bucket = aws_s3_bucket.data.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
 
 resource "aws_s3_bucket" "logs" {
   bucket_prefix = "${var.project_name}-logs-bucket"
