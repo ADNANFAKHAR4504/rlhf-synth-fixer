e5a894f0b939661b7297898db3db5bf8a5974946
waqara-turing <waqar.a@turing.com>
2025-08-19 15:09:00 +0500

    nat

--- lib/provider.tf
+++ lib/provider.tf
@@ -4,9 +4,6 @@ terraform {
   required_version = ">= 1.4.0"
 
    
-  # Partial backend config: values are injected at `terraform init` time
-backend "s3" {}
-
 }
   
 # Primary AWS provider for general resources
