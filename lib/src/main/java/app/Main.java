dcba59530c85653191700ba2d35fb45c611121b5
Anthony Oride <oride.a@turing.com>
2025-08-19 17:42:38 +0100

    gradle init

---
+++ lib/src/main/java/app/Main.java
@@ -0,0 +1,40 @@
+package app;
+
+import com.pulumi.Pulumi;
+import com.pulumi.aws.s3.Bucket;
+import com.pulumi.aws.s3.BucketArgs;
+import com.pulumi.aws.s3.BucketVersioning;
+import com.pulumi.aws.s3.BucketVersioningArgs;
+import com.pulumi.aws.s3.inputs.BucketVersioningVersioningConfigurationArgs;
+import com.pulumi.aws.s3.BucketServerSideEncryptionConfiguration;
+import com.pulumi.aws.s3.BucketServerSideEncryptionConfigurationArgs;
+import com.pulumi.aws.s3.inputs.BucketServerSideEncryptionConfigurationRuleArgs;
+import com.pulumi.aws.s3.inputs.BucketServerSideEncryptionConfigurationRuleApplyServerSideEncryptionByDefaultArgs;
+
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Pulumi.run(ctx -> {
+
+            Bucket bucket = new Bucket("java-app-bucket", BucketArgs.builder()
+                    .tags(Map.of("Environment", "development"))
+                    .build());
+
+            new BucketVersioning(bucket.bucket().toString() + "-versioning",
+                    BucketVersioningArgs.builder().bucket(bucket.id()).versioningConfiguration(
+                            BucketVersioningVersioningConfigurationArgs.builder().status("Enabled").build()).build());
+
+            BucketServerSideEncryptionConfiguration encryption = new BucketServerSideEncryptionConfiguration(
+                    bucket.bucket().toString() + "-encryption", BucketServerSideEncryptionConfigurationArgs.builder()
+                    .bucket(bucket.id()).rules(BucketServerSideEncryptionConfigurationRuleArgs.builder()
+                            .applyServerSideEncryptionByDefault(
+                                    BucketServerSideEncryptionConfigurationRuleApplyServerSideEncryptionByDefaultArgs.builder()
+                                            .sseAlgorithm("AES256").build()).build()).build());
+
+
+            ctx.export("bucketName", bucket.id());
+            ctx.export("bucket_encryption", encryption.id());
+        });
+    }
+}
