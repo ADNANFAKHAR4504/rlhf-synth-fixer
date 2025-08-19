c3261a4a49fc0c7e397f87c0f5709ac5b71868f4
sreeni-ops <sreenivas.k@turing.com>
2025-08-19 19:45:30 +0530

    Failure-Recovery-and-High-Availability_CloudFormation_YAML_n7t9f2ac5d0l v9

--- test/tap-stack.unit.test.ts
+++ test/tap-stack.unit.test.ts
@@ -5,7 +5,7 @@ describe('High Availability Web App CloudFormation Template', () => {
   let template: any;
 
   beforeAll(() => {
-    const templatePath = path.join(__dirname, '../lib/high-availability-webapp.json'); // JSON version of the template
+    const templatePath = path.join(__dirname, '../lib/TapStack.json');
     const templateContent = fs.readFileSync(templatePath, 'utf8');
     template = JSON.parse(templateContent);
   });
