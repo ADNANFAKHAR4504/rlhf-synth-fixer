01348239df852c0d0bf5a5f25b81796619e5ab0d
ngwakoleslieelijah <raditupa@gmail.com>
2025-08-20 22:52:05 +0200

    explicit S3 bucket policy and subsequently fixing

--- lib/modules/database/database.tf
+++ lib/modules/database/database.tf
@@ -13,7 +13,7 @@ resource "aws_db_instance" "main" {
   allocated_storage      = 20
   engine                 = "mysql"
   instance_class         = "db.t3.micro"
-  db_name                = "${var.project_name}db"
+  db_name                = "${replace(var.project_name, "-", "")}db"
   username               = var.db_username
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.main.name
