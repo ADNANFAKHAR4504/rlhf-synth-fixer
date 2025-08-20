b1037c0099e9ec1caef99d90e1c551aacd57e100
karosi12 <adekayor@gmail.com>
2025-08-20 16:58:56 +0100

    feat: added integration and unit test

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -99,3 +99,291 @@ module "database_us_east_1" {
   private_subnet_ids         = module.vpc_us_east_1.private_subnet_ids
   database_security_group_id = module.vpc_us_east_1.database_security_group_id
 }
+
+module "logging_us_east_1" {
+  source = "./modules/logging"
+  providers = {
+    aws = aws.us_east_1
+  }
+
+  region      = local.regions.us_east_1.name
+  environment = local.environment
+  common_tags = local.common_tags
+}
+
+# =============================================================================
+# OUTPUTS
+# =============================================================================
+
+# VPC Outputs
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc_us_east_1.vpc_id
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = module.vpc_us_east_1.vpc_cidr_block
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = module.vpc_us_east_1.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = module.vpc_us_east_1.private_subnet_ids
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = module.vpc_us_east_1.internet_gateway_id
+}
+
+output "web_security_group_id" {
+  description = "ID of the web security group"
+  value       = module.vpc_us_east_1.web_security_group_id
+}
+
+output "database_security_group_id" {
+  description = "ID of the database security group"
+  value       = module.vpc_us_east_1.database_security_group_id
+}
+
+# IAM Outputs
+output "ec2_instance_profile_name" {
+  description = "EC2 instance profile name"
+  value       = module.iam_us_east_1.ec2_instance_profile_name
+}
+
+output "ec2_role_arn" {
+  description = "EC2 role ARN"
+  value       = module.iam_us_east_1.ec2_role_arn
+}
+
+output "rds_monitoring_role_arn" {
+  description = "RDS monitoring role ARN"
+  value       = module.iam_us_east_1.rds_monitoring_role_arn
+}
+
+# Compute Outputs
+output "launch_template_id" {
+  description = "ID of the launch template"
+  value       = module.compute_us_east_1.launch_template_id
+}
+
+output "launch_template_name" {
+  description = "Name of the launch template"
+  value       = module.compute_us_east_1.launch_template_name
+}
+
+output "autoscaling_group_id" {
+  description = "ID of the autoscaling group"
+  value       = module.compute_us_east_1.autoscaling_group_id
+}
+
+output "autoscaling_group_name" {
+  description = "Name of the autoscaling group"
+  value       = module.compute_us_east_1.autoscaling_group_name
+}
+
+output "load_balancer_id" {
+  description = "ID of the application load balancer"
+  value       = module.compute_us_east_1.load_balancer_id
+}
+
+output "load_balancer_arn" {
+  description = "ARN of the application load balancer"
+  value       = module.compute_us_east_1.load_balancer_arn
+}
+
+output "load_balancer_dns_name" {
+  description = "DNS name of the application load balancer"
+  value       = module.compute_us_east_1.load_balancer_dns_name
+}
+
+output "load_balancer_zone_id" {
+  description = "Zone ID of the application load balancer"
+  value       = module.compute_us_east_1.load_balancer_zone_id
+}
+
+output "target_group_arn" {
+  description = "ARN of the target group"
+  value       = module.compute_us_east_1.target_group_arn
+}
+
+output "target_group_name" {
+  description = "Name of the target group"
+  value       = module.compute_us_east_1.target_group_name
+}
+
+output "listener_arn" {
+  description = "ARN of the load balancer listener"
+  value       = module.compute_us_east_1.listener_arn
+}
+
+output "ami_id" {
+  description = "ID of the Amazon Linux AMI used"
+  value       = module.compute_us_east_1.ami_id
+}
+
+output "instance_type" {
+  description = "Instance type used in the launch template"
+  value       = module.compute_us_east_1.instance_type
+}
+
+# Database Outputs
+output "database_instance_id" {
+  description = "ID of the primary database instance"
+  value       = module.database_us_east_1.database_instance_id
+}
+
+output "database_instance_arn" {
+  description = "ARN of the primary database instance"
+  value       = module.database_us_east_1.database_instance_arn
+}
+
+output "database_endpoint" {
+  description = "Endpoint of the primary database instance"
+  value       = module.database_us_east_1.database_endpoint
+}
+
+output "database_port" {
+  description = "Port of the primary database instance"
+  value       = module.database_us_east_1.database_port
+}
+
+output "database_name" {
+  description = "Name of the primary database"
+  value       = module.database_us_east_1.database_name
+}
+
+output "database_username" {
+  description = "Username of the primary database"
+  value       = module.database_us_east_1.database_username
+}
+
+output "database_identifier" {
+  description = "Identifier of the primary database instance"
+  value       = module.database_us_east_1.database_identifier
+}
+
+output "database_engine_version" {
+  description = "Engine version of the database"
+  value       = module.database_us_east_1.database_engine_version
+}
+
+output "database_instance_class" {
+  description = "Instance class of the database"
+  value       = module.database_us_east_1.database_instance_class
+}
+
+output "database_allocated_storage" {
+  description = "Allocated storage of the database"
+  value       = module.database_us_east_1.database_allocated_storage
+}
+
+output "database_encrypted" {
+  description = "Whether the database storage is encrypted"
+  value       = module.database_us_east_1.database_encrypted
+}
+
+output "subnet_group_id" {
+  description = "ID of the database subnet group"
+  value       = module.database_us_east_1.subnet_group_id
+}
+
+output "subnet_group_name" {
+  description = "Name of the database subnet group"
+  value       = module.database_us_east_1.subnet_group_name
+}
+
+output "parameter_group_id" {
+  description = "ID of the database parameter group"
+  value       = module.database_us_east_1.parameter_group_id
+}
+
+output "parameter_group_name" {
+  description = "Name of the database parameter group"
+  value       = module.database_us_east_1.parameter_group_name
+}
+
+output "ssm_parameter_name" {
+  description = "Name of the SSM parameter storing the database password"
+  value       = module.database_us_east_1.ssm_parameter_name
+}
+
+output "ssm_parameter_arn" {
+  description = "ARN of the SSM parameter storing the database password"
+  value       = module.database_us_east_1.ssm_parameter_arn
+}
+
+# Logging Outputs
+output "application_log_group_name" {
+  description = "Name of the application CloudWatch log group"
+  value       = module.logging_us_east_1.application_log_group_name
+}
+
+output "application_log_group_arn" {
+  description = "ARN of the application CloudWatch log group"
+  value       = module.logging_us_east_1.application_log_group_arn
+}
+
+output "rds_log_group_name" {
+  description = "Name of the RDS CloudWatch log group"
+  value       = module.logging_us_east_1.rds_log_group_name
+}
+
+output "rds_log_group_arn" {
+  description = "ARN of the RDS CloudWatch log group"
+  value       = module.logging_us_east_1.rds_log_group_arn
+}
+
+output "dashboard_name" {
+  description = "Name of the CloudWatch dashboard"
+  value       = module.logging_us_east_1.dashboard_name
+}
+
+output "dashboard_arn" {
+  description = "ARN of the CloudWatch dashboard"
+  value       = module.logging_us_east_1.dashboard_arn
+}
+
+output "log_retention_days" {
+  description = "Log retention period in days"
+  value       = module.logging_us_east_1.log_retention_days
+}
+
+# Environment Information
+output "environment" {
+  description = "Environment name"
+  value       = local.environment
+}
+
+output "region" {
+  description = "Primary region"
+  value       = local.regions.us_east_1.name
+}
+
+output "common_tags" {
+  description = "Common tags applied to all resources"
+  value       = local.common_tags
+}
+
+# Summary Outputs
+output "infrastructure_summary" {
+  description = "Summary of the deployed infrastructure"
+  value = {
+    environment       = local.environment
+    region            = local.regions.us_east_1.name
+    vpc_id            = module.vpc_us_east_1.vpc_id
+    vpc_cidr          = module.vpc_us_east_1.vpc_cidr_block
+    public_subnets    = length(module.vpc_us_east_1.public_subnet_ids)
+    private_subnets   = length(module.vpc_us_east_1.private_subnet_ids)
+    load_balancer_dns = module.compute_us_east_1.load_balancer_dns_name
+    database_endpoint = module.database_us_east_1.database_endpoint
+    autoscaling_group = module.compute_us_east_1.autoscaling_group_name
+    dashboard_name    = module.logging_us_east_1.dashboard_name
+  }
+}
--- test/terraform.int.test.ts
+++ test/terraform.int.test.ts
@@ -1,7 +1,326 @@
-describe('Turn Around Prompt API Integration Tests', () => {
-  describe('Write Integration TESTS', () => {
-    test('Dont forget!', async () => {
-      expect(false).toBe(true);
+import { AutoScalingClient, DescribeAutoScalingGroupsCommand } from '@aws-sdk/client-auto-scaling';
+import { CloudWatchLogsClient, DescribeLogGroupsCommand } from '@aws-sdk/client-cloudwatch-logs';
+import { DescribeSecurityGroupsCommand, DescribeSubnetsCommand, DescribeVpcsCommand, EC2Client } from '@aws-sdk/client-ec2';
+import { DescribeLoadBalancersCommand, ElasticLoadBalancingV2Client } from '@aws-sdk/client-elastic-load-balancing-v2';
+import { GetRoleCommand, IAMClient } from '@aws-sdk/client-iam';
+import { DescribeDBInstancesCommand, RDSClient } from '@aws-sdk/client-rds';
+import { GetParameterCommand, SSMClient } from '@aws-sdk/client-ssm';
+import { GetCallerIdentityCommand, STSClient } from '@aws-sdk/client-sts';
+import fs from 'fs';
+import path from 'path';
+
+// Load reference outputs
+const FLAT_OUTPUTS_PATH = path.resolve(__dirname, '../cfn-outputs/flat-outputs.json');
+const referenceOutputs = JSON.parse(fs.readFileSync(FLAT_OUTPUTS_PATH, 'utf8'));
+
+// AWS Clients
+const stsClient = new STSClient({ region: referenceOutputs.region });
+const ec2Client = new EC2Client({ region: referenceOutputs.region });
+const rdsClient = new RDSClient({ region: referenceOutputs.region });
+const elbv2Client = new ElasticLoadBalancingV2Client({ region: referenceOutputs.region });
+const asgClient = new AutoScalingClient({ region: referenceOutputs.region });
+const logsClient = new CloudWatchLogsClient({ region: referenceOutputs.region });
+const iamClient = new IAMClient({ region: referenceOutputs.region });
+const ssmClient = new SSMClient({ region: referenceOutputs.region });
+
+describe('Terraform Infrastructure Integration Tests', () => {
+  let accountId: string;
+
+  beforeAll(async () => {
+    // Get AWS account ID
+    const stsResponse = await stsClient.send(new GetCallerIdentityCommand({}));
+    accountId = stsResponse.Account!;
+  });
+
+  describe('AWS Account and Region Validation', () => {
+    test('should have valid AWS account ID', () => {
+      expect(accountId).toBeDefined();
+      expect(accountId).toMatch(/^\d{12}$/);
+    });
+
+    test('should be in correct region', () => {
+      expect(referenceOutputs.region).toBe('us-east-1');
+    });
+
+    test('should have correct environment', () => {
+      expect(referenceOutputs.environment).toBe('dev');
+    });
+  });
+
+  describe('VPC Infrastructure Validation', () => {
+    test('should have valid VPC', async () => {
+      const response = await ec2Client.send(new DescribeVpcsCommand({
+        VpcIds: [referenceOutputs.vpc_id]
+      }));
+
+      expect(response.Vpcs).toHaveLength(1);
+      const vpc = response.Vpcs![0];
+      expect(vpc.VpcId).toBe(referenceOutputs.vpc_id);
+      expect(vpc.CidrBlock).toBe(referenceOutputs.vpc_cidr_block);
+      expect(vpc.State).toBe('available');
+    });
+
+    test('should have correct number of public subnets', async () => {
+      const publicSubnetIds = JSON.parse(referenceOutputs.public_subnet_ids);
+      expect(publicSubnetIds).toHaveLength(2);
+
+      const response = await ec2Client.send(new DescribeSubnetsCommand({
+        SubnetIds: publicSubnetIds
+      }));
+
+      expect(response.Subnets).toHaveLength(2);
+      response.Subnets!.forEach(subnet => {
+        expect(subnet.State).toBe('available');
+        expect(subnet.MapPublicIpOnLaunch).toBe(true);
+      });
+    });
+
+    test('should have correct number of private subnets', async () => {
+      const privateSubnetIds = JSON.parse(referenceOutputs.private_subnet_ids);
+      expect(privateSubnetIds).toHaveLength(2);
+
+      const response = await ec2Client.send(new DescribeSubnetsCommand({
+        SubnetIds: privateSubnetIds
+      }));
+
+      expect(response.Subnets).toHaveLength(2);
+      response.Subnets!.forEach(subnet => {
+        expect(subnet.State).toBe('available');
+        expect(subnet.MapPublicIpOnLaunch).toBe(false);
+      });
+    });
+
+    test('should have valid internet gateway', async () => {
+      const response = await ec2Client.send(new DescribeVpcsCommand({
+        VpcIds: [referenceOutputs.vpc_id]
+      }));
+
+      const vpc = response.Vpcs![0];
+      expect(vpc.VpcId).toBe(referenceOutputs.vpc_id);
+      expect(vpc.State).toBe('available');
+    });
+
+    test('should have valid security groups', async () => {
+      const securityGroupIds = [
+        referenceOutputs.web_security_group_id,
+        referenceOutputs.database_security_group_id
+      ];
+
+      const response = await ec2Client.send(new DescribeSecurityGroupsCommand({
+        GroupIds: securityGroupIds
+      }));
+
+      expect(response.SecurityGroups).toHaveLength(2);
+      response.SecurityGroups!.forEach(sg => {
+        expect(sg.GroupId).toBeDefined();
+        expect(sg.VpcId).toBe(referenceOutputs.vpc_id);
+      });
+    });
+  });
+
+  describe('Database Infrastructure Validation', () => {
+    test('should have valid RDS instance', async () => {
+      const response = await rdsClient.send(new DescribeDBInstancesCommand({
+        DBInstanceIdentifier: referenceOutputs.database_identifier
+      }));
+
+      expect(response.DBInstances).toHaveLength(1);
+      const dbInstance = response.DBInstances![0];
+      expect(dbInstance.DBInstanceIdentifier).toBe(referenceOutputs.database_identifier);
+      expect(dbInstance.DBInstanceStatus).toBe('available');
+      expect(dbInstance.Engine).toBe('postgres');
+      expect(dbInstance.EngineVersion).toBe(referenceOutputs.database_engine_version);
+      expect(dbInstance.DBInstanceClass).toBe(referenceOutputs.database_instance_class);
+      expect(dbInstance.AllocatedStorage).toBe(parseInt(referenceOutputs.database_allocated_storage));
+      expect(dbInstance.StorageEncrypted).toBe(referenceOutputs.database_encrypted === 'true');
+    });
+
+    test('should have valid database endpoint', () => {
+      expect(referenceOutputs.database_endpoint).toMatch(/\.rds\.amazonaws\.com:\d+$/);
+      expect(referenceOutputs.database_port).toBe('5432');
+    });
+
+    test('should have valid database subnet group', () => {
+      expect(referenceOutputs.subnet_group_name).toMatch(/^dev-db-subnet-group-us-east-1$/);
+    });
+
+    test('should have valid parameter group', () => {
+      expect(referenceOutputs.parameter_group_name).toMatch(/^dev-db-params-us-east-1$/);
+    });
+  });
+
+  describe('Compute Infrastructure Validation', () => {
+    test('should have valid load balancer', async () => {
+      const response = await elbv2Client.send(new DescribeLoadBalancersCommand({
+        LoadBalancerArns: [referenceOutputs.load_balancer_arn]
+      }));
+
+      expect(response.LoadBalancers).toHaveLength(1);
+      const lb = response.LoadBalancers![0];
+      expect(lb.LoadBalancerArn).toBe(referenceOutputs.load_balancer_arn);
+      expect(lb.DNSName).toBe(referenceOutputs.load_balancer_dns_name);
+      expect(lb.State?.Code).toBe('active');
+      expect(lb.Type).toBe('application');
+    });
+
+    test('should have valid autoscaling group', async () => {
+      const response = await asgClient.send(new DescribeAutoScalingGroupsCommand({
+        AutoScalingGroupNames: [referenceOutputs.autoscaling_group_name]
+      }));
+
+      expect(response.AutoScalingGroups).toHaveLength(1);
+      const asg = response.AutoScalingGroups![0];
+      expect(asg.AutoScalingGroupName).toBe(referenceOutputs.autoscaling_group_name);
+      expect(asg.AutoScalingGroupARN).toMatch(/^arn:aws:autoscaling:us-east-1:\d+:autoScalingGroup:/);
+      expect(asg.AutoScalingGroupARN).toContain(referenceOutputs.autoscaling_group_name);
+      expect(asg.AutoScalingGroupName).toBeDefined();
+      expect(asg.AutoScalingGroupARN).toBeDefined();
+    });
+
+    test('should have valid launch template', () => {
+      expect(referenceOutputs.launch_template_id).toMatch(/^lt-[a-z0-9]+$/);
+      expect(referenceOutputs.instance_type).toBe('t3.micro');
+    });
+
+    test('should have valid target group', () => {
+      expect(referenceOutputs.target_group_arn).toMatch(/^arn:aws:elasticloadbalancing:/);
+      expect(referenceOutputs.target_group_name).toMatch(/^dev-tg-us-east-1$/);
+    });
+  });
+
+  describe('IAM Infrastructure Validation', () => {
+    test('should have valid EC2 role', async () => {
+      const response = await iamClient.send(new GetRoleCommand({
+        RoleName: referenceOutputs.ec2_role_arn.split('/').pop()
+      }));
+
+      expect(response.Role).toBeDefined();
+      expect(response.Role!.RoleName).toBe('dev-ec2-role-us-east-1');
+      expect(response.Role!.Arn).toBe(referenceOutputs.ec2_role_arn);
+    });
+
+    test('should have valid EC2 instance profile', () => {
+      expect(referenceOutputs.ec2_instance_profile_name).toBe('dev-ec2-profile-us-east-1');
+    });
+
+    test('should have valid RDS monitoring role', async () => {
+      const response = await iamClient.send(new GetRoleCommand({
+        RoleName: referenceOutputs.rds_monitoring_role_arn.split('/').pop()
+      }));
+
+      expect(response.Role).toBeDefined();
+      expect(response.Role!.RoleName).toBe('dev-rds-monitoring-role-us-east-1');
+      expect(response.Role!.Arn).toBe(referenceOutputs.rds_monitoring_role_arn);
+    });
+  });
+
+  describe('Logging Infrastructure Validation', () => {
+    test('should have valid CloudWatch log groups', async () => {
+      const logGroupNames = [
+        referenceOutputs.application_log_group_name,
+        referenceOutputs.rds_log_group_name
+      ];
+
+      const response = await logsClient.send(new DescribeLogGroupsCommand({
+        logGroupNamePrefix: '/aws/'
+      }));
+
+      const foundLogGroups = response.logGroups!.filter(lg => 
+        logGroupNames.includes(lg.logGroupName!)
+      );
+
+      expect(foundLogGroups).toHaveLength(2);
+      foundLogGroups.forEach(lg => {
+        expect(lg.logGroupName).toBeDefined();
+        expect(lg.retentionInDays).toBe(parseInt(referenceOutputs.log_retention_days));
+      });
+    });
+
+    test('should have valid CloudWatch dashboard', () => {
+      expect(referenceOutputs.dashboard_arn).toMatch(/^arn:aws:cloudwatch::\d+:dashboard\//);
+      expect(referenceOutputs.dashboard_name).toBe('dev-dashboard-us-east-1');
+    });
+  });
+
+  describe('Security and Configuration Validation', () => {
+    test('should have valid SSM parameter for database password', async () => {
+      const response = await ssmClient.send(new GetParameterCommand({
+        Name: referenceOutputs.ssm_parameter_name,
+        WithDecryption: false
+      }));
+
+      expect(response.Parameter).toBeDefined();
+      expect(response.Parameter!.Name).toBe(referenceOutputs.ssm_parameter_name);
+      expect(response.Parameter!.Type).toBe('SecureString');
+    });
+
+    test('should have consistent tagging', () => {
+      const commonTags = JSON.parse(referenceOutputs.common_tags);
+      expect(commonTags.Environment).toBe('dev');
+      expect(commonTags.ManagedBy).toBe('terraform');
+      expect(commonTags.Owner).toBe('platform-team');
+      expect(commonTags.Project).toBe('multi-region-infrastructure');
+    });
+
+    test('should have valid infrastructure summary', () => {
+      const summary = JSON.parse(referenceOutputs.infrastructure_summary);
+      expect(summary.environment).toBe('dev');
+      expect(summary.region).toBe('us-east-1');
+      expect(summary.vpc_id).toBe(referenceOutputs.vpc_id);
+      expect(summary.vpc_cidr).toBe(referenceOutputs.vpc_cidr_block);
+      expect(summary.public_subnets).toBe(2);
+      expect(summary.private_subnets).toBe(2);
+      expect(summary.load_balancer_dns).toBe(referenceOutputs.load_balancer_dns_name);
+      expect(summary.database_endpoint).toBe(referenceOutputs.database_endpoint);
+    });
+  });
+
+  describe('Connectivity and Health Checks', () => {
+    test('should have valid AMI', () => {
+      expect(referenceOutputs.ami_id).toMatch(/^ami-[a-z0-9]+$/);
+    });
+
+    test('should have valid load balancer zone ID', () => {
+      expect(referenceOutputs.load_balancer_zone_id).toMatch(/^Z[A-Z0-9]+$/);
+    });
+
+    test('should have valid listener ARN', () => {
+      expect(referenceOutputs.listener_arn).toMatch(/^arn:aws:elasticloadbalancing:/);
+    });
+
+    test('should have valid database instance ARN', () => {
+      expect(referenceOutputs.database_instance_arn).toMatch(/^arn:aws:rds:us-east-1:\d+:db:/);
+    });
+
+    test('should have valid database instance ID', () => {
+      expect(referenceOutputs.database_instance_id).toMatch(/^db-[A-Z0-9]+$/);
+    });
+  });
+
+  describe('Resource Relationships Validation', () => {
+    test('should have consistent VPC references', () => {
+      // All resources should reference the same VPC
+      expect(referenceOutputs.vpc_id).toBeDefined();
+      expect(referenceOutputs.vpc_cidr_block).toBe('10.0.0.0/16');
+    });
+
+    test('should have consistent subnet references', () => {
+      const publicSubnets = JSON.parse(referenceOutputs.public_subnet_ids);
+      const privateSubnets = JSON.parse(referenceOutputs.private_subnet_ids);
+      
+      expect(publicSubnets).toHaveLength(2);
+      expect(privateSubnets).toHaveLength(2);
+      
+      // All subnets should be unique
+      const allSubnets = [...publicSubnets, ...privateSubnets];
+      const uniqueSubnets = [...new Set(allSubnets)];
+      expect(uniqueSubnets).toHaveLength(4);
+    });
+
+    test('should have consistent security group references', () => {
+      expect(referenceOutputs.web_security_group_id).toMatch(/^sg-[a-z0-9]+$/);
+      expect(referenceOutputs.database_security_group_id).toMatch(/^sg-[a-z0-9]+$/);
+      expect(referenceOutputs.web_security_group_id).not.toBe(referenceOutputs.database_security_group_id);
     });
   });
 });
