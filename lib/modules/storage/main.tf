b09003a1582fb68862342f3c6d0edd13975a7977
ngwakoleslieelijah <raditupa@gmail.com>
2025-08-20 14:59:16 +0200

    policy fix

--- lib/modules/storage/main.tf
+++ lib/modules/storage/main.tf
@@ -36,10 +36,10 @@ resource "aws_s3_bucket_versioning" "logs" {
 resource "aws_s3_bucket_public_access_block" "logs" {
   bucket = aws_s3_bucket.logs.id
 
-  block_public_acls       = true
-  block_public_policy     = true
+  block_public_acls       = false
+  block_public_policy     = false
   ignore_public_acls      = true
-  restrict_public_buckets = true
+  restrict_public_buckets = false
 }
 
 resource "aws_vpc_endpoint" "s3" {
