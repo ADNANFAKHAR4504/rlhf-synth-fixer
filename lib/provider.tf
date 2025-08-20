d0e8ef0191c6a9c30b6833056bc0df554e084152
Harshita Agarwal <harshita.g@turing.com>
2025-08-20 13:18:45 +0530

    fix

--- lib/provider.tf
+++ lib/provider.tf
@@ -1,18 +1,6 @@
 # /-----------------------------------------------------------------------------
 # | Terraform & Provider Configuration
 # |-----------------------------------------------------------------------------
-terraform {
-  # This backend block is required by your CI/CD pipeline.
-  # The configuration is passed in dynamically during initialization.
-  backend "s3" {}
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
 # Variable for the default AWS region.
 variable "aws_region" {
@@ -28,12 +16,12 @@ provider "aws" {
 
 # Provider alias for the US East (N. Virginia) region.
 provider "aws" {
-  alias  = "us-east-1"
+  alias  = "useast1"
   region = "us-east-1"
 }
 
 # Provider alias for the US West (Oregon) region.
 provider "aws" {
-  alias  = "us-west-2"
+  alias  = "uswest2"
   region = "us-west-2"
 }
