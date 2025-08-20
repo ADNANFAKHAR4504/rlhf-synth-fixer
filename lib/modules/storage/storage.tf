2a51ca8708c8dec4ae82291346cd6b25ddbfd2d5
ngwakoleslieelijah <raditupa@gmail.com>
2025-08-20 22:33:48 +0200

    starting with the removal of the explicit S3 bucket policy and subsequently fixing a series of cascading errors

--- lib/modules/storage/storage.tf
+++ lib/modules/storage/storage.tf
@@ -19,11 +19,6 @@ resource "aws_s3_bucket" "data" {
   }
 }
 
-output "s3_logs_bucket_name" {
-  description = "The name of the S3 logs bucket"
-  value       = aws_s3_bucket.logs.bucket
-}
-
 resource "aws_s3_bucket" "logs" {
   bucket = "${lower(substr(var.project_name, 0, 20))}-logs-bucket-${random_pet.suffix.id}"
 
