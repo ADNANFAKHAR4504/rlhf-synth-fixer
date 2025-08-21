24192cca0612ca1f6f548c7217b5455d53de35ab
ngwakoleslieelijah <raditupa@gmail.com>
2025-08-22 00:00:06 +0200

    all identified issues

--- lib/modules/monitoring/outputs.tf
+++ lib/modules/monitoring/outputs.tf
@@ -5,5 +5,5 @@ output "sns_topic_arn" {
 
 output "ec2_log_group_name" {
   description = "Name of the CloudWatch log group for EC2 instances"
-  value       = data.aws_cloudwatch_log_group.ec2_logs.name
+  value       = aws_cloudwatch_log_group.ec2_logs.name
 }
