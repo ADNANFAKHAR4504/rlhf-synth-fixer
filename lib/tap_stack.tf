c27255e2e779253daa62853da6098e3087de0157
David Michael <davidlearn27@gmail.com>
2025-08-21 20:30:08 +0100

    Refactor tap_stack.tf by removing outdated Terraform configuration for providers and required version. Streamline the file to focus on essential variables for AWS region.

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -1,20 +1,5 @@
 # tap_stack.tf
 
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
-  }
-}
-
 # Variables
 variable "aws_region" {
   description = "AWS region"
