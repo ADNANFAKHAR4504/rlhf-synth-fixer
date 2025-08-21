87aec3ff458a8ba160741ac3b7e1892a125855bc
root <root@DESKTOP-3KTOO66.localdomain>
2025-08-21 10:15:32 +0530

    adding s3 bucket kms key encryption

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -520,12 +520,41 @@ resource "aws_s3_bucket_public_access_block" "main" {
 }
 
 # S3 Bucket Server Side Encryption
+#resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
+#  bucket = aws_s3_bucket.main.id
+#
+#  rule {
+#    apply_server_side_encryption_by_default {
+#      sse_algorithm = "AES256"
+#    }
+#  }
+#}
+resource "aws_kms_key" "s3_encryption" {
+  description = "KMS key for S3 bucket encryption"
+  deletion_window_in_days = 10
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-s3-kms-key"
+  }
+}
+
+resource "aws_kms_alias" "s3_encryption_alias" {
+  name          = "alias/${var.project_name}-${var.environment}-s3-kms-key"
+  target_key_id = aws_kms_key.s3_encryption.id
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
   bucket = aws_s3_bucket.main.id
 
+  depends_on = [
+    aws_s3_bucket.main,
+    aws_kms_key.s3_encryption
+  ]
+
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.s3_encryption.arn
     }
   }
 }
