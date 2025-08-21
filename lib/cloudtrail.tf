6bb5ad7951f1290a163002d9eb85a2bae8486b30
Olatunjym <131082354+Olatunjym@users.noreply.github.com>
2025-08-21 04:13:30 +0100

    Update cloudtrail.tf

--- lib/cloudtrail.tf
+++ lib/cloudtrail.tf
@@ -66,7 +66,7 @@ resource "aws_cloudtrail" "main" {
   include_global_service_events = true
   is_multi_region_trail         = true
   enable_log_file_validation    = true
-  cloud_watch_logs_group_arn    = aws_cloudwatch_log_group.cloudtrail.arn
+  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.cloudtrail.arn}:*"
   cloud_watch_logs_role_arn     = aws_iam_role.cloudtrail_logs.arn
   tags = {
     Name        = "${var.name_prefix}-${var.environment}-cloudtrail"
