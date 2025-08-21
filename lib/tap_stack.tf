7509870cfc0f631308c263e1ecd276d618c40e7d
olatunjym <adeteyelinux@gmail.com>
2025-08-21 07:42:47 +0100

    updated tap_stack as per claude review

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -807,11 +807,11 @@ resource "aws_acm_certificate" "alb_cert" {
 }
 
 resource "aws_route53_record" "alb_cert_validation" {
-  for_each = { for dvo in aws_acm_certificate.alb_cert.domain_validation_options : dvo.resource_record_name => dvo }
+  count   = length(aws_acm_certificate.alb_cert.domain_validation_options)
   zone_id = aws_route53_zone.secure_prod_zone.zone_id
-  name    = each.value.resource_record_name
-  type    = each.value.resource_record_type
-  records = [each.value.resource_record_value]
+  name    = aws_acm_certificate.alb_cert.domain_validation_options[count.index].resource_record_name
+  type    = aws_acm_certificate.alb_cert.domain_validation_options[count.index].resource_record_type
+  records = [aws_acm_certificate.alb_cert.domain_validation_options[count.index].resource_record_value]
   ttl     = 60
 }
 
