262d2322032b3d52e8f1b5682263e9d02521b6b3
ngwakoleslieelijah <raditupa@gmail.com>
2025-08-22 14:58:51 +0200

    Updating the providers

--- lib/provider.tf
+++ lib/provider.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -6,16 +7,25 @@ terraform {
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = "~> 3.1"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
     }
   }
 }
 
 provider "aws" {
-  region  = var.region
-  profile = "default"
+  region = var.aws_region
 
   default_tags {
-    tags = local.common_tags
+    tags = {
+      Environment = var.environment
+      Project     = var.project_name
+      Owner       = var.owner
+      ManagedBy   = "Terraform"
+      CostCenter  = var.cost_center
+    }
   }
 }
