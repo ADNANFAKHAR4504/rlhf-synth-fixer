c948c2f8281d9fac1ea9fbfeb1da65540190f1bd
root <root@DESKTOP-3KTOO66.localdomain>
2025-08-23 00:27:11 +0530

    adding provider comfigiration

--- lib/provider.tf
+++ lib/provider.tf
@@ -18,3 +18,12 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "us_west_2"
+  region = "us-west-2"
+}
