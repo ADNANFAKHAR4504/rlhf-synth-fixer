096d9047d4645143749983636f5bd9b7f13b82f1
Siva Kumar <sk800489@gmail.com>
2025-08-24 23:28:43 +0530

    feat: refactor infrastructure code with environment-based configuration and comprehensive testing

--- lib/tap_stack.py
+++ lib/tap_stack.py
@@ -1,34 +1,49 @@
 import aws_cdk as cdk
 from aws_cdk import (
     Stack,
+    StackProps,
     aws_ec2 as ec2,
     aws_iam as iam,
-    aws_autoscaling as autoscaling,
-    aws_elasticloadbalancingv2 as elbv2,
     aws_s3 as s3,
-    aws_kms as kms,
     aws_rds as rds,
+    aws_elasticloadbalancingv2 as elbv2,
+    aws_autoscaling as autoscaling,
     aws_cloudwatch as cloudwatch,
     aws_cloudfront as cloudfront,
     aws_cloudfront_origins as origins,
     aws_route53 as route53,
     aws_route53_targets as route53_targets,
+    aws_kms as kms,
+    Duration,
     CfnOutput,
     RemovalPolicy
 )
 from constructs import Construct
+from typing import Optional
+
+
+class TapStackProps(StackProps):
+    """Properties for TapStack"""
+    def __init__(self, environment_suffix: str, **kwargs):
+        super().__init__(**kwargs)
+        self.environment_suffix = environment_suffix
+
 
 class TapStack(Stack):
-    def __init__(self, scope: Construct, construct_id: str, env_name: str, region_code: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: Construct, construct_id: str, *, props: Optional[TapStackProps] = None, **kwargs) -> None:
+        # Set termination protection for production environments only
+        termination_protection = props.environment_suffix == 'prod' if props else False
+        super().__init__(scope, construct_id, env=props.env if props else None, 
+                        termination_protection=termination_protection, **kwargs)
         
-        self.env_name = env_name
-        self.region_code = region_code
+        self.environment_suffix = props.environment_suffix if props else 'dev'
+        self.region_name = self.region or 'us-east-1'
         
         self.kms_key = kms.Key(
-            self, f'KmsKey-{env_name}-{region_code}',
-            description=f'KMS key for {env_name} environment in {region_code}',
-            enable_key_rotation=True
+            self, f'KmsKey{self.environment_suffix}',
+            description=f'KMS key for {self.environment_suffix} environment in {cdk.Aws.REGION}',
+            enable_key_rotation=True,
+            removal_policy=RemovalPolicy.DESTROY if self.environment_suffix != 'prod' else RemovalPolicy.RETAIN
         )
         
         self.vpc = self.create_vpc()
@@ -40,25 +55,28 @@ class TapStack(Stack):
         self.asg = self.create_asg()
         self.create_cloudwatch_alarms()
         
-        if self.env_name == 'prod':
+        if self.environment_suffix == 'prod':
             self.cloudfront = self.create_cloudfront()
         
         self.hosted_zone = self.create_route53()
+        
+        # Create outputs for integration testing
+        self._create_outputs()
     
     def create_vpc(self):
         vpc = ec2.Vpc(
-            self, f'Vpc-{self.env_name}-{self.region_code}',
-            vpc_name=f'vpc-{self.env_name}-{self.region_code}',
+            self, f'Vpc{self.environment_suffix}',
+            vpc_name=f'vpc-{self.environment_suffix}',
             ip_addresses=ec2.IpAddresses.cidr('10.0.0.0/16'),
             max_azs=3,
             subnet_configuration=[
                 ec2.SubnetConfiguration(
-                    name=f'public-{self.env_name}-{self.region_code}',
+                    name=f'public-{self.environment_suffix}',
                     subnet_type=ec2.SubnetType.PUBLIC,
                     cidr_mask=24
                 ),
                 ec2.SubnetConfiguration(
-                    name=f'private-{self.env_name}-{self.region_code}',
+                    name=f'private-{self.environment_suffix}',
                     subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
                     cidr_mask=24
                 )
@@ -68,27 +86,27 @@ class TapStack(Stack):
     
     def create_security_groups(self):
         alb_sg = ec2.SecurityGroup(
-            self, f'AlbSg-{self.env_name}-{self.region_code}',
+            self, f'AlbSg{self.environment_suffix}',
             vpc=self.vpc,
-            description=f'ALB security group for {self.env_name}',
-            security_group_name=f'alb-sg-{self.env_name}-{self.region_code}'
+            description=f'ALB security group for {self.environment_suffix}',
+            security_group_name=f'alb-sg-{self.environment_suffix}'
         )
         alb_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80))
         alb_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(443))
         
         ec2_sg = ec2.SecurityGroup(
-            self, f'Ec2Sg-{self.env_name}-{self.region_code}',
+            self, f'Ec2Sg{self.environment_suffix}',
             vpc=self.vpc,
-            description=f'EC2 security group for {self.env_name}',
-            security_group_name=f'ec2-sg-{self.env_name}-{self.region_code}'
+            description=f'EC2 security group for {self.environment_suffix}',
+            security_group_name=f'ec2-sg-{self.environment_suffix}'
         )
         ec2_sg.add_ingress_rule(alb_sg, ec2.Port.tcp(80))
         
         rds_sg = ec2.SecurityGroup(
-            self, f'RdsSg-{self.env_name}-{self.region_code}',
+            self, f'RdsSg{self.environment_suffix}',
             vpc=self.vpc,
-            description=f'RDS security group for {self.env_name}',
-            security_group_name=f'rds-sg-{self.env_name}-{self.region_code}'
+            description=f'RDS security group for {self.environment_suffix}',
+            security_group_name=f'rds-sg-{self.environment_suffix}'
         )
         rds_sg.add_ingress_rule(ec2_sg, ec2.Port.tcp(3306))
         
@@ -100,8 +118,8 @@ class TapStack(Stack):
     
     def create_iam_role(self):
         role = iam.Role(
-            self, f'Ec2Role-{self.env_name}-{self.region_code}',
-            role_name=f'ec2-role-{self.env_name}-{self.region_code}',
+            self, f'Ec2Role{self.environment_suffix}',
+            role_name=f'ec2-role-{self.environment_suffix}',
             assumed_by=iam.ServicePrincipal('ec2.amazonaws.com'),
             inline_policies={
                 'S3Access': iam.PolicyDocument(
@@ -113,12 +131,12 @@ class TapStack(Stack):
                                 's3:PutObject',
                                 's3:DeleteObject'
                             ],
-                            resources=[f'arn:aws:s3:::tap-{self.env_name}-{self.region_code}-*/*']
+                            resources=[f'arn:aws:s3:::tap-{self.environment_suffix}-*/*']
                         ),
                         iam.PolicyStatement(
                             effect=iam.Effect.ALLOW,
                             actions=['s3:ListBucket'],
-                            resources=[f'arn:aws:s3:::tap-{self.env_name}-{self.region_code}-*']
+                            resources=[f'arn:aws:s3:::tap-{self.environment_suffix}-*']
                         )
                     ]
                 ),
@@ -131,7 +149,9 @@ class TapStack(Stack):
                                 'logs:CreateLogStream',
                                 'logs:PutLogEvents'
                             ],
-                            resources=[f'arn:aws:logs:{self.region}:{self.account}:log-group:/aws/ec2/{self.env_name}/*']
+                            resources=[
+                                f'arn:aws:logs:{self.region}:{self.account}:log-group:/aws/ec2/{self.environment_suffix}/*'
+                            ]
                         )
                     ]
                 ),
@@ -151,68 +171,79 @@ class TapStack(Stack):
         )
         
         iam.InstanceProfile(
-            self, f'Ec2InstanceProfile-{self.env_name}-{self.region_code}',
-            instance_profile_name=f'ec2-profile-{self.env_name}-{self.region_code}',
+            self, f'Ec2InstanceProfile{self.environment_suffix}',
+            instance_profile_name=f'ec2-profile-{self.environment_suffix}',
             role=role
         )
         
         return role
     
     def create_s3_bucket(self):
-        bucket = s3.Bucket(
-            self, f'S3Bucket-{self.env_name}-{self.region_code}',
-            bucket_name=f'tap-{self.env_name}-{self.region_code}-{self.account}',
+        self.s3_bucket = s3.Bucket(
+            self, f'S3Bucket{self.environment_suffix}',
+            bucket_name=f's3-bucket-{self.environment_suffix}',
             versioned=True,
             encryption=s3.BucketEncryption.KMS,
             encryption_key=self.kms_key,
-            removal_policy=RemovalPolicy.RETAIN,
-            enforce_ssl=True
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            # Enable auto-deletion of objects for non-prod environments
+            auto_delete_objects=self.environment_suffix != 'prod',
+            removal_policy=RemovalPolicy.DESTROY if self.environment_suffix != 'prod' else RemovalPolicy.RETAIN
         )
-        return bucket
+        return self.s3_bucket
     
     def create_rds(self):
         subnet_group = rds.SubnetGroup(
-            self, f'RdsSubnetGroup-{self.env_name}-{self.region_code}',
-            description=f'RDS subnet group for {self.env_name}',
+            self, f'RdsSubnetGroup{self.environment_suffix}',
+            description=f'RDS subnet group for {self.environment_suffix}',
             vpc=self.vpc,
             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS)
         )
         
-        instance = rds.DatabaseInstance(
-            self, f'RdsInstance-{self.env_name}-{self.region_code}',
-            engine=rds.DatabaseInstanceEngine.mysql(version=rds.MysqlEngineVersion.VER_8_0),
-            instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
+        self.rds_instance = rds.DatabaseInstance(
+            self, f'RdsInstance{self.environment_suffix}',
+            engine=rds.DatabaseInstanceEngine.mysql(
+                version=rds.MysqlEngineVersion.VER_8_0_39
+            ),
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO
+            ),
             vpc=self.vpc,
-            subnet_group=subnet_group,
+            vpc_subnets=ec2.SubnetSelection(
+                subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
+            ),
             security_groups=[self.security_groups['rds']],
+            allocated_storage=20,
             storage_encrypted=True,
             storage_encryption_key=self.kms_key,
-            backup_retention=cdk.Duration.days(7),
-            delete_automated_backups=False,
-            deletion_protection=self.env_name == 'prod',
+            backup_retention=Duration.days(7),
             database_name='tapdb',
+            deletion_protection=self.environment_suffix == 'prod',
+            delete_automated_backups=self.environment_suffix != 'prod',
             credentials=rds.Credentials.from_generated_secret(
                 'admin',
-                secret_name=f'rds-credentials-{self.env_name}-{self.region_code}'
-            )
+                secret_name=f'rds-credentials-{self.environment_suffix}'
+            ),
+            subnet_group=subnet_group,
+            removal_policy=RemovalPolicy.DESTROY if self.environment_suffix != 'prod' else RemovalPolicy.SNAPSHOT
         )
-        return instance
+        return self.rds_instance
     
     def create_alb(self):
         alb = elbv2.ApplicationLoadBalancer(
-            self, f'Alb-{self.env_name}-{self.region_code}',
+            self, f'Alb{self.environment_suffix}',
             vpc=self.vpc,
             internet_facing=True,
             security_group=self.security_groups['alb'],
-            load_balancer_name=f'alb-{self.env_name}-{self.region_code}'
+            load_balancer_name=f'alb-{self.environment_suffix}'
         )
         
         target_group = elbv2.ApplicationTargetGroup(
-            self, f'TargetGroup-{self.env_name}-{self.region_code}',
+            self, f'TargetGroup{self.environment_suffix}',
             vpc=self.vpc,
             port=80,
             protocol=elbv2.ApplicationProtocol.HTTP,
-            target_group_name=f'tg-{self.env_name}-{self.region_code}',
+            target_group_name=f'tg-{self.environment_suffix}',
             health_check=elbv2.HealthCheck(
                 enabled=True,
                 healthy_threshold_count=2,
@@ -226,7 +257,7 @@ class TapStack(Stack):
         )
         
         alb.add_listener(
-            f'AlbListener-{self.env_name}-{self.region_code}',
+            f'AlbListener{self.environment_suffix}',
             port=80,
             default_target_groups=[target_group]
         )
@@ -241,12 +272,12 @@ class TapStack(Stack):
             'yum install -y httpd',
             'systemctl start httpd',
             'systemctl enable httpd',
-            f'echo "<h1>Hello from {self.env_name} in {self.region_code}</h1>" > /var/www/html/index.html'
+            f'echo "<h1>Hello from {self.environment_suffix} in {self.region_name}</h1>" > /var/www/html/index.html'
         )
         
         launch_template = ec2.LaunchTemplate(
-            self, f'LaunchTemplate-{self.env_name}-{self.region_code}',
-            launch_template_name=f'lt-{self.env_name}-{self.region_code}',
+            self, f'LaunchTemplate{self.environment_suffix}',
+            launch_template_name=f'lt-{self.environment_suffix}',
             instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
             machine_image=ec2.AmazonLinuxImage(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2),
             security_group=self.security_groups['ec2'],
@@ -265,7 +296,7 @@ class TapStack(Stack):
         )
         
         asg = autoscaling.AutoScalingGroup(
-            self, f'Asg-{self.env_name}-{self.region_code}',
+            self, f'Asg{self.environment_suffix}',
             vpc=self.vpc,
             launch_template=launch_template,
             min_capacity=1,
@@ -273,7 +304,7 @@ class TapStack(Stack):
             desired_capacity=2,
             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS),
             health_checks=autoscaling.HealthChecks.ec2(),
-            auto_scaling_group_name=f'asg-{self.env_name}-{self.region_code}'
+            auto_scaling_group_name=f'asg-{self.environment_suffix}'
         )
         
         asg.attach_to_application_target_group(self.target_group)
@@ -281,8 +312,8 @@ class TapStack(Stack):
     
     def create_cloudwatch_alarms(self):
         cloudwatch.Alarm(
-            self, f'CpuAlarm-{self.env_name}-{self.region_code}',
-            alarm_name=f'cpu-alarm-{self.env_name}-{self.region_code}',
+            self, f'CpuAlarm{self.environment_suffix}',
+            alarm_name=f'cpu-alarm-{self.environment_suffix}',
             metric=cloudwatch.Metric(
                 namespace='AWS/AutoScaling',
                 metric_name='CPUUtilization',
@@ -299,7 +330,7 @@ class TapStack(Stack):
     
     def create_cloudfront(self):
         distribution = cloudfront.Distribution(
-            self, f'CloudFront-{self.env_name}-{self.region_code}',
+            self, f'CloudFront{self.environment_suffix}',
             default_behavior=cloudfront.BehaviorOptions(
                 origin=origins.LoadBalancerV2Origin(self.alb),
                 viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
@@ -312,12 +343,12 @@ class TapStack(Stack):
     
     def create_route53(self):
         hosted_zone = route53.HostedZone(
-            self, f'HostedZone-{self.env_name}-{self.region_code}',
-            zone_name=f'{self.env_name}-{self.region_code}.tap.internal'
+            self, f'HostedZone{self.environment_suffix}',
+            zone_name=f'{self.environment_suffix}.tap.internal'
         )
         
         route53.ARecord(
-            self, f'AlbRecord-{self.env_name}-{self.region_code}',
+            self, f'AlbRecord{self.environment_suffix}',
             zone=hosted_zone,
             record_name='alb',
             target=route53.RecordTarget.from_alias(
@@ -327,7 +358,7 @@ class TapStack(Stack):
         
         if hasattr(self, 'cloudfront'):
             route53.ARecord(
-                self, f'CloudFrontRecord-{self.env_name}-{self.region_code}',
+                self, f'CloudFrontRecord{self.environment_suffix}',
                 zone=hosted_zone,
                 record_name='cdn',
                 target=route53.RecordTarget.from_alias(
@@ -336,3 +367,44 @@ class TapStack(Stack):
             )
         
         return hosted_zone
+    
+    def _create_outputs(self):
+        """Create CloudFormation outputs for integration testing"""
+        # VPC outputs
+        CfnOutput(self, 'VpcId', value=self.vpc.vpc_id, description='VPC ID')
+        
+        # S3 outputs
+        CfnOutput(self, 'S3BucketName', value=self.s3_bucket.bucket_name, description='S3 Bucket Name')
+        
+        # RDS outputs
+        CfnOutput(self, 'RdsInstanceId', 
+                 value=self.rds_instance.instance_identifier, 
+                 description='RDS Instance Identifier')
+        CfnOutput(self, 'RdsSecretArn', 
+                 value=self.rds_instance.secret.secret_arn if hasattr(self.rds_instance, 'secret') else '',
+                 description='RDS Secret ARN')
+        
+        # ALB outputs
+        CfnOutput(self, 'AlbArn', value=self.alb.load_balancer_arn, description='ALB ARN')
+        CfnOutput(self, 'AlbDnsName', value=self.alb.load_balancer_dns_name, description='ALB DNS Name')
+        
+        # ASG outputs
+        CfnOutput(self, 'AsgName', 
+                 value=self.asg.auto_scaling_group_name, 
+                 description='Auto Scaling Group Name')
+        
+        # KMS outputs
+        CfnOutput(self, 'KmsKeyId', value=self.kms_key.key_id, description='KMS Key ID')
+        
+        # Route53 outputs
+        CfnOutput(self, 'HostedZoneId', 
+                 value=self.hosted_zone.hosted_zone_id, 
+                 description='Route53 Hosted Zone ID')
+        
+        # IAM outputs
+        CfnOutput(self, 'IamRoleName', 
+                 value=self.iam_role.role_name, 
+                 description='IAM Role Name')
+        CfnOutput(self, 'InstanceProfileName', 
+                 value=f'ec2-profile-{self.environment_suffix}',
+                 description='Instance Profile Name')
