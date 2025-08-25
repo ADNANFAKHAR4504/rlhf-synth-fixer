ba50dc604f32dcf24543f6507ed460fcdec2f6d6
jannatul-turing <jannatul.h@turing.com>
2025-08-25 19:55:58 +0530

    IAC-292030: First turn commit

---
+++ lib/outputs.tf
@@ -0,0 +1,64 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "security_group_ssh_id" {
+  description = "ID of the SSH security group"
+  value       = aws_security_group.ssh_access.id
+}
+
+output "security_group_web_id" {
+  description = "ID of the web security group"
+  value       = aws_security_group.web_access.id
+}
+
+output "s3_bucket_name" {
+  description = "Name of the secure S3 bucket"
+  value       = aws_s3_bucket.secure_bucket.bucket
+}
+
+output "kms_key_id" {
+  description = "ID of the KMS key"
+  value       = aws_kms_key.security_key.key_id
+}
+
+output "kms_key_arn" {
+  description = "ARN of the KMS key"
+  value       = aws_kms_key.security_key.arn
+}
+
+output "cloudtrail_name" {
+  description = "Name of the CloudTrail"
+  value       = aws_cloudtrail.main.name
+}
+
+output "guardduty_detector_id" {
+  description = "ID of the GuardDuty detector"
+  value       = aws_guardduty_detector.main.id
+}
+
+output "config_recorder_name" {
+  description = "Name of the Config recorder"
+  value       = aws_config_configuration_recorder.main.name
+}
+
+output "terraform_user_name" {
+  description = "Name of the Terraform IAM user"
+  value       = aws_iam_user.terraform_user.name
+}
+
+output "mfa_role_arn" {
+  description = "ARN of the MFA-required role"
+  value       = aws_iam_role.mfa_required_role.arn
+}
---
+++ lib/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "allowed_ssh_cidr" {
+  description = "CIDR block allowed for SSH access"
+  type        = string
+  default     = "203.0.113.0/32"  # Replace with your actual IP
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "SecurityConfiguration"
+}
