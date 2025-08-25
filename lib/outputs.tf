26b46d080f0959612b2e12d80dabbb07ce51bae1
sudhakar01 <sudhakarsingh23@gmail.com>
2025-08-24 21:20:47 -0600

    security-configuration-as-code_Terraform_HCL_g9b7k6d3f1c4s8p0

--- lib/outputs.tf
+++ lib/outputs.tf
@@ -1,19 +1,21 @@
+# outputs.tf
+
 output "cloudtrail_arn" {
-  value       = aws_cloudtrail.main.arn
   description = "ARN of the multi-region CloudTrail"
+  value       = aws_cloudtrail.main.arn
 }
 
 output "guardduty_detector_id" {
-  value       = aws_guardduty_detector.this.id
-  description = "GuardDuty detector ID"
+  description = "GuardDuty detector ID (null if disabled or pre-existing)"
+  value       = length(aws_guardduty_detector.this) > 0 ? aws_guardduty_detector.this[0].id : null
 }
 
 output "sns_topic_arn" {
-  value       = aws_sns_topic.security.arn
   description = "SNS topic for security alarms"
+  value       = aws_sns_topic.security.arn
 }
 
 output "logs_bucket_name" {
-  value       = aws_s3_bucket.logs.bucket
   description = "Primary S3 logs bucket name"
+  value       = aws_s3_bucket.logs.bucket
 }
