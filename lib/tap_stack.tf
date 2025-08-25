5228e5f164b78d06518d5fb989848c5d36a63e36
sivav-cmd <siva.v@turing.com>
2025-08-26 00:03:08 +0530

    s3 bucket issue fixes

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -239,7 +239,7 @@ resource "aws_db_instance" "main" {
 
 # --- S3 Bucket ---
 resource "aws_s3_bucket" "main" {
-  bucket = "${var.project_name}-bucket-${var.environment}"
+  bucket        = "${var.project_name}-bucket-${var.environment}"
   force_destroy = true
 
   tags = merge(local.tags, {
