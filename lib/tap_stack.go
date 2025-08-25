28d629a94b5eb4eb864ac1d62ba1b652da2fd212
ashish-a-turing <ashish.a2@turing.com>
2025-08-25 21:36:50 +0530

    fixed linting

--- lib/tap_stack.go
+++ lib/tap_stack.go
@@ -300,47 +300,4 @@ func main() {
 
 	// Synthesize the app to generate the Terraform configuration
 	app.Synth()
-}vironmentSuffix = "dev"
-	}
-
-	stateBucket := os.Getenv("TERRAFORM_STATE_BUCKET")
-	if stateBucket == "" {
-		stateBucket = "iac-rlhf-tf-states"
-	}
-
-	stateBucketRegion := os.Getenv("TERRAFORM_STATE_BUCKET_REGION")
-	if stateBucketRegion == "" {
-		stateBucketRegion = "us-east-1"
-	}
-
-	awsRegion := os.Getenv("AWS_REGION")
-	if awsRegion == "" {
-		awsRegion = "us-east-1"
-	}
-
-	repositoryName := os.Getenv("REPOSITORY")
-	if repositoryName == "" {
-		repositoryName = "unknown"
-	}
-
-	commitAuthor := os.Getenv("COMMIT_AUTHOR")
-	if commitAuthor == "" {
-		commitAuthor = "unknown"
-	}
-
-	// Calculate the stack name
-	stackName := fmt.Sprintf("TapStack%s", environmentSuffix)
-
-	// Create the TapStack with the calculated properties
-	NewTapStack(app, stackName, &TapStackProps{
-		EnvironmentSuffix: environmentSuffix,
-		StateBucket:       stateBucket,
-		StateBucketRegion: stateBucketRegion,
-		AwsRegion:         awsRegion,
-		RepositoryName:    repositoryName,
-		CommitAuthor:      commitAuthor,
-	})
-
-	// Synthesize the app to generate the Terraform configuration
-	app.Synth()
 }
