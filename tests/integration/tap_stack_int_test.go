1f66e79ac07ef9eca687544055ab57b01a3eb53c
Bala C <balasl342@gmail.com>
2025-08-26 11:43:45 +0530

    renamed the resource

--- tests/integration/tap_stack_int_test.go
+++ tests/integration/tap_stack_int_test.go
@@ -31,7 +31,7 @@ func loadDeploymentOutputs(t *testing.T) map[string]string {
 		return map[string]string{
 			"S3BucketName":    "secure-webapp-storage-test-12345",
 			"KMSKeyId":        "arn:aws:kms:us-east-1:123456789012:key/12345678-1234-1234-1234-123456789012",
-			"KMSKeyAlias":     "alias/s3-webappenc-key-test",
+			"KMSKeyAlias":     "alias/s3-web-app-enc-key-test",
 			"CloudWatchAlarm": "SecurityViolation-test",
 		}
 	}
@@ -169,7 +169,7 @@ func TestTapStackIntegrationDeployment(t *testing.T) {
 		// Get key alias from outputs or generate expected alias
 		keyAlias := outputs["KMSKeyAlias"]
 		if keyAlias == "" {
-			keyAlias = fmt.Sprintf("alias/s3-webappenc-key-%s", envSuffix)
+			keyAlias = fmt.Sprintf("alias/s3-web-app-enc-key-%s", envSuffix)
 		}
 
 		// Find the KMS key by alias
