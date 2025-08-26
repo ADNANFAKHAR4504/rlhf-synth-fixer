2ad7c4e8080f754ea61a5f41cf8b350e5ee441ef
Govardhan Yadava <govardhan.y@turing.com>
2025-08-27 00:23:29 +0530

    Fix ec2 instance type

--- lib/src/main/java/app/Main.java
+++ lib/src/main/java/app/Main.java
@@ -409,7 +409,7 @@ class SecureInfrastructureStack extends Stack {
                 .engine(DatabaseInstanceEngine.mysql(MySqlInstanceEngineProps.builder()
                         .version(MysqlEngineVersion.VER_8_0_35)
                         .build()))
-                .instanceType(software.amazon.awscdk.services.ec2.InstanceType.T3_MICRO)
+                .instanceType(software.amazon.awscdk.services.ec2.InstanceType.of("t3", "micro"))
                 .vpc(vpc)
                 .vpcSubnets(SubnetSelection.builder()
                         .subnetType(SubnetType.PRIVATE_ISOLATED)
