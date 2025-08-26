3038be9641d1f2f8b2de0efbd4adce2f7536a5ff
Chirag Patel <chirag.p@turing.com>
2025-08-26 13:52:26 +0530

    Fixed deployment issue

--- lib/src/main/java/app/Main.java
+++ lib/src/main/java/app/Main.java
@@ -334,7 +334,7 @@ public final class Main {
         
         // 5. S3 Buckets for CloudTrail logs
         var cloudTrailBucket = new Bucket("bucket-cloudtrail-logs", BucketArgs.builder()
-            .bucket("yourcompany-production-cloudtrail-logs-4251718")
+            .bucket("yourcompany-production-cloudtrail-logs-13469")
             .tags(getStandardTags(config, "storage", "s3"))
             .build(), CustomResourceOptions.builder()
             .deleteBeforeReplace(true)
