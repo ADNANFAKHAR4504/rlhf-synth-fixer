7d129cc90e3b638d05d47d89c8ae29959fc448be
rajendra-s-aws <rajendra.s@turing.com>
2025-08-26 17:41:03 +0530

    Update tap_stack_unit_test.go

--- tests/unit/tap_stack_unit_test.go
+++ tests/unit/tap_stack_unit_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/aws/jsii-runtime-go"
 )
 
-// synthNestedTemplate builds the app/stack and returns the Template of the nested stack "WebInfra-dev"
+// synthNestedTemplate builds the app/stack and returns the Template of the nested stack "WebInfra-dev" 
 func synthNestedTemplate(t *testing.T) assertions.Template {
 	t.Helper()
 
