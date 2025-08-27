d20d69d0f2352296a007e61da4efce5fa6953462
Harshita Agarwal <harshita.g@turing.com>
2025-08-27 20:03:20 +0530

    update tap.ts

--- bin/tap.ts
+++ bin/tap.ts
@@ -17,7 +17,6 @@ Tags.of(app).add('Repository', repositoryName);
 Tags.of(app).add('Author', commitAuthor);
 
 new TapStack(app, stackName, {
-  stackName: stackName, // This ensures CloudFormation stack name includes the suffix
   environmentSuffix: environmentSuffix, // Pass the suffix to the stack
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
