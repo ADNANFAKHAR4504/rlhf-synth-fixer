8e119159d316d0b91d5ff9a67f7b4cf4e4e6977e
root <root@DESKTOP-3KTOO66.localdomain>
2025-08-27 14:04:39 +0530

    adding integration test cases

--- tests/integration/tap_stack_int_test.go
+++ tests/integration/tap_stack_int_test.go
@@ -16,11 +16,12 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/rds"
 	rdstypes "github.com/aws/aws-sdk-go-v2/service/rds/types"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	s3types "github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-// DeploymentOutputs represents the Pulumi stack outputs
+// DeploymentOutputs holds Pulumi flat-outputs.json data
 type DeploymentOutputs struct {
 	Ec2InstanceId      string `json:"ec2InstanceId"`
 	Ec2PrivateIp       string `json:"ec2PrivateIp"`
@@ -62,32 +63,32 @@ func TestMain(m *testing.M) {
 }
 
 func TestVPCAndSubnets(t *testing.T) {
-	client := ec2.NewFromConfig(awsConfig)
+	ec2Client := ec2.NewFromConfig(awsConfig)
 
 	require.NotEmpty(t, outputs.VpcId)
-	resp, err := client.DescribeVpcs(ctx, &ec2.DescribeVpcsInput{VpcIds: []string{outputs.VpcId}})
+	resp, err := ec2Client.DescribeVpcs(ctx, &ec2.DescribeVpcsInput{VpcIds: []string{outputs.VpcId}})
 	require.NoError(t, err)
 	require.Len(t, resp.Vpcs, 1)
 	assert.Equal(t, "10.0.0.0/16", *resp.Vpcs[0].CidrBlock)
 
 	subnetIDs := []string{outputs.PrivateSubnetEc2Id, outputs.PrivateSubnetRdsId, outputs.PublicSubnetId}
-	subnetsResp, err := client.DescribeSubnets(ctx, &ec2.DescribeSubnetsInput{SubnetIds: subnetIDs})
+	subnetsResp, err := ec2Client.DescribeSubnets(ctx, &ec2.DescribeSubnetsInput{SubnetIds: subnetIDs})
 	require.NoError(t, err)
 	assert.GreaterOrEqual(t, len(subnetsResp.Subnets), len(subnetIDs))
 
 	azSet := map[string]bool{}
-	for _, sn := range subnetsResp.Subnets {
-		assert.Equal(t, outputs.VpcId, *sn.VpcId)
-		azSet[*sn.AvailabilityZone] = true
+	for _, subnet := range subnetsResp.Subnets {
+		assert.Equal(t, outputs.VpcId, *subnet.VpcId)
+		azSet[*subnet.AvailabilityZone] = true
 	}
 	assert.GreaterOrEqual(t, len(azSet), 2)
 }
 
 func TestEC2Instance(t *testing.T) {
-	client := ec2.NewFromConfig(awsConfig)
+	ec2Client := ec2.NewFromConfig(awsConfig)
 
 	require.NotEmpty(t, outputs.Ec2InstanceId)
-	resp, err := client.DescribeInstances(ctx, &ec2.DescribeInstancesInput{InstanceIds: []string{outputs.Ec2InstanceId}})
+	resp, err := ec2Client.DescribeInstances(ctx, &ec2.DescribeInstancesInput{InstanceIds: []string{outputs.Ec2InstanceId}})
 	require.NoError(t, err)
 	require.Len(t, resp.Reservations, 1)
 
@@ -106,10 +107,10 @@ func TestEC2Instance(t *testing.T) {
 }
 
 func TestRDSInstance(t *testing.T) {
-	client := rds.NewFromConfig(awsConfig)
+	rdsClient := rds.NewFromConfig(awsConfig)
 	require.NotEmpty(t, outputs.RdsEndpoint)
 
-	instancesResp, err := client.DescribeDBInstances(ctx, &rds.DescribeDBInstancesInput{})
+	instancesResp, err := rdsClient.DescribeDBInstances(ctx, &rds.DescribeDBInstancesInput{})
 	require.NoError(t, err)
 
 	var foundInstance *rdstypes.DBInstance
@@ -138,14 +139,15 @@ func TestRDSInstance(t *testing.T) {
 }
 
 func TestS3Bucket(t *testing.T) {
-	client := s3.NewFromConfig(awsConfig)
+	s3Client := s3.NewFromConfig(awsConfig)
 
 	require.NotEmpty(t, outputs.S3BucketName)
 
-	_, err := client.HeadBucket(ctx, &s3.HeadBucketInput{Bucket: &outputs.S3BucketName})
+	_, err := s3Client.HeadBucket(ctx, &s3.HeadBucketInput{Bucket: &outputs.S3BucketName})
 	assert.NoError(t, err)
 
-	versioning, err := client.GetBucketVersioning(ctx, &s3.GetBucketVersioningInput{Bucket: &outputs.S3BucketName})
+	versioning, err := s3Client.GetBucketVersioning(ctx, &s3.GetBucketVersioningInput{Bucket: &outputs.S3BucketName})
 	require.NoError(t, err)
-	assert.Equal(t, "Enabled", versioning.Status.String())
+
+	assert.Equal(t, s3types.BucketVersioningStatusEnabled, versioning.Status)
 }
