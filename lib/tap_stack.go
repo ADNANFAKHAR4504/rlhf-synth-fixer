6950233633cc88c0705cace8bda4da510403204f
Rahul Ladumor <42517460+rahulladumor@users.noreply.github.com>
2025-08-28 14:33:54 +0530

    refactor: move tap_stack from main to integration package and rename main to Run

--- lib/tap_stack.go
+++ lib/tap_stack.go
@@ -1,10 +1,11 @@
-package main
+package integration
 
 import (
 	"github.com/TuringGpt/iac-test-automations/lib/stack"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func main() {
+// Run starts the Pulumi program for integration purposes.
+func Run() {
 	pulumi.Run(stack.CreateTapStack)
 }
