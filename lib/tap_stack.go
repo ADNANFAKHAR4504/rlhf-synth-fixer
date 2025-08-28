adf1a09b2b6890ea078b1b061815f45f44b84b7d
Rahul Ladumor <42517460+rahulladumor@users.noreply.github.com>
2025-08-28 15:09:26 +0530

    refactor: move integration package to main for direct execution

--- lib/tap_stack.go
+++ lib/tap_stack.go
@@ -1,11 +1,10 @@
-package integration
+package main
 
 import (
 	"github.com/TuringGpt/iac-test-automations/lib/stack"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-// Run starts the Pulumi program for integration purposes.
-func Run() {
+func main() {
 	pulumi.Run(stack.CreateTapStack)
 }
