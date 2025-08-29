00938e86d0cf49159ddc0a5f6778569d3cd670ad
root <root@DESKTOP-3KTOO66.localdomain>
2025-08-29 19:23:17 +0530

    correcting provider tf file

--- lib/provider.tf
+++ lib/provider.tf
@@ -18,3 +18,12 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+provider "aws" {
+  alias  = "us_east_2"
+  region = var.primary_region
+}
+
+provider "aws" {
+  alias  = "us_west_1"
+  region = var.secondary_region
+}
