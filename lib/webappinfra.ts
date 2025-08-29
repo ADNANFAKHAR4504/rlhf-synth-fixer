cbe6aeb0046e13fb41a73ca39de16b57ea51706e
ashish-a-turing <ashish.a2@turing.com>
2025-08-29 21:43:29 +0530

    fixed linting

--- lib/webappinfra.ts
+++ lib/webappinfra.ts
@@ -1,6 +1,25 @@
 import * as aws from '@pulumi/aws';
 import * as pulumi from '@pulumi/pulumi';
 
+function getAlbServiceAccountId(region: string): string {
+  const albAccounts: Record<string, string> = {
+    'us-east-1': '127311923021',
+    'us-east-2': '033677994240',
+    'us-west-1': '027434742980',
+    'us-west-2': '797873946194',
+    'ap-south-1': '718504428378',
+    'ap-northeast-1': '582318560864',
+    'ap-northeast-2': '600734575887',
+    'ap-southeast-1': '114774131450',
+    'ap-southeast-2': '783225319266',
+    'eu-central-1': '054676820928',
+    'eu-west-1': '156460612806',
+    'eu-west-2': '652711504416',
+    'eu-west-3': '009996457667',
+  };
+  return albAccounts[region] || '127311923021';
+}
+
 export class WebAppInfrastructure {
   public readonly vpc: aws.ec2.Vpc;
   public readonly publicSubnets: aws.ec2.Subnet[];
@@ -231,6 +250,29 @@ export class WebAppInfrastructure {
             },
           },
         ],
+      },
+      { provider: this.provider }
+    );
+
+    new aws.s3.BucketPolicy(
+      `alb-logs-policy-${environment}`,
+      {
+        bucket: albLogsBucket.id,
+        policy: albLogsBucket.arn.apply(bucketArn =>
+          JSON.stringify({
+            Version: '2012-10-17',
+            Statement: [
+              {
+                Effect: 'Allow',
+                Principal: {
+                  AWS: `arn:aws:iam::${getAlbServiceAccountId(region)}:root`,
+                },
+                Action: 's3:PutObject',
+                Resource: `${bucketArn}/*`,
+              },
+            ],
+          })
+        ),
       },
       { provider: this.provider }
     );
