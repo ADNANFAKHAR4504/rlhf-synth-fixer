885c30b098ea86a4e4eae1299dcd7b8ab6961e91
Anthony Oride <oride.a@turing.com>
2025-09-01 23:33:02 +0100

    fixing comments

--- lib/src/main/java/app/Main.java
+++ lib/src/main/java/app/Main.java
@@ -3,9 +3,13 @@ package app;
 import com.hashicorp.cdktf.App;
 
 
-public class Main
-{
-    public static void main(String[] args) {
+public final class Main {
+
+    private Main() {
+        // Utility class should not be instantiated
+    }
+
+    public static void main(final String[] args) {
         final App app = new App();
         new MainStack(app, "cdktf-java");
         app.synth();
--- templates/cdktf-java/lib/src/main/java/app/Main.java
+++ templates/cdktf-java/lib/src/main/java/app/Main.java
@@ -6,9 +6,16 @@ import com.hashicorp.cdktf.App;
 import com.hashicorp.cdktf.TerraformStack;
 
 
-public class Main
-{
-    public static void main(String[] args) {
+public final class Main {
+
+    /**
+     * Private constructor to prevent instantiation of utility class.
+     */
+    private Main() {
+        // Utility class should not be instantiated
+    }
+
+    public static void main(final String[] args) {
         final App app = new App();
         new MainStack(app, "cdktf-java");
         app.synth();
