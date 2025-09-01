9cca0765bea5d701b448d9a34e563d6f2a58f2f8
rahulpatil-at-607332757880 <rahulpati118@yahoo.in>
2025-09-01 23:22:38 +0530

    fixed errors

--- lib/tap-stack.ts
+++ lib/tap-stack.ts
@@ -63,7 +63,32 @@ export class WebAppStack extends cdk.Stack {
     );
 
     // Grant ELB service account permission for ALB logs
-    const elbServiceAccount = new iam.AccountPrincipal('797873946194'); // us-west-2 ELB service account
+    // ELB service account IDs by region - AWS managed accounts for ALB access logs
+    const elbServiceAccountIds: { [region: string]: string } = {
+      'us-east-1': '127311923021',
+      'us-east-2': '033677994240',
+      'us-west-1': '027434742980',
+      'us-west-2': '797873946194',
+      'eu-west-1': '156460612806',
+      'eu-west-2': '652711504416',
+      'eu-west-3': '009996457667',
+      'eu-central-1': '054676820928',
+      'ap-northeast-1': '582318560864',
+      'ap-northeast-2': '600734575887',
+      'ap-southeast-1': '114774131450',
+      'ap-southeast-2': '783225319266',
+      'ap-south-1': '718504428378',
+      'sa-east-1': '507241528517',
+      'ca-central-1': '985666609251',
+      // Add more regions as needed
+    };
+
+    const elbServiceAccountId = elbServiceAccountIds[this.region];
+    if (!elbServiceAccountId) {
+      throw new Error(`ELB service account ID not found for region: ${this.region}. Please add the service account ID for this region.`);
+    }
+
+    const elbServiceAccount = new iam.AccountPrincipal(elbServiceAccountId);
     albLogsBucket.addToResourcePolicy(
       new iam.PolicyStatement({
         sid: 'AWSLogDeliveryAclCheck',
