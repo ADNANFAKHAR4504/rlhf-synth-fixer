b321f4e7b21cfc2efeebeae80a303cb5ec09dbc7
Harshita Agarwal <harshita.g@turing.com>
2025-09-02 18:06:56 +0530

    fix lint error

--- bin/tap.ts
+++ bin/tap.ts
@@ -6,8 +6,10 @@ const app = new App();
 
 // Get environment variables from the environment or use defaults
 const environmentSuffix = process.env.ENVIRONMENT_SUFFIX || 'dev';
-const stateBucket = process.env.TERRAFORM_STATE_BUCKET || 'prod-config-logs-us-west-2-a8e48bba';
-const stateBucketRegion = process.env.TERRAFORM_STATE_BUCKET_REGION || 'us-west-2';
+const stateBucket =
+  process.env.TERRAFORM_STATE_BUCKET || 'prod-config-logs-us-west-2-a8e48bba';
+const stateBucketRegion =
+  process.env.TERRAFORM_STATE_BUCKET_REGION || 'us-west-2';
 const awsRegion = process.env.AWS_REGION || 'us-west-2';
 const repositoryName = process.env.REPOSITORY || 'unknown';
 const commitAuthor = process.env.COMMIT_AUTHOR || 'unknown';
