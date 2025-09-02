9e9b05c367072e7d08b075e9344fa25a55b55e70
Siva Kumar <sk800489@gmail.com>
2025-09-02 17:58:10 +0530

    lint fix

--- lib/tap-stack.ts
+++ lib/tap-stack.ts
@@ -234,7 +234,7 @@ export class TapStack extends cdk.Stack {
         config.rdsInstanceSize !== ec2.InstanceSize.MICRO,
       performanceInsightRetention:
         config.rdsInstanceClass !== ec2.InstanceClass.T3 ||
-          config.rdsInstanceSize !== ec2.InstanceSize.MICRO
+        config.rdsInstanceSize !== ec2.InstanceSize.MICRO
           ? rds.PerformanceInsightRetention.DEFAULT
           : undefined,
     });
