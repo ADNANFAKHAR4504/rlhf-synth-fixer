cb1472a40f38e9f0ec4da3dea5f07a40a079a60e
abdullahj2 <abdullah.j2@turing.com>
2025-09-05 03:40:00 +0100

    CDK-PY-349205 task

--- tap.py
+++ tap.py
@@ -35,7 +35,7 @@ props = TapStackProps(
     environment_suffix=environment_suffix,
     env=cdk.Environment(
         account=os.getenv('CDK_DEFAULT_ACCOUNT'),
-        region=os.getenv('CDK_DEFAULT_REGION')
+        region="region=o"us-west-2""
     )
 )
 
