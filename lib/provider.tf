88dbb9332293bf2f7e7a27518aaa86b75c018585
root <root@DESKTOP-3KTOO66.localdomain>
2025-09-05 13:54:52 +0530

    fixing provder config

--- lib/provider.tf
+++ lib/provider.tf
@@ -17,4 +17,29 @@ terraform {
 # Primary AWS provider for general resources
 #provider "aws" {
 #  region = var.aws_region
-#}
+#
+}
+
+provider "aws" {
+  alias  = "us_east_2"
+  region = var.primary_region
+  #access_key = var.aws_access_key
+  #secret_key = var.aws_secret_key
+
+  #skip_metadata_api_check     = true
+  #skip_region_validation      = true
+  #skip_credentials_validation = true
+  #skip_requesting_account_id  = true
+}
+
+provider "aws" {
+  alias  = "us_west_1"
+  region = var.secondary_region
+  #access_key = var.aws_access_key
+  #secret_key = var.aws_secret_key
+
+  #skip_metadata_api_check     = true
+  #skip_region_validation      = true
+  #skip_credentials_validation = true
+  #skip_requesting_account_id  = true
+}
