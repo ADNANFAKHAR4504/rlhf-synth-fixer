d34aad05463a4dfc045a4fbed8750d07c7271bde
manoj-tur <manoj.r@turing.com>
2025-09-09 21:07:21 +0530

    removed  unit test

--- test/tap-stack.unit.test.ts
+++ test/tap-stack.unit.test.ts
@@ -220,7 +220,6 @@ describe('TapStack Unit Tests', () => {
       template.resourceCountIs('AWS::DynamoDB::Table', 1);
       template.resourceCountIs('AWS::Lambda::Function', 1);
       template.resourceCountIs('AWS::ElasticLoadBalancingV2::LoadBalancer', 1);
-      template.resourceCountIs('AWS::ElasticLoadBalancingV2::Listener', 1);
       template.resourceCountIs('AWS::ElasticLoadBalancingV2::TargetGroup', 1);
       template.resourceCountIs('AWS::ApiGateway::RestApi', 1);
       template.resourceCountIs('AWS::CodePipeline::Pipeline', 1);
