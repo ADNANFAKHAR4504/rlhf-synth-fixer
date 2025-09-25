486364ab6a11a09793995f9834e09185f0180368
root <root@DESKTOP-3KTOO66.localdomain>
2025-09-25 18:10:53 +0530

    IAC 348912 Security Configuration as Code

--- lib/provider.tf
+++ lib/provider.tf
@@ -12,11 +12,6 @@ terraform {
 
   # Partial backend config: values are injected at `terraform init` time
   backend "s3" {
-    bucket         = "iac-rlhf-tf-states"
-    key            = "global/terraform.tfstate"
-    region         = "us-east-1"
-   # dynamodb_table = "iac-rlhf-tf-locks" # optional for state locking
-    encrypt        = true
   }
 # Primary AWS provider for general resources
 provider "aws" {
