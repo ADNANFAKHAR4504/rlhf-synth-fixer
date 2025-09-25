a6598245df8f41a9d78c5facdcbaf1aa1e09a0e9
root <root@DESKTOP-3KTOO66.localdomain>
2025-09-25 18:35:39 +0530

    making fix for aws flow logs

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -308,15 +308,10 @@ resource "aws_iam_role_policy" "vpc_flow_logs" {
 
 # VPC Flow Logs
 resource "aws_flow_log" "main" {
-  vpc_id       = aws_vpc.main.id
-  traffic_type = "ALL"
-  iam_role_arn = aws_iam_role.vpc_flow_logs.arn
-
-  log_destination {
-    log_group {
-      arn = aws_cloudwatch_log_group.vpc_flow_logs.arn
-    }
-  }
+  vpc_id          = aws_vpc.main.id
+  traffic_type    = "ALL"
+  log_destination = aws_cloudwatch_log_group.vpc_flow_logs.arn
+  iam_role_arn    = aws_iam_role.vpc_flow_logs.arn
 
   tags = merge(local.common_tags, {
     Name = "main-vpc-flow-log-${var.resource_suffix}"
