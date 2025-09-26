62c38919f80b23364b6ba7e8abf05d8899a6bbeb
sreeni-ops <sreenivas.k@turing.com>
2025-09-26 13:59:32 +0530

    Added delete stack and wait for delete to complete

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -91,7 +91,8 @@ elif [ "$PLATFORM" = "cfn" ] && [ "$LANGUAGE" = "yaml" ]; then
 elif [ "$PLATFORM" = "cfn" ] && [ "$LANGUAGE" = "json" ]; then
   echo "✅ CloudFormation JSON project detected, deploying with AWS CLI..."
   aws cloudformation describe-stack-events --stack-name TapStackpr3067 --region us-east-1
-  # aws cloudformation delete-stack --stack-name TapStackpr3067 --region us-east-1
+  aws cloudformation delete-stack --stack-name TapStackpr3067 --region us-east-1
+  aws cloudformation wait stack-delete-complete --stack-name TapStackpr3067 --region us-east-1
   npm run cfn:deploy-json
 
 elif [ "$PLATFORM" = "tf" ]; then
