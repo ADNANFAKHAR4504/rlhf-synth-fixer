3d3f70559353fa825971a9fd0b2d1a1b2717edaf
Emmanuel Nyachoke <nyachoke.e@turing.com>
2025-09-29 15:57:00 +0300

    Add random and archive

--- lib/provider.tf
+++ lib/provider.tf
@@ -8,6 +8,14 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4"
+    }
   }
 
   # Partial backend config: values are injected at `terraform init` time
