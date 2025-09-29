3bd109695a73c1399ea419b73bb8ebcda4ff70d6
Ashwin R <ashwin1@netapp.com>
2025-09-29 10:44:55 +0530

    added modal response

---
+++ test/tap-stack.unit.test.ts
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+
+const environmentSuffix = process.env.ENVIRONMENT_SUFFIX || 'dev';
+
+describe('TapStack CloudFormation Template', () => {
+  let template: any;
+
+  beforeAll(() => {
+    // If youre testing a yaml template. run `pipenv run cfn-flip-to-json > lib/TapStack.json`
+    // Otherwise, ensure the template is in JSON format.
+    const templatePath = path.join(__dirname, '../lib/TapStack.json');
+    const templateContent = fs.readFileSync(templatePath, 'utf8');
+    template = JSON.parse(templateContent);
+  });
+
+  describe('Write Integration TESTS', () => {
+    test('Dont forget!', async () => {
+      expect(true).toBe(true);
+    });
+  });
+
+
+});
