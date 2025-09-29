86dfd9569d8603bee5e91345c255a95175eae48b
Emmanuel Nyachoke <nyachoke.e@turing.com>
2025-09-29 15:46:23 +0300

    Remove unused backend

--- lib/versions.tf
+++ lib/versions.tf
@@ -11,19 +11,6 @@ terraform {
       version = "~> 3.5"
     }
   }
-
-  # Remote state configuration example
-  backend "s3" {
-    # These values should be configured during terraform init
-    # terraform init -backend-config="bucket=my-terraform-state" \
-    #               -backend-config="key=secure-env/terraform.tfstate" \
-    #               -backend-config="region=ap-southeast-1"
-    # bucket         = "my-terraform-state-bucket"
-    # key            = "secure-env/terraform.tfstate"
-    # region         = "ap-southeast-1"
-    # encrypt        = true
-    # dynamodb_table = "terraform-state-lock"
-  }
 }
 
 
