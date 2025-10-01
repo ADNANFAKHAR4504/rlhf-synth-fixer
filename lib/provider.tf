c6f3172cd973cc2c8c0a550a00fefda650090e54
David Michael <davidlearn27@gmail.com>
2025-10-01 23:08:22 +0100

    feat(provider): Add aliased AWS providers for us-east-1 and us-west-2

--- lib/provider.tf
+++ lib/provider.tf
@@ -18,3 +18,15 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+# Aliased provider for us-east-1
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
+# Aliased provider for us-west-2
+provider "aws" {
+  alias  = "us_west_2"
+  region = "us-west-2"
+}
