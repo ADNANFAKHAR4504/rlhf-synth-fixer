ebfe871ec8220056806ed73459f4ab231e2a7a7d
anthonyoride007 <anthonyoride@gmail.com>
2025-10-01 23:21:16 +0100

    task init

--- lib/src/main/java/app/config/ComputeStackConfig.java
+++
@@ -1,8 +0,0 @@
-package app.config;
-
-import app.stacks.MonitoringStack;
-import app.stacks.NetworkStack;
-import app.stacks.StorageStack;
-
-public record ComputeStackConfig(NetworkStack network, StorageStack storage, MonitoringStack monitoring) {
-}
