f63bc07c0e1234b3deb6e3c3f5d8d563d518151d
Emmanuel Nyachoke <nyachoke.e@turing.com>
2025-10-01 21:12:24 +0300

    Deplyment

--- lib/outputs.tf
+++ lib/outputs.tf
@@ -10,7 +10,7 @@ output "cloudfront_domain_name" {
 
 output "api_gateway_url" {
   description = "URL of the API Gateway"
-  value       = aws_api_gateway_deployment.main.invoke_url
+  value       = "https://${aws_api_gateway_rest_api.main.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.main.stage_name}"
 }
 
 output "rds_endpoint" {
