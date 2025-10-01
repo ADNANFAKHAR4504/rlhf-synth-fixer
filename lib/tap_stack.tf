e0d68003b912026ab1191f0803fe20e2a0fa2c70
jpoola-turing <ubuntu@Jaquar>
2025-10-01 18:03:07 +0530

    Add AWS region

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -1,5 +1,11 @@
 # Input Variables
 # -----------------------------
+variable "aws_region" {
+  description = "AWS region for all resources"
+  type        = string
+  default     = "us-west-2"
+}
+# -----------------------------
 
 variable "lambda_allowed_ips" {
   description = "List of known IP addresses allowed to access API Gateway"
