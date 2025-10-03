fdd086bbbdcbafee7bbea87bb39e39208a254f80
Joshua Okorie <okorie.j@turing.com>
2025-10-03 16:15:12 -0700

    fix for build

--- lib/src/main/java/app/Main.java
+++ lib/src/main/java/app/Main.java
@@ -954,59 +954,6 @@ class TapStack extends Stack {
         return new WebSocketResources(webSocketApi, stage);
     }
 
-    private AppSyncResources createAppSyncApi(final AuthResources auth) {
-    String inlineSchema = 
-            "type Query {\n" +
-            "  getDocument(documentId: ID!): Document\n" +
-            "  listDocuments(limit: Int, nextToken: String): DocumentConnection\n" +
-            "}\n\n" +
-            "type Mutation {\n" +
-            "  createDocument(input: CreateDocumentInput!): Document\n" +
-            "  updateDocument(input: UpdateDocumentInput!): Document\n" +
-            "  deleteDocument(documentId: ID!): Document\n" +
-            "}\n\n" +
-            "type Document {\n" +
-            "  documentId: ID!\n" +
-            "  title: String!\n" +
-            "  content: String!\n" +
-            "  createdAt: AWSDateTime!\n" +
-            "  updatedAt: AWSDateTime!\n" +
-            "  owner: String!\n" +
-            "}\n\n" +
-            "type DocumentConnection {\n" +
-            "  items: [Document!]!\n" +
-            "  nextToken: String\n" +
-            "}\n\n" +
-            "input CreateDocumentInput {\n" +
-            "  title: String!\n" +
-            "  content: String!\n" +
-            "}\n\n" +
-            "input UpdateDocumentInput {\n" +
-            "  documentId: ID!\n" +
-            "  title: String\n" +
-            "  content: String\n" +
-            "}\n";
-    
-    GraphqlApi api = new GraphqlApi(this, "DocumentCollabAPI" + environmentSuffix,
-            GraphqlApiProps.builder()
-                    .name("DocumentCollabGraphQLAPI-" + environmentSuffix)
-                    .schema(SchemaFile.fromString(inlineSchema))
-                    .authorizationConfig(AuthorizationConfig.builder()
-                            .defaultAuthorization(AuthorizationMode.builder()
-                                    .authorizationType(AuthorizationType.USER_POOL)
-                                    .userPoolConfig(UserPoolConfig.builder()
-                                            .userPool(auth.getUserPool())
-                                            .build())
-                                    .build())
-                            .build())
-                    .xrayEnabled(true)
-                    .build());
-
-    return new AppSyncResources(api);
-}
-
-
-
     private StateMachine createStepFunctionsWorkflow(final LambdaResources lambdas,
                                                      final DocumentDataResources data) {
         Role stepFunctionsRole = new Role(this, "StepFunctionsRole" + environmentSuffix,
@@ -1204,7 +1151,7 @@ class TapStack extends Stack {
                     .value(resources.getAppSync().getApi().getGraphqlUrl())
                     .exportName("AppSyncApiUrl-" + environmentSuffix)
                     .build());
-                    
+
             new CfnOutput(this, "AppSyncApiId", CfnOutputProps.builder()
                     .description("AppSync GraphQL API ID")
                     .value(resources.getAppSync().getApi().getApiId())
