ed8abed6112753ed8d1f2888e4caccb6450b7012
harshita-turing <harshita.g@turing.com>
2025-10-08 00:45:40 +0530

    fix build

--- lib/main.ts
+++ lib/main.ts
@@ -1,15 +1,15 @@
-import { App } from "cdktf";
-import { FinancialProcessorStack } from "./financial-processor-stack";
+import { App } from 'cdktf';
+import { FinancialProcessorStack } from './financial-processor-stack';
 
 const app = new App();
 
-new FinancialProcessorStack(app, "financial-processor", {
-  environment: "production",
-  appName: "financial-processor",
-  costCenter: "FinOps",
-  primaryRegion: "us-east-1",
-  secondaryRegion: "us-west-2",
-  domainName: "financial-processor.example.com",
+new FinancialProcessorStack(app, 'financial-processor', {
+  environment: 'production',
+  appName: 'financial-processor',
+  costCenter: 'FinOps',
+  primaryRegion: 'us-east-1',
+  secondaryRegion: 'us-west-2',
+  domainName: 'financial-processor.example.com',
 });
 
 app.synth();
