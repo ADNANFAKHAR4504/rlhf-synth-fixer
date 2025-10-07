167bbe0edef57f563cb9c9b4ae368834d1902f00
adnan fakhar <afakhar@cloud303.io>
2025-10-07 21:54:58 +0500

    chore: remove generated JS and declaration files; keep only source TS files in lib and bin

--- lib/backup-infrastructure-stack.d.ts
+++
@@ -1,9 +0,0 @@
-import { Construct } from 'constructs';
-interface BackupInfrastructureStackProps {
-    region: string;
-    environmentSuffix?: string;
-}
-export declare class BackupInfrastructureStack extends Construct {
-    constructor(scope: Construct, id: string, props: BackupInfrastructureStackProps);
-}
-export {};
