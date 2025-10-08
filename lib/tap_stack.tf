51797e7c434933a94fb48734e1c373d0e256ac5d
Emmanuel <emmanuelogunfowokan@gmail.com>
2025-10-08 21:17:23 +0100

    Resolve deploy errors

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -355,7 +355,6 @@ resource "aws_iam_role_policy" "lambda_failover" {
         Effect = "Allow"
         Action = [
           "ec2:CreateNetworkInterface",
-          "ec2:DescribeNetworkInterfaces",
           "ec2:DeleteNetworkInterface"
         ]
         Resource = [
@@ -363,6 +362,13 @@ resource "aws_iam_role_policy" "lambda_failover" {
           "arn:aws:ec2:${var.aws_region}:${data.aws_caller_identity.current.account_id}:subnet/*",
           "arn:aws:ec2:${var.aws_region}:${data.aws_caller_identity.current.account_id}:security-group/*"
         ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeNetworkInterfaces"
+        ]
+        Resource = "*"
       },
       {
         Effect = "Allow"
