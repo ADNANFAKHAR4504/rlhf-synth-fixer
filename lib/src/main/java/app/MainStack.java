e52d1edb1540ce9e5549a0def5722d1fce77041b
anthonyoride007 <anthonyoride@gmail.com>
2025-10-12 02:25:30 +0100

    feat(tests): added integration and unit tests

--- lib/src/main/java/app/MainStack.java
+++ lib/src/main/java/app/MainStack.java
@@ -10,6 +10,8 @@ import app.constructs.NetworkConstruct;
 import app.constructs.EcsClusterConstruct;
 import app.constructs.ServiceDiscoveryConstruct;
 import app.constructs.ServiceConstruct;
+import com.hashicorp.cdktf.TerraformOutput;
+import com.hashicorp.cdktf.TerraformOutputConfig;
 import com.hashicorp.cdktf.providers.aws.subnet.Subnet;
 import com.hashicorp.cdktf.providers.tls.provider.TlsProvider;
 import software.constructs.Construct;
@@ -68,13 +70,120 @@ public class MainStack extends TerraformStack {
                 network.getVpc().getId(), network.getPrivateSubnets().stream().map(Subnet::getId).toList(),
                 loadBalancer.getTargetGroups(), serviceDiscovery.getServices(), monitoringConfig.logRetentionDays());
 
+        List<ServiceConstruct> services = new java.util.ArrayList<>();
         for (ServiceConfig serviceConfig : getServiceConfigs(deploymentConfig)) {
-            new ServiceConstruct(this, serviceConfig.serviceName(), serviceConfig);
+            services.add(new ServiceConstruct(this, serviceConfig.serviceName(), serviceConfig));
         }
 
         // Create Monitoring
-        new MonitoringConstruct(this, "monitoring", monitoringConfig, cluster.getCluster().getName(),
-                getServiceConfigs(null));
+        MonitoringConstruct monitoring = new MonitoringConstruct(this, "monitoring", monitoringConfig,
+                cluster.getCluster().getName(), getServiceConfigs(null));
+
+        // Output VPC information
+        new TerraformOutput(this, "vpcId", TerraformOutputConfig.builder()
+                .value(network.getVpc().getId())
+                .description("VPC ID")
+                .build());
+
+        new TerraformOutput(this, "vpcCidr", TerraformOutputConfig.builder()
+                .value(network.getVpc().getCidrBlock())
+                .description("VPC CIDR Block")
+                .build());
+
+        // Output Subnet information
+        new TerraformOutput(this, "publicSubnetIds", TerraformOutputConfig.builder()
+                .value(String.join(",", network.getPublicSubnets().stream().map(Subnet::getId).toList()))
+                .description("Public Subnet IDs")
+                .build());
+
+        new TerraformOutput(this, "privateSubnetIds", TerraformOutputConfig.builder()
+                .value(String.join(",", network.getPrivateSubnets().stream().map(Subnet::getId).toList()))
+                .description("Private Subnet IDs")
+                .build());
+
+        // Output ECS Cluster information
+        new TerraformOutput(this, "ecsClusterName", TerraformOutputConfig.builder()
+                .value(cluster.getCluster().getName())
+                .description("ECS Cluster Name")
+                .build());
+
+        new TerraformOutput(this, "ecsClusterArn", TerraformOutputConfig.builder()
+                .value(cluster.getCluster().getArn())
+                .description("ECS Cluster ARN")
+                .build());
+
+        // Output Load Balancer information
+        new TerraformOutput(this, "albArn", TerraformOutputConfig.builder()
+                .value(loadBalancer.getAlb().getArn())
+                .description("Application Load Balancer ARN")
+                .build());
+
+        new TerraformOutput(this, "albDnsName", TerraformOutputConfig.builder()
+                .value(loadBalancer.getAlb().getDnsName())
+                .description("Application Load Balancer DNS Name")
+                .build());
+
+        new TerraformOutput(this, "albUrl", TerraformOutputConfig.builder()
+                .value("https://" + loadBalancer.getAlb().getDnsName())
+                .description("Application Load Balancer URL")
+                .build());
+
+        // Output Service Discovery information
+        new TerraformOutput(this, "serviceDiscoveryNamespace", TerraformOutputConfig.builder()
+                .value(serviceDiscovery.getNamespace().getName())
+                .description("Service Discovery Namespace")
+                .build());
+
+        new TerraformOutput(this, "serviceDiscoveryNamespaceId", TerraformOutputConfig.builder()
+                .value(serviceDiscovery.getNamespace().getId())
+                .description("Service Discovery Namespace ID")
+                .build());
+
+        // Output Target Group ARNs
+        loadBalancer.getTargetGroups().forEach((name, tg) -> {
+            new TerraformOutput(this, name + "TargetGroupArn", TerraformOutputConfig.builder()
+                    .value(tg.getArn())
+                    .description(name + " Target Group ARN")
+                    .build());
+        });
+
+        // Output ECS Service information
+        for (int i = 0; i < services.size(); i++) {
+            ServiceConstruct service = services.get(i);
+            ServiceConfig serviceConfig = getServiceConfigs(deploymentConfig).get(i);
+            String serviceName = serviceConfig.serviceName();
+
+            new TerraformOutput(this, serviceName + "ServiceName", TerraformOutputConfig.builder()
+                    .value(service.getService().getName())
+                    .description(serviceName + " ECS Service Name")
+                    .build());
+
+            new TerraformOutput(this, serviceName + "ServiceArn", TerraformOutputConfig.builder()
+                    .value(service.getService().getId())
+                    .description(serviceName + " ECS Service ARN")
+                    .build());
+
+            new TerraformOutput(this, serviceName + "TaskDefinitionArn", TerraformOutputConfig.builder()
+                    .value(service.getTaskDefinition().getArn())
+                    .description(serviceName + " Task Definition ARN")
+                    .build());
+
+            new TerraformOutput(this, serviceName + "SecurityGroupId", TerraformOutputConfig.builder()
+                    .value(service.getSecurityGroup().getId())
+                    .description(serviceName + " Security Group ID")
+                    .build());
+        }
+
+        // Output Monitoring information
+        new TerraformOutput(this, "snsTopicArn", TerraformOutputConfig.builder()
+                .value(monitoring.getAlarmTopic().getArn())
+                .description("SNS Topic ARN for Alarms")
+                .build());
+
+        new TerraformOutput(this, "cloudWatchAlarmCount", TerraformOutputConfig.builder()
+                .value(String.valueOf(monitoring.getAlarms().size()))
+                .description("Number of CloudWatch Alarms")
+                .build());
     }
 
     private List<ServiceConfig> getServiceConfigs(final ServiceConfig.DeploymentConfig config) {
