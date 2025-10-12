8bf830e78ca16a0957294db65a42031c19a3595b
harshita-turing <harshita.g@turing.com>
2025-10-12 19:18:27 +0530

    fix: update integration tests to remove placeholder test

--- test/tap-stack.int.test.ts
+++ test/tap-stack.int.test.ts
@@ -1,10 +1,3 @@
-describe('Turn Around Prompt API Integration Tests', () => {
-  describe('Write Integration TESTS', () => {
-    test('Dont forget!', async () => {
-      expect(false).toBe(true);
-    });
-  });
-});
 import {
   AutoScalingClient,
   DescribeAutoScalingGroupsCommand,
