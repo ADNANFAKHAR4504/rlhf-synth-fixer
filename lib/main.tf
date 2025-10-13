2398eac9edb047b82a9201638b3e14d8d70233f3
rajeshkumar <kanakatla.k@turing.com>
2025-10-13 16:59:53 +0530

    fix(terraform): resolve S3 CORS configuration issue
    Remove OPTIONS method from CORS config.
    
    Fix unit test regex patterns and update for jsonencode.
    
    Add documentation to MODEL_FAILURES.md.
    
    All 69 unit + 33 integration tests pass

--- lib/main.tf
+++ lib/main.tf
@@ -1,6 +1,6 @@
 # Variables
 variable "aws_region" {
-  description = "The AWS region where resources will be create"
+  description = "The AWS region where resources will be created"
   type        = string
   default     = "us-west-2"
 }
