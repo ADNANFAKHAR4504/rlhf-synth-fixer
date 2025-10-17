8fc7fb08e0bd06a9d7626f1f26031d4c53610eae
rajendra-s-aws <rajendra.s@turing.com>
2025-10-17 11:05:37 +0530

    feat(synth-4474569941): remove outer scope file

--- __main__.py
+++
@@ -1,9 +0,0 @@
-import pulumi
-from lib.tap_stack import TapStack
-
-# Get configuration
-config = pulumi.Config()
-environment_suffix = config.get("environmentSuffix") or "dev"
-
-# Create the IoT monitoring stack
-stack = TapStack(environment_suffix)
