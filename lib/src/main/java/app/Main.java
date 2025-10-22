e24dc3d542e0cbb28ff66286db59a0fc200fa226
Joshua Okorie <okorie.j@turing.com>
2025-10-22 08:47:51 -0700

    build: update java bundling to pass linting stage

--- lib/src/main/java/app/Main.java
+++ lib/src/main/java/app/Main.java
@@ -775,7 +775,9 @@ class ComputeStack extends Stack {
                         .image(Runtime.JAVA_17.getBundlingImage())
                         .command(Arrays.asList(
                                 "/bin/sh", "-c",
-                                "mvn clean package && cp /asset-input/target/*-shaded.jar /asset-output/routing.jar"
+                                "mvn clean package && " 
+                                + "cp /asset-input/target/lambda-functions-1.0.0-SNAPSHOT-shaded.jar "
+                                + "/asset-output/routing.jar"
                         ))
                         .build())
                     .build()))
@@ -1074,7 +1076,9 @@ class RealTimeStack extends Stack {
                                 .image(Runtime.JAVA_17.getBundlingImage())
                                 .command(Arrays.asList(
                                         "/bin/sh", "-c",
-                                        "mvn clean package && cp /asset-input/target/*-shaded.jar /asset-output/" + functionType.toLowerCase() + ".jar"
+                                        "mvn clean package && "  
+                                        + "cp /asset-input/target/lambda-functions-1.0.0-SNAPSHOT-shaded.jar "
+                                        + "/asset-output/" + functionType.toLowerCase() + ".jar"
                                 ))
                                 .build())
                         .build()))
@@ -1114,7 +1118,9 @@ class RealTimeStack extends Stack {
                                 .image(Runtime.JAVA_17.getBundlingImage())
                                 .command(Arrays.asList(
                                         "/bin/sh", "-c",
-                                        "mvn clean package && cp /asset-input/target/*-shaded.jar /asset-output/notification.jar"
+                                        "mvn clean package && " 
+                                        + "cp /asset-input/target/lambda-functions-1.0.0-SNAPSHOT-shaded.jar " 
+                                        + "/asset-output/notification.jar"
                                 ))
                                 .build())
                         .build()))
