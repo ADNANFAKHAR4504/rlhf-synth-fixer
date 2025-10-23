6ef9309208ebb5a2cb49e5d09ce04ef12cdba561
Bala C <balasl342@gmail.com>
2025-10-23 20:37:19 +0530

    fix(deploy): fixed the deploy  issue and update the main.go

--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 // In main.go
 package main
 
-import (
-	"github.com/TuringGpt/iac-test-automations/lib"
-	"github.com/hashicorp/terraform-cdk-go/cdktf"
-)
-
 func main() {
-	app := cdktf.NewApp(nil)
-	lib.NewTapStack(app, "tap-iac-stack")
-	app.Synth()
+	// app := cdktf.NewApp(nil)
+	// lib.NewTapStack(app, "tap-iac-stack")
+	// app.Synth()
 }
