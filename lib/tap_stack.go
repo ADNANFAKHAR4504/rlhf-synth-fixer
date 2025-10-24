00d5d3a0150668c92eb7a9b90188861169193f29
Mayank Sethi <sethi.m@turing.com>
2025-10-24 18:04:08 +0530

    fix: unit test issues

--- lib/tap_stack.go
+++ lib/tap_stack.go
@@ -1,10 +1,9 @@
-package main
+package lib
 
 import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"path/filepath"
 
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/appautoscaling"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/cloudwatch"
@@ -936,19 +935,4 @@ func CreateStack(ctx *pulumi.Context) error {
 	ctx.Export("ecsLogGroupName", ecsLogGroup.Name)
 
 	return nil
-}
-
-func main() {
-	// Set TMPDIR to a location that allows execution
-	// Avoid /tmp which may have noexec mount flag
-	homeDir, err := os.UserHomeDir()
-	if err == nil {
-		tmpDir := filepath.Join(homeDir, ".pulumi", "tmp")
-		os.MkdirAll(tmpDir, 0755)
-		os.Setenv("TMPDIR", tmpDir)
-	}
-
-	pulumi.Run(func(ctx *pulumi.Context) error {
-		return CreateStack(ctx)
-	})
 }
--- main.go
+++ main.go
@@ -1,8 +1,24 @@
-// In main.go
 package main
 
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/TuringGpt/iac-test-automations/lib"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
 func main() {
-	// app := cdktf.NewApp(nil)
-	// lib.NewTapStack(app, "tap-iac-stack")
-	// app.Synth()
+	// Set TMPDIR to a location that allows execution
+	// Avoid /tmp which may have noexec mount flag
+	homeDir, err := os.UserHomeDir()
+	if err == nil {
+		tmpDir := filepath.Join(homeDir, ".pulumi", "tmp")
+		os.MkdirAll(tmpDir, 0755)
+		os.Setenv("TMPDIR", tmpDir)
+	}
+
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		return lib.CreateStack(ctx)
+	})
 }
