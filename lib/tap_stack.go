a4ec22d456bc5fee8bdf70db5953bd3e41923c19
Bala C <balasl342@gmail.com>
2025-10-24 14:24:02 +0530

    fix(deploy): fixed the file path permission issue

--- lib/tap_stack.go
+++ lib/tap_stack.go
@@ -1,9 +1,10 @@
-package lib
+package main
 
 import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/appautoscaling"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/cloudwatch"
@@ -932,4 +933,19 @@ func CreateStack(ctx *pulumi.Context) error {
 	ctx.Export("ecsLogGroupName", ecsLogGroup.Name)
 
 	return nil
+}
+
+func main() {
+	// Set TMPDIR to a location that allows execution
+	// Avoid /tmp which may have noexec mount flag
+	homeDir, err := os.UserHomeDir()
+	if err == nil {
+		tmpDir := filepath.Join(homeDir, ".pulumi", "tmp")
+		os.MkdirAll(tmpDir, 0755)
+		os.Setenv("TMPDIR", tmpDir)
+	}
+
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		return CreateStack(ctx)
+	})
 }
