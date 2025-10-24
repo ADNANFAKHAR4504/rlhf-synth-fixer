41a3d08976bc86214f2ca97949ae08a945bffecc
Bala C <balasl342@gmail.com>
2025-10-24 21:41:50 +0530

    fix(deploy): fixed the deployment issue

--- lib/tap_stack.go
+++ lib/tap_stack.go
@@ -1,4 +1,4 @@
-package lib
+package main
 
 import (
 	"encoding/json"
@@ -17,17 +17,6 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
-
-func init() {
-	// Set TMPDIR to a location that allows execution
-	// Avoid /tmp which may have noexec mount flag in CI environments
-	homeDir, err := os.UserHomeDir()
-	if err == nil {
-		tmpDir := filepath.Join(homeDir, ".pulumi", "tmp")
-		os.MkdirAll(tmpDir, 0755)
-		os.Setenv("TMPDIR", tmpDir)
-	}
-}
 
 func CreateStack(ctx *pulumi.Context) error {
 	cfg := config.New(ctx, "")
@@ -947,4 +936,19 @@ func CreateStack(ctx *pulumi.Context) error {
 	ctx.Export("ecsLogGroupName", ecsLogGroup.Name)
 
 	return nil
+}
+
+func main() {
+	// Set TMPDIR to a location that allows execution
+	// Avoid /tmp which may have noexec mount flag
+	homeDir, err := os.UserHomeDir()
+	if err == nil {
+		tmpDir := filepath.Join(homeDir, ".pulumi", "tmp")
+		os.MkdirAll(tmpDir, 0755)
+		os.Setenv("TMPDIR", tmpDir)
+	}
+
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		return CreateStack(ctx)
+	})
 }
