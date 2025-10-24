fb9323245f2616c4d18eb3cf49ab8eefc9baebeb
Mayank Sethi <sethi.m@turing.com>
2025-10-24 18:30:36 +0530

    fix: deployment

--- lib/tap_stack.go
+++ lib/tap_stack.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/appautoscaling"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/cloudwatch"
@@ -16,6 +17,17 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
+
+func init() {
+	// Set TMPDIR to a location that allows execution
+	// Avoid /tmp which may have noexec mount flag in CI environments
+	homeDir, err := os.UserHomeDir()
+	if err == nil {
+		tmpDir := filepath.Join(homeDir, ".pulumi", "tmp")
+		os.MkdirAll(tmpDir, 0755)
+		os.Setenv("TMPDIR", tmpDir)
+	}
+}
 
 func CreateStack(ctx *pulumi.Context) error {
 	cfg := config.New(ctx, "")
