4eca5ee484054afea2f44ec8cfba39dce3a90ce5
Devang Pandya <devang.p@turing.com>
2025-10-28 21:35:53 +0530

    fix: remove Application Auto Scaling for Aurora Serverless v2
    - Aurora Serverless v2 does NOT use Application Auto Scaling API
    - Remove aws_appautoscaling_target (unsupported scalable_dimension)
    - Remove aws_appautoscaling_policy (scaling is automatic)
    - Remove aws_appautoscaling_scheduled_action (not supported)
    - Remove aws_cloudwatch_log_metric_filter (log group doesn't exist yet)
    
    Key Information:
    - Serverless v2 scales automatically via serverlessv2_scaling_configuration
    - Min/max capacity is set in main.tf, no separate API needed
    - Scaling happens automatically based on CPU and connections
    - Application Auto Scaling is for Aurora provisioned instances only
    
    Fixes errors:
    - ValidationException: scalableDimension not in enum
    - ResourceNotFoundException: log group does not exist

--- lib/autoscaling.tf
+++ lib/autoscaling.tf
@@ -1,90 +1,19 @@
-# autoscaling.tf - Auto-scaling policies for Aurora Serverless
+# autoscaling.tf - Auto-scaling configuration for Aurora Serverless v2
 
-# Application Auto Scaling target for Aurora
-resource "aws_appautoscaling_target" "aurora_serverless" {
-  service_namespace  = "rds"
-  scalable_dimension = "rds:cluster:ServerlessV2ScalingConfiguration"
-  resource_id        = "cluster:${aws_rds_cluster.aurora_serverless.cluster_identifier}"
-  min_capacity       = floor(var.aurora_min_capacity)
-  max_capacity       = floor(var.aurora_max_capacity)
+# Note: Aurora Serverless v2 automatically scales based on the serverlessv2_scaling_configuration
+# defined in the RDS cluster resource. Application Auto Scaling API is NOT used for Serverless v2.
+# The scaling happens automatically within the min_capacity and max_capacity bounds.
 
-  depends_on = [aws_rds_cluster_instance.aurora_instance]
-}
+# For reference: The following resources are NOT needed for Aurora Serverless v2:
+# - aws_appautoscaling_target (not supported for Serverless v2)
+# - aws_appautoscaling_policy (scaling is automatic)
+# - aws_appautoscaling_scheduled_action (not supported for Serverless v2)
+# - aws_cloudwatch_log_metric_filter (requires log group to exist first)
 
-# Auto-scaling policy based on CPU utilization
-resource "aws_appautoscaling_policy" "aurora_cpu" {
-  name               = "${var.project_name}-${var.environment_suffix}-aurora-cpu-scaling"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.aurora_serverless.resource_id
-  scalable_dimension = aws_appautoscaling_target.aurora_serverless.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.aurora_serverless.service_namespace
+# Aurora Serverless v2 scaling is configured in main.tf via:
+# - serverlessv2_scaling_configuration {
+#     min_capacity = var.aurora_min_capacity
+#     max_capacity = var.aurora_max_capacity
+#   }
 
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "RDSReaderAverageCPUUtilization"
-    }
-
-    target_value       = var.cpu_scale_up_threshold
-    scale_in_cooldown  = 300
-    scale_out_cooldown = 60
-  }
-}
-
-# Auto-scaling policy based on database connections
-resource "aws_appautoscaling_policy" "aurora_connections" {
-  name               = "${var.project_name}-${var.environment_suffix}-aurora-connections-scaling"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.aurora_serverless.resource_id
-  scalable_dimension = aws_appautoscaling_target.aurora_serverless.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.aurora_serverless.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "RDSReaderAverageDatabaseConnections"
-    }
-
-    target_value       = var.connections_scale_up_threshold
-    scale_in_cooldown  = 300
-    scale_out_cooldown = 60
-  }
-}
-
-# Custom CloudWatch metric for gaming-specific metrics
-resource "aws_cloudwatch_log_metric_filter" "concurrent_players" {
-  name           = "${var.project_name}-${var.environment_suffix}-concurrent-players"
-  log_group_name = "/aws/rds/cluster/${aws_rds_cluster.aurora_serverless.cluster_identifier}/general"
-  pattern        = "[timestamp, request_id, user, database, query_time, lock_time, rows_sent, rows_examined, query]"
-
-  metric_transformation {
-    name      = "ConcurrentPlayers"
-    namespace = "${var.project_name}/Gaming"
-    value     = "1"
-  }
-}
-
-# Scheduled scaling for predictable gaming patterns
-resource "aws_appautoscaling_scheduled_action" "scale_up_peak" {
-  name               = "${var.project_name}-${var.environment_suffix}-scale-up-peak"
-  service_namespace  = aws_appautoscaling_target.aurora_serverless.service_namespace
-  resource_id        = aws_appautoscaling_target.aurora_serverless.resource_id
-  scalable_dimension = aws_appautoscaling_target.aurora_serverless.scalable_dimension
-  schedule           = "cron(0 18 * * ? *)" # 6 PM UTC daily
-
-  scalable_target_action {
-    min_capacity = 4
-    max_capacity = var.aurora_max_capacity
-  }
-}
-
-resource "aws_appautoscaling_scheduled_action" "scale_down_offpeak" {
-  name               = "${var.project_name}-${var.environment_suffix}-scale-down-offpeak"
-  service_namespace  = aws_appautoscaling_target.aurora_serverless.service_namespace
-  resource_id        = aws_appautoscaling_target.aurora_serverless.resource_id
-  scalable_dimension = aws_appautoscaling_target.aurora_serverless.scalable_dimension
-  schedule           = "cron(0 2 * * ? *)" # 2 AM UTC daily
-
-  scalable_target_action {
-    min_capacity = floor(var.aurora_min_capacity)
-    max_capacity = 8
-  }
-}
+# This file is intentionally minimal as Aurora Serverless v2 handles scaling automatically.
