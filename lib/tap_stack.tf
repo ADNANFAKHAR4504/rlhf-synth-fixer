022eec29e98261ac5709c8b8ca20609af173ad19
root <root@DESKTOP-3KTOO66.localdomain>
2025-10-28 19:07:21 +0530

    fix(rds): rds version fix

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -669,8 +669,6 @@ resource "aws_db_instance" "primary" {
   monitoring_interval             = 60
   monitoring_role_arn            = aws_iam_role.rds_monitoring.arn
   
-  # Enable automated backups for read replica
-  apply_immediately = false
   
   tags = merge(local.primary_tags, {
     Name = "rds-primary-${local.resource_suffix}"
