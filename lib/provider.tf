fde65419b7cdb3e4550e992e158264e2242b1bc8
root <root@DESKTOP-3KTOO66.localdomain>
2025-10-30 20:48:27 +0530

    feat(rds): rds migration and replication

--- lib/provider.tf
+++ lib/provider.tf
@@ -16,5 +16,11 @@ terraform {
 
 # Primary AWS provider for general resources
 provider "aws" {
-  region = var.aws_region
+  alias  = "us_east_1"
+  region = var.primary_region
+}
+
+provider "aws" {
+  alias  = "us_west_2"
+  region = var.secondary_region
 }
