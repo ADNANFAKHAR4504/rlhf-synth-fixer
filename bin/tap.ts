a6a7ee309575dc3b3bbc56423dfca3e41fd9b3e5
Akshat Jain <akshat.j@turing.com>
2025-10-30 19:31:21 +0530

    fix: initialize infrastructure environment project 101000696

--- bin/tap.ts
+++ bin/tap.ts
@@ -4,7 +4,7 @@ import { TapStack } from '../lib/tap-stack';
 
 const app = new App();
 
-const environmentSuffix = process.env.ENV_SUFFIX || 'prod-soc';
+const environmentSuffix = process.env.ENV_SUFFIX || 'prod-soc-v1';
 
 new TapStack(app, 'Soc2BaselineStack', {
   environmentSuffix: environmentSuffix,
