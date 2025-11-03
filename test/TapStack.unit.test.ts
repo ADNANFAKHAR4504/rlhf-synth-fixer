9ccff7a7d3438e501b36082d2a673d514cf4d4e7
Rajendra <rajendra.s@turing.com>
2025-11-03 19:01:53 +0530

    test: inline pulumi mocks into unit suite

--- test/TapStack.unit.test.ts
+++ test/TapStack.unit.test.ts
@@ -1,6 +1,84 @@
-import './pulumi-setup';
-import * as stack from '../lib/index';
 import * as pulumi from '@pulumi/pulumi';
+
+const setUpPulumiTestEnvironment = () => {
+  process.env.ENVIRONMENT_SUFFIX = 'test';
+  process.env.AWS_REGION = 'us-east-1';
+  process.env.INPUT_BUCKET_NAME = 'image-input-test';
+  process.env.OUTPUT_BUCKET_NAME = 'image-output-test';
+
+  pulumi.runtime.setMocks(
+    {
+      newResource: (
+        args: pulumi.runtime.MockResourceArgs
+      ): { id: string; state: Record<string, unknown> } => {
+        const outputs: Record<string, unknown> = {
+          ...args.inputs,
+          arn:
+            args.type === 'aws:s3/bucket:Bucket'
+              ? `arn:aws:s3:::${args.inputs.bucket}`
+              : args.type === 'aws:lambda/function:Function'
+                ? `arn:aws:lambda:us-east-1:123456789012:function:${args.inputs.name}`
+                : args.type === 'aws:lambda/layerVersion:LayerVersion'
+                  ? `arn:aws:lambda:us-east-1:123456789012:layer:${args.inputs.layerName}:1`
+                  : args.type === 'aws:iam/role:Role'
+                    ? `arn:aws:iam::123456789012:role/${args.name}`
+                    : args.type === 'aws:iam/policy:Policy'
+                      ? `arn:aws:iam::123456789012:policy/${args.name}`
+                      : args.type === 'aws:kms/key:Key'
+                        ? 'arn:aws:kms:us-east-1:123456789012:key/test-key-id'
+                        : `arn:aws:${args.type}:us-east-1:123456789012:${args.name}`,
+          id:
+            args.type === 'aws:s3/bucket:Bucket'
+              ? args.inputs.bucket
+              : args.type === 'aws:lambda/function:Function'
+                ? args.inputs.name
+                : args.type === 'aws:kms/key:Key'
+                  ? 'test-key-id'
+                  : args.name,
+        };
+
+        if (args.type === 'aws:lambda/function:Function') {
+          outputs.invoke_arn = `arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${outputs.arn}/invocations`;
+          outputs.qualified_arn = `${outputs.arn}:$LATEST`;
+        }
+
+        if (args.type === 'aws:lambda/functionUrl:FunctionUrl') {
+          outputs.functionUrl = `https://${args.name}.lambda-url.us-east-1.on.aws/`;
+        }
+
+        if (args.type === 'aws:kms/key:Key') {
+          outputs.keyId = 'test-key-id';
+        }
+
+        const idValue =
+          typeof outputs.id === 'string' && outputs.id.length > 0
+            ? (outputs.id as string)
+            : args.name;
+
+        return {
+          id: idValue,
+          state: outputs,
+        };
+      },
+      call: (args: pulumi.runtime.MockCallArgs) => args.inputs,
+    },
+    'lambda-image-processing-optimization',
+    'test',
+    true
+  );
+
+  pulumi.runtime.setAllConfig({
+    'lambda-image-processing-optimization:environmentSuffix': 'test',
+    'lambda-image-processing-optimization:inputBucketName': 'image-input-test',
+    'lambda-image-processing-optimization:outputBucketName': 'image-output-test',
+  });
+};
+
+const stack = (() => {
+  setUpPulumiTestEnvironment();
+  // eslint-disable-next-line global-require, import/no-dynamic-require
+  return require('../lib/index') as typeof import('../lib/index');
+})();
 
 describe('Lambda Image Processing Infrastructure', () => {
   describe('S3 Buckets', () => {
