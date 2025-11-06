315c4e07bf76ef19244df8261412cd1bdb523408
mianadnanfakharhayyat <adnan.f@turing.com>
2025-11-06 15:55:39 +0500

    fix: region chage

--- bin/tap.ts
+++ bin/tap.ts
@@ -21,6 +21,6 @@ new TapStack(app, stackName, {
   environmentSuffix: environmentSuffix, // Pass the suffix to the stack
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
+    region: 'us-east-1',
   },
 });
