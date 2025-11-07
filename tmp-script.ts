1d6c33e89c2d6b183049fca94d09c322fd256a71
mianadnanfakharhayyat <adnan.f@turing.com>
2025-11-07 19:09:06 +0500

    fix: improve test

--- tmp-script.ts
+++
@@ -1,6 +0,0 @@
-import { Testing } from 'cdktf';
-import { TapStack } from './lib/tap-stack';
-const app = Testing.app();
-const stack = new TapStack(app, 'test');
-const synth = Testing.synth(stack);
-console.log(Object.keys(synth));
