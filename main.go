7fbeba2cc39cdb39658cda5e4ca950e676b25341
Arpit Patidar <arpit.p@turing.com>
2025-11-08 02:22:32 +0000

    feat(synth-101000868): provisioning of infrastructure environments
    Platform: cdk-go
    Complexity: hard
    Training Quality: 9/10
    
    Task ID: 101000868

---
+++ main.go
@@ -0,0 +1,8 @@
+// This file is not needed for CDK Go projects
+// The main entry point is in bin/tap.go
+package main
+
+func main() {
+	// This file exists for compatibility with repository structure
+	// Actual CDK code is in bin/tap.go and lib/tap_stack.go
+}
