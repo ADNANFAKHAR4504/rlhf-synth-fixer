89708c42dcee2b75976ad8fcfb58b7f2258dc1b1
kanishkah-turing <kanishka.h@turing.com>
2025-11-09 11:30:58 +0530

    feat: adding default values

--- lib/variables.tf
+++ lib/variables.tf
@@ -7,21 +7,25 @@ variable "aws_region" {
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "availability_zones" {
   description = "Availability zones"
   type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
 }
 
 variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "public_subnet_cidrs" {
   description = "CIDR blocks for public subnets"
   type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
 variable "enable_nat_gateway" {
@@ -39,6 +43,7 @@ variable "single_nat_gateway" {
 variable "rds_instance_class" {
   description = "RDS instance class"
   type        = string
+  default     = "db.t3.micro"
 }
 
 variable "db_name" {
@@ -62,26 +67,31 @@ variable "rds_allocated_storage" {
 variable "rds_backup_retention" {
   description = "RDS backup retention period in days"
   type        = number
+  default     = 7
 }
 
 variable "rds_multi_az" {
   description = "Enable Multi-AZ for RDS"
   type        = bool
+  default     = false
 }
 
 variable "ecs_task_count" {
   description = "Number of ECS tasks"
   type        = number
+  default     = 1
 }
 
 variable "ecs_task_cpu" {
   description = "ECS task CPU units"
   type        = string
+  default     = "256"
 }
 
 variable "ecs_task_memory" {
   description = "ECS task memory in MB"
   type        = string
+  default     = "512"
 }
 
 variable "certificate_arn" {
@@ -93,14 +103,17 @@ variable "certificate_arn" {
 variable "s3_transition_days" {
   description = "Days before transitioning to STANDARD_IA"
   type        = number
+  default     = 30
 }
 
 variable "s3_glacier_days" {
   description = "Days before transitioning to GLACIER"
   type        = number
+  default     = 90
 }
 
 variable "s3_expiration_days" {
   description = "Days before expiring objects"
   type        = number
+  default     = 365
 }
