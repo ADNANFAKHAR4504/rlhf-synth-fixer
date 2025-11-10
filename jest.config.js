b0c6552129af0cecaa27119e9e2adb2a76065ce6
veera-turing <veerasolaiyappan.v@turing.com>
2025-11-10 18:49:48 +0530

    feat(serverless): removed jest file changes

--- jest.config.js
+++ jest.config.js
@@ -9,37 +9,27 @@ module.exports = {
     '^.+\\.mjs$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(aws-cdk-lib|@aws-cdk|constructs|@aws-sdk|@smithy)/)',
+    'node_modules/(?!(aws-cdk-lib|@aws-cdk|constructs)/)',
   ],
   collectCoverageFrom: [
     '<rootDir>/lib/**/*.ts',
     '<rootDir>/lib/**/*.mjs',
-    '<rootDir>/lib/**/*.js',
     '!<rootDir>/bin/**/*.ts',
     '!<rootDir>/**/*.d.ts',
+    '!<rootDir>/**/*.js',
     '!<rootDir>/**/*.test.ts',
-    '!<rootDir>/**/*.test.js',
     '!<rootDir>/node_modules/**',
   ],
   coverageReporters: ['text', 'lcov', 'json-summary'],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+      branches: 90, 
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
   },
   testTimeout: 30000,
   silent: false,
   verbose: true,
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-      tsconfig: {
-        allowJs: true,
-        esModuleInterop: true
-      }
-    }
-  }
 };
