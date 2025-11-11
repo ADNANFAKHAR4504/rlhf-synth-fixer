257074e5df9f77cdbfa74791989a967f8b5274f0
“mayanksethi-turing” <sethi.m@turing.com>
2025-11-11 12:45:39 +0530

    fix: fixed the deployment and unit test issues

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -51,7 +51,7 @@ if [ "$PLATFORM" = "cdk" ]; then
 
   echo "Bootstrapping account $CURRENT_ACCOUNT_ID in ap-southeast-1..."
   npx cdk bootstrap aws://${CURRENT_ACCOUNT_ID}/ap-southeast-1 --force --context environmentSuffix=${ENVIRONMENT_SUFFIX}
-  
+
   echo "✅ both regions bootstrapped successfully"
 
   # npm run cdk:bootstrap
