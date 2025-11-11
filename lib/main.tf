7e6fce53656b7935325b86622339e032977954f8
sanket-turing <sanket.s@turing.com>
2025-11-11 17:59:28 +0530

    fix: update alb name

--- lib/main.tf
+++ lib/main.tf
@@ -293,7 +293,7 @@ resource "aws_lb" "main" {
 
 # Target Group
 resource "aws_lb_target_group" "main" {
-  name     = "${var.project_name}-tg"
+  name     = "${var.project_name}-tg-new"
   port     = 3000
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
@@ -311,7 +311,7 @@ resource "aws_lb_target_group" "main" {
   deregistration_delay = 30
 
   tags = {
-    Name        = "${var.project_name}-tg"
+    Name        = "${var.project_name}-tg-new"
     Environment = var.environment
     Project     = var.project_name
     ManagedBy   = "Terraform"
