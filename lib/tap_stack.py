d7e19d73e440dfefc063b37475966e01f0f9dfcc
emmanuel-turing <ogunfowokan.e@turing>
2025-11-12 06:47:39 +0100

    fix(deploy): fix deployment errors

--- lib/tap_stack.py
+++ lib/tap_stack.py
@@ -181,12 +181,98 @@ class TapStack(pulumi.ComponentResource):
             ])
         )
 
-        # Export outputs
+        # Register outputs using register_outputs
         self.register_outputs({
-            'primary_api_endpoint': self.api_gateway_stack.primary_api_endpoint,
-            'secondary_api_endpoint': self.api_gateway_stack.secondary_api_endpoint,
+            # Aurora outputs
+            'aurora_global_cluster_id': self.aurora_stack.global_cluster_id,
+            'aurora_primary_cluster_id': self.aurora_stack.primary_cluster_id,
             'aurora_primary_endpoint': self.aurora_stack.primary_endpoint,
+            'aurora_secondary_endpoint': self.aurora_stack.secondary_endpoint,
+            'aurora_secondary_cluster_arn': self.aurora_stack.secondary_cluster_arn,
+            
+            # DynamoDB outputs
             'dynamodb_table_name': self.dynamodb_stack.table_name,
-            # BUG #4: Missing several important outputs
-            # Missing: Aurora secondary endpoint, S3 buckets, SNS topics, failover function
+            'dynamodb_table_arn': self.dynamodb_stack.table_arn,
+            
+            # S3 outputs
+            's3_primary_bucket_name': self.s3_stack.primary_bucket_name,
+            's3_secondary_bucket_name': self.s3_stack.secondary_bucket_name,
+            
+            # Lambda outputs
+            'lambda_primary_function_arn': self.lambda_stack.primary_function_arn,
+            'lambda_secondary_function_arn': self.lambda_stack.secondary_function_arn,
+            'lambda_primary_function_name': self.lambda_stack.primary_function_name,
+            
+            # API Gateway outputs
+            'api_primary_api_id': self.api_gateway_stack.primary_api_id,
+            'api_primary_endpoint': self.api_gateway_stack.primary_api_endpoint,
+            'api_secondary_endpoint': self.api_gateway_stack.secondary_api_endpoint,
+            
+            # Route53 outputs
+            'route53_health_check_id': self.route53_stack.health_check_id,
+            
+            # SNS outputs
+            'sns_primary_topic_arn': self.sns_stack.primary_topic_arn,
+            'sns_secondary_topic_arn': self.sns_stack.secondary_topic_arn,
+            
+            # Monitoring outputs
+            'monitoring_composite_alarm_arn': self.monitoring_stack.composite_alarm_arn,
+            'monitoring_aurora_cpu_alarm_arn': self.monitoring_stack.aurora_cpu_alarm.arn,
+            'monitoring_lambda_error_alarm_arn': self.monitoring_stack.lambda_error_alarm.arn,
+            'monitoring_api_error_alarm_arn': self.monitoring_stack.api_error_alarm.arn,
+            
+            # Synthetics outputs
+            'synthetics_primary_canary_name': self.synthetics_stack.primary_canary.name,
+            'synthetics_secondary_canary_name': self.synthetics_stack.secondary_canary.name,
+            
+            # Failover outputs
+            'failover_function_arn': self.failover_stack.failover_function_arn,
+            'failover_function_name': self.failover_stack.failover_function_name,
         })
+
+        # Export outputs using pulumi.export()
+        # Aurora outputs
+        pulumi.export('aurora_global_cluster_id', self.aurora_stack.global_cluster_id)
+        pulumi.export('aurora_primary_cluster_id', self.aurora_stack.primary_cluster_id)
+        pulumi.export('aurora_primary_endpoint', self.aurora_stack.primary_endpoint)
+        pulumi.export('aurora_secondary_endpoint', self.aurora_stack.secondary_endpoint)
+        pulumi.export('aurora_secondary_cluster_arn', self.aurora_stack.secondary_cluster_arn)
+        
+        # DynamoDB outputs
+        pulumi.export('dynamodb_table_name', self.dynamodb_stack.table_name)
+        pulumi.export('dynamodb_table_arn', self.dynamodb_stack.table_arn)
+        
+        # S3 outputs
+        pulumi.export('s3_primary_bucket_name', self.s3_stack.primary_bucket_name)
+        pulumi.export('s3_secondary_bucket_name', self.s3_stack.secondary_bucket_name)
+        
+        # Lambda outputs
+        pulumi.export('lambda_primary_function_arn', self.lambda_stack.primary_function_arn)
+        pulumi.export('lambda_secondary_function_arn', self.lambda_stack.secondary_function_arn)
+        pulumi.export('lambda_primary_function_name', self.lambda_stack.primary_function_name)
+        
+        # API Gateway outputs
+        pulumi.export('api_primary_api_id', self.api_gateway_stack.primary_api_id)
+        pulumi.export('api_primary_endpoint', self.api_gateway_stack.primary_api_endpoint)
+        pulumi.export('api_secondary_endpoint', self.api_gateway_stack.secondary_api_endpoint)
+        
+        # Route53 outputs
+        pulumi.export('route53_health_check_id', self.route53_stack.health_check_id)
+        
+        # SNS outputs
+        pulumi.export('sns_primary_topic_arn', self.sns_stack.primary_topic_arn)
+        pulumi.export('sns_secondary_topic_arn', self.sns_stack.secondary_topic_arn)
+        
+        # Monitoring outputs
+        pulumi.export('monitoring_composite_alarm_arn', self.monitoring_stack.composite_alarm_arn)
+        pulumi.export('monitoring_aurora_cpu_alarm_arn', self.monitoring_stack.aurora_cpu_alarm.arn)
+        pulumi.export('monitoring_lambda_error_alarm_arn', self.monitoring_stack.lambda_error_alarm.arn)
+        pulumi.export('monitoring_api_error_alarm_arn', self.monitoring_stack.api_error_alarm.arn)
+        
+        # Synthetics outputs
+        pulumi.export('synthetics_primary_canary_name', self.synthetics_stack.primary_canary.name)
+        pulumi.export('synthetics_secondary_canary_name', self.synthetics_stack.secondary_canary.name)
+        
+        # Failover outputs
+        pulumi.export('failover_function_arn', self.failover_stack.failover_function_arn)
+        pulumi.export('failover_function_name', self.failover_stack.failover_function_name)
