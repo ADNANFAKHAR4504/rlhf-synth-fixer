903290ce69576846a5357a225312589009e4c7f2
sanket-turing <sanket.s@turing.com>
2025-11-13 19:35:19 +0530

    fix: fixing region

--- lib/tap-stack.tf
+++ lib/tap-stack.tf
@@ -25,7 +25,7 @@ variable "vpc_cidr" {
 variable "availability_zones" {
   description = "List of availability zones"
   type        = list(string)
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "custom_ami_id" {
