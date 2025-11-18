33a739bc214791c257e6a145c8b83a61e4a65b59
veera-turing <veerasolaiyappan.v@turing.com>
2025-11-18 22:42:26 +0530

    feat(app-development): changed the stack name format

--- bin/tap.ts
+++ bin/tap.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import 'source-map-support/register';
 import { TapStack } from '../lib/tap-stack';
 
 const app = new cdk.App();
@@ -9,7 +9,7 @@ const app = new cdk.App();
 const environmentSuffix =
   app.node.tryGetContext('environmentSuffix') || `dev-${Date.now()}`;
 
-new TapStack(app, `TapStack-${environmentSuffix}`, {
+new TapStack(app, `TapStack${environmentSuffix}`, {
   environmentSuffix,
   env: {
     region: process.env.CDK_DEFAULT_REGION || 'us-east-1',
