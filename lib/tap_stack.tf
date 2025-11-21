7f5bb2067352f2b605f8ec042475f0f2316a192e
jpoola-turing <ubuntu@Jaquar>
2025-11-21 22:53:05 +0530

    fix: remove iam role arn

--- lib/tap_stack.tf
+++ lib/tap_stack.tf
@@ -601,7 +601,6 @@ resource "aws_iam_role_policy" "vpc_flow_logs_s3" {
 
 # VPC Flow Logs
 resource "aws_flow_log" "vpc" {
-  iam_role_arn         = aws_iam_role.vpc_flow_logs.arn
   log_destination      = aws_s3_bucket.vpc_flow_logs.arn
   log_destination_type = "s3"
   traffic_type         = "ALL"
