19c68322bd11cd679d716547f247074607d2a3bc
root <root@Olatunjym.localdomain>
2025-11-23 05:30:26 +0100

    chore: refactored and addressed review's comment

--- lib/api_gateway.tf
+++ lib/api_gateway.tf
@@ -42,7 +42,6 @@ resource "aws_api_gateway_integration" "webhook_lambda" {
 resource "aws_api_gateway_deployment" "webhook_deployment" {
   depends_on  = [aws_api_gateway_integration.webhook_lambda]
   rest_api_id = aws_api_gateway_rest_api.webhook_api.id
-  stage_name  = var.environment
 
   lifecycle { create_before_destroy = true }
 }
