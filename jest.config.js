004e3d9ffa284ae57f06fe8bd37154fa86b608f7
chriscrsov <christian.o@turing.com>
2025-11-24 13:24:14 +0100

    refactor: cleanup process2

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts', '**/*.test.mjs', '**/*.test.js'],
+  testMatch: ['**/*.test.ts', '**/*.test.mjs'],
   preset: 'ts-jest',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'mjs'],
   transform: {
