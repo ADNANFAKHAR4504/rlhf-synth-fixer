f9e50f558ec06a605abd0a90abaf1acbdfc4aea2
kanishkah-turing <kanishka.h@turing.com>
2025-11-25 03:18:35 +0530

    feat: pr num and region change

--- lib/variables.tf
+++ lib/variables.tf
@@ -63,7 +63,7 @@ variable "kube_proxy_version" {
 variable "pr_number" {
   description = "PR number for resource identification"
   type        = string
-  default     = "pr7201"
+  default     = "pr7219"
 }
 
 variable "common_tags" {
