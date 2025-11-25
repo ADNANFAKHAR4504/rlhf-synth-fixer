4a09ee67efe2eb175adf05a0075653786ecc9b6a
Raaj C <223481822+raaj1021@users.noreply.github.com>
2025-11-25 09:35:31 +0530

    fix(deploy): fix deploy errors

--- lib/tap.py
+++ lib/tap.py
@@ -189,7 +189,7 @@ class PaymentInfrastructureStack(TerraformStack):
         table = DynamodbTable(
             self,
             'payments_table',
-            name=f'payment-{self.environment_suffix}-payments-v2',
+            name=f'payment-{self.environment_suffix}-payments-v3',
             billing_mode='PAY_PER_REQUEST',
             hash_key='payment_id',
             range_key='timestamp',
@@ -227,7 +227,7 @@ class PaymentInfrastructureStack(TerraformStack):
                 enabled=True,
                 kms_key_arn=self.kms_key_dynamodb.arn
             ),
-            tags={**self.common_tags, 'Name': f'payment-{self.environment_suffix}-payments-v2'},
+            tags={**self.common_tags, 'Name': f'payment-{self.environment_suffix}-payments-v3'},
             provider=self.primary_provider
         )
 
