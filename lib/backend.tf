df05e7dda3ab793e33de3216d9b9f75fcf953ff0
adnan-turing <adnan.f@turing.com>
2025-11-28 16:47:10 +0500

    fix: add changes

--- lib/backend.tf
+++ lib/backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "terraform-state-eks-cluster"
-    key            = "eks/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock-eks"
-  }
+  backend "s3" {}
 }
