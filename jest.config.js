d5bc2a70812fb6d107713286f7ec28a0f1b0c75a
Bala C <balasl342@gmail.com>
2025-11-28 12:02:41 +0530

    fix(add_file): added a jest config file

---
+++ jest.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  testMatch: ['**/*.test.ts', '**/*.test.mjs'],
+  preset: 'ts-jest',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'mjs'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    // Transform JS files (so ESM syntax in some node_modules can be transpiled)
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.mjs$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    // allow transforming some packages that ship ESM (including kubernetes client and its deps)
+    'node_modules/(?!(aws-cdk-lib|@aws-cdk|constructs|@aws-sdk|@smithy|@kubernetes/client-node|openid-client|oauth4webapi|jose)/)',
+  ],
+  collectCoverageFrom: [
+    '<rootDir>/lib/**/*.ts',
+    '<rootDir>/lib/**/*.mjs',
+    '<rootDir>/lib/**/*.js',
+    '!<rootDir>/bin/**/*.ts',
+    '!<rootDir>/**/*.d.ts',
+    '!<rootDir>/**/*.test.ts',
+    '!<rootDir>/**/*.test.js',
+    '!<rootDir>/node_modules/**',
+  ],
+  coverageReporters: ['text', 'lcov', 'json-summary'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 85,
+      lines: 85,
+      statements: 85,
+    },
+  },
+  testTimeout: 60000,
+  silent: false,
+  verbose: true,
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+      tsconfig: {
+        allowJs: true,
+        esModuleInterop: true,
+      },
+    },
+  },
+};
