d1840a0892a4aed44c81d328bd9bb927e4ad5012
Akshat Jain <akshat.j@turing.com>
2025-12-01 19:40:10 +0530

    feat(synth-101912847): serverless event processing

--- lib/variables.tf
+++ lib/variables.tf
@@ -1,7 +1,7 @@
 variable "environment_suffix" {
   description = "Environment suffix for unique resource naming"
   type        = string
-  default     = "dev5"
+  default     = "dev9"
 }
 
 variable "aws_region" {
