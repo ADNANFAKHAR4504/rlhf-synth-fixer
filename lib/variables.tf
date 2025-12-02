a734ed330d0fe3ef792f9594b960a368f59dcc34
adnan-turing <adnan.f@turing.com>
2025-12-02 14:00:54 +0500

    fix: add changes

--- lib/variables.tf
+++ lib/variables.tf
@@ -19,6 +19,7 @@ variable "environment_suffix" {
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "availability_zones" {
@@ -30,51 +31,61 @@ variable "availability_zones" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
+  default     = "t3.micro"
 }
 
 variable "min_size" {
   description = "Minimum number of instances in Auto Scaling Group"
   type        = number
+  default     = 1
 }
 
 variable "max_size" {
   description = "Maximum number of instances in Auto Scaling Group"
   type        = number
+  default     = 3
 }
 
 variable "desired_capacity" {
   description = "Desired number of instances in Auto Scaling Group"
   type        = number
+  default     = 2
 }
 
 variable "db_instance_class" {
   description = "RDS instance class"
   type        = string
+  default     = "db.t3.micro"
 }
 
 variable "db_backup_retention_days" {
   description = "Database backup retention period in days"
   type        = number
+  default     = 7
 }
 
 variable "db_multi_az" {
   description = "Enable Multi-AZ for RDS"
   type        = bool
+  default     = false
 }
 
 variable "enable_s3_versioning" {
   description = "Enable S3 bucket versioning"
   type        = bool
+  default     = true
 }
 
 variable "s3_lifecycle_days" {
   description = "Days before transitioning S3 objects to cheaper storage"
   type        = number
+  default     = 30
 }
 
 variable "enable_ssl" {
   description = "Enable SSL certificate for ALB"
   type        = bool
+  default     = false
 }
 
 variable "ssl_certificate_arn" {
