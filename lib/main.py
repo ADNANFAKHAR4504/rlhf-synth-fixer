11210c6799e7f528fe6b3f25199efa920820e62f
adnan-turing <adnan.f@turing.com>
2025-12-03 14:16:06 -0500

    fix: add changes

--- lib/main.py
+++ lib/main.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 """CDKTF Application Entry Point"""
 
+import os
 from cdktf import App
 from lib.tap_stack import TapStack
 
 app = App()
-TapStack(app, "vpc-peering-cross-account")
+
+# Read environment suffix from environment variable or use default
+environment_suffix = os.environ.get('ENVIRONMENT_SUFFIX', 'dev')
+
+TapStack(app, "vpc-peering-cross-account", environment_suffix=environment_suffix)
 app.synth()
--- main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""CDKTF Application Entry Point"""
-
-from cdktf import App
-from lib.tap_stack import TapStack
-
-app = App()
-TapStack(app, "vpc-peering-cross-account")
-app.synth()
