ef1ee8ad1396776fbae7848f2ed2e065fb4afb38
rajendra-s-aws <rajendra.s@turing.com>
2025-12-05 02:21:26 +0000

    fix: perform cleanup of files

--- bin/tap.ts
+++ bin/tap.ts
@@ -1,44 +1,12 @@
 #!/usr/bin/env node
 import { App } from 'cdktf';
-import { TapStack } from '../lib/tap-stack';
+import { TapStack } from './lib/tap-stack';
 
 const app = new App();
 
-// Get environment variables from the environment or use defaults
-const environmentSuffix = process.env.ENVIRONMENT_SUFFIX || 'dev';
-const stateBucket = process.env.TERRAFORM_STATE_BUCKET || 'iac-rlhf-tf-states';
-const stateBucketRegion =
-  process.env.TERRAFORM_STATE_BUCKET_REGION || 'us-east-1';
-const awsRegion = process.env.AWS_REGION || 'us-east-1';
-const repositoryName = process.env.REPOSITORY || 'unknown';
-const commitAuthor = process.env.COMMIT_AUTHOR || 'unknown';
-const prNumber = process.env.PR_NUMBER || 'unknown';
-const team = process.env.TEAM || 'unknown';
-const createdAt = new Date().toISOString();
-
-// Calculate the stack name
-const stackName = `TapStack${environmentSuffix}`;
-
-// defautlTags is structured in adherence to the AwsProviderDefaultTags interface
-const defaultTags = {
-  tags: {
-    Environment: environmentSuffix,
-    Repository: repositoryName,
-    Author: commitAuthor,
-    PRNumber: prNumber,
-    Team: team,
-    CreatedAt: createdAt,
-  },
-};
-
-// Create the TapStack with the calculated properties
-new TapStack(app, stackName, {
-  environmentSuffix: environmentSuffix,
-  stateBucket: stateBucket,
-  stateBucketRegion: stateBucketRegion,
-  awsRegion: awsRegion,
-  defaultTags: [defaultTags],
+new TapStack(app, 'TapStackdev', {
+  environmentSuffix: process.env.ENVIRONMENT_SUFFIX || 'dev',
+  awsRegion: process.env.AWS_REGION || 'us-east-1',
 });
 
-// Synthesize the app to generate the Terraform configuration
 app.synth();
