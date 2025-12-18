9048bd5d448c1735e35799ebee6463573ce7c356
Ubuntu <ubuntu@ip-10-0-1-70.ec2.internal>
2025-12-18 12:18:45 +0000

    fix(cdk): add cloudformation outputs to parent stack for deployment tracking
    The CI/CD Deploy job checks for outputs in the parent TapStack,
    but all outputs were defined only in the nested ResourcesStack.
    
    This commit adds CfnOutput constructs in the parent TapStack that
    reference the nested stack's resources, ensuring the Deploy job
    can successfully collect deployment outputs.
    
    Changes:
    - Added 8 CloudFormation outputs to TapStack:
      - S3BucketName, S3BucketArn
      - EC2InstanceId, EC2InstancePrivateIp
      - ElasticIP
      - SecurityGroupId, VpcId
      - InstanceRoleArn
    - All outputs reference resources from the nested ResourcesStack
    - Export names follow pattern: {stackName}-{OutputKey}
    
    This resolves the 'No deployment outputs found!' error.

--- lib/tap-stack.ts
+++ lib/tap-stack.ts
@@ -19,5 +19,55 @@ export class TapStack extends cdk.Stack {
       environmentSuffix,
       env: props?.env,
     });
+
+    // Export outputs from nested stack to parent stack
+    // CI/CD scripts expect outputs in the parent stack
+    new cdk.CfnOutput(this, 'S3BucketName', {
+      value: this.resourcesStack.bucket.bucketName,
+      description: 'S3 Bucket Name',
+      exportName: `${this.stackName}-S3BucketName`,
+    });
+
+    new cdk.CfnOutput(this, 'S3BucketArn', {
+      value: this.resourcesStack.bucket.bucketArn,
+      description: 'S3 Bucket ARN',
+      exportName: `${this.stackName}-S3BucketArn`,
+    });
+
+    new cdk.CfnOutput(this, 'EC2InstanceId', {
+      value: this.resourcesStack.instance.instanceId,
+      description: 'EC2 Instance ID',
+      exportName: `${this.stackName}-EC2InstanceId`,
+    });
+
+    new cdk.CfnOutput(this, 'EC2InstancePrivateIp', {
+      value: this.resourcesStack.instance.instancePrivateIp,
+      description: 'EC2 Instance Private IP',
+      exportName: `${this.stackName}-EC2InstancePrivateIp`,
+    });
+
+    new cdk.CfnOutput(this, 'ElasticIP', {
+      value: this.resourcesStack.eip.ref,
+      description: 'Elastic IP Address',
+      exportName: `${this.stackName}-ElasticIP`,
+    });
+
+    new cdk.CfnOutput(this, 'SecurityGroupId', {
+      value: this.resourcesStack.securityGroup.securityGroupId,
+      description: 'Security Group ID',
+      exportName: `${this.stackName}-SecurityGroupId`,
+    });
+
+    new cdk.CfnOutput(this, 'VpcId', {
+      value: this.resourcesStack.vpc.vpcId,
+      description: 'VPC ID',
+      exportName: `${this.stackName}-VpcId`,
+    });
+
+    new cdk.CfnOutput(this, 'InstanceRoleArn', {
+      value: this.resourcesStack.instanceRole.roleArn,
+      description: 'EC2 Instance IAM Role ARN',
+      exportName: `${this.stackName}-InstanceRoleArn`,
+    });
   }
 }
