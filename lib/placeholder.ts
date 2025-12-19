1385432a295fb0aee6e5ca77687eae6992973311
adnan-turing <adnan.f@turing.com>
2025-12-20 00:16:49 +0500

    fix: add changes

--- lib/placeholder.ts
+++ lib/placeholder.ts
@@ -2,5 +2,5 @@
 // The build script runs "tsc --skipLibCheck" which requires at least one .ts file.
 // This is a no-op placeholder that doesn't affect the actual JavaScript CDK code.
 
-export {};
+export { };
 
